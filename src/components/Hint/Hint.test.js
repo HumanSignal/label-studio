38edd6d22964150485fdad147409c025f0f13a72
Nik Shevchenko <shevchenko.nik@icloud.com>
2019-08-02 17:40:02 +0300

    Hint test

---
+++ src/components/Hint/Hint.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { shallowToJson } from "enzyme-to-json";
+
+import Hint from "./Hint";
+
+describe("Hint", () => {
+  it("Should render correctly", () => {
+    const output = shallow(<Hint copy="test">Test</Hint>);
+    expect(shallowToJson(output)).toMatchSnapshot();
+  });
+});
