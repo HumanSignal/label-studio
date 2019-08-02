aa7ad640b8ca245b2fe5ac3dbb0e1c133da9ae63
Nik Shevchenko <shevchenko.nik@icloud.com>
2019-08-02 20:12:30 +0300

    Unit tests of utils 96.59%

--- src/utils/__tests__/date.test.js
+++ src/utils/__tests__/date.test.js
@@ -1,9 +1,19 @@
-import { prettyDate } from "../date";
+import { prettyDate, msToHMS } from "../date";
 
 describe("Helper function prettyDate", () => {
   test("Undefined", () => {
     expect(prettyDate(undefined)).toBeUndefined();
     expect(prettyDate(null)).toBeUndefined();
     expect(prettyDate(123)).toBeUndefined();
+  });
+
+  test("Time Zone Moscow", () => {
+    expect(prettyDate(new Date().toISOString())).toBe("3 hours ago");
+  });
+});
+
+describe("Helper function msToHMS", () => {
+  test("Correct", () => {
+    expect(msToHMS(10000)).toBe("0:0:10");
   });
 });
