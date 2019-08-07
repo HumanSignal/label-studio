69b033425b0bf56a8cf0db06f091bb4cbd0123a1
Nik Shevchenko <shevchenko.nik@icloud.com>
2019-08-07 09:28:30 +0300

    Styles update. 0.0.3 (#9)
    * Styles with media
    
    * 0.0.3
    
    * v. 0.0.3
    
    * Readme to 0.0.3

--- build/service-worker.js
+++ build/service-worker.js
@@ -13,9 +13,7 @@
 
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
-importScripts(
-  "/heartexlabs/label-studio/precache-manifest.31f9c661cb79d18e37a72a8e72810ed4.js"
-);
+importScripts("/heartexlabs/label-studio/precache-manifest.d1503fe7b3768c4d072fcce0421307c2.js");
 
 workbox.clientsClaim();
 
@@ -29,6 +27,5 @@ workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/heartexlabs/label-studio/index.html", {
-  
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/],
 });
