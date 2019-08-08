6983fc41494b27e5cfaa708a16e4dd4661a618c5
Nik Shevchenko <shevchenko.nik@icloud.com>
2019-08-08 19:03:56 +0300

    Variables to API

---
+++ src/constants/Api.js
@@ -0,0 +1,9 @@
+export const API_URL = {
+  MAIN: "/api",
+  TASKS: "/tasks",
+  COMPLETIONS: "/completions",
+  CANCEL: "/cancel",
+  PROJECTS: "/projects",
+  NEXT: "/next",
+  EXPERT_INSRUCTIONS: "/expert_instruction",
+};
