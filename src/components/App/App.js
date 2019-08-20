374b4f4d97ada68474863a124e070ab2d39a2d6d
makseq-msi3 <makseq@gmail.com>
2019-08-20 05:26:17 +0300

    Back.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -103,8 +103,7 @@ const App = inject("store")(
 
         if (store.labeledSuccess) return this.renderSuccess();
 
-        if (!store.completionStore.currentCompletion && !store.hasInterface('completions'))
-            return this.renderNoCompletion();
+        if (!store.completionStore.currentCompletion) return this.renderNoCompletion();
 
         const { root } = store.completionStore.currentCompletion;
 
