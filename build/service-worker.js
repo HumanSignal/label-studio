f0279ac8056feb9365e84a744de6c7b568786e63
Nik Shevchenko <shevchenko.nik@icloud.com>
2019-09-27 19:48:12 +0300

    Public and Build update

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.67a3cb312dc157da7764dc95bb8d3ab2.js"
+  "/precache-manifest.1266865b5392486047d6668c3b592fa8.js"
 );
 
 workbox.clientsClaim();
