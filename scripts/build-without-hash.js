67fb823a3d0c979e926864661dceb95dbe7d9ec4
Nik Shevchenko <shevchenko.nik@icloud.com>
2019-10-24 20:50:45 +0300

    Build without hash

--- scripts/build-without-hash.js
+++ scripts/build-without-hash.js
@@ -9,6 +9,16 @@ config.optimization.splitChunks = {
 };
 
 /**
+ * Disable hash generation in production build for JS
+ */
+config.output.filename = "static/js/[name].bundle.js";
+
+/**
+ * Disable hash generation in production build for CSS
+ */
+config.plugins[5].options.filename = "static/css/[name].bundle.css";
+
+/**
  * Disable chunks in production build
  */
 config.optimization.runtimeChunk = false;
