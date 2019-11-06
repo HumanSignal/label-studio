7083b6c597537a037e9ef643a49aeb6a66cb0579
Nik Shevchenko <shevchenko.nik@icloud.com>
2019-11-06 16:10:19 +0100

    Example brush init

---
+++ examples/image_segmentation/index.js
@@ -0,0 +1,5 @@
+import config from "./config.xml";
+import tasks from "./tasks.json.js";
+import completion from "./completions/1.json.js";
+
+export const ImageSegmentation = { config, tasks, completion };
