648f33260c2b4976cceb0f371fe67ce08e9ef3a2
Nik Shevchenko <shevchenko.nik@icloud.com>
2019-11-06 16:13:53 +0100

    RLE

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,5 +4,6 @@ import * as UDate from "./date";
 import { guidGenerator } from "./unique";
 import { debounce } from "./debounce";
 import { styleToProp } from "./styles";
+import * as RLe from "./rle";
 
-export default { Checkers, Colors, UDate, guidGenerator, debounce, styleToProp };
+export default { Checkers, Colors, UDate, guidGenerator, debounce, styleToProp, RLe };
