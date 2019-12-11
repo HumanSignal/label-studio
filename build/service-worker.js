2a814aadc91ddcce29e777da94f97fb5079b63d8
deppp <mikhail.maluyk@gmail.com>
2019-12-11 16:52:57 +0300

    latest build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ee0d53fbf5fe7939d3239b7722020ec9.js"
+  "/precache-manifest.61329a249fc4b6458e4493b55746d80b.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
