6945e36a8dbc9276d6505d414cb75be26aafa0ee
deppp <mikhail.maluyk@gmail.com>
2019-12-14 22:58:18 +0300

    fix size and add style for header

--- src/interfaces/visual/Header.js
+++ src/interfaces/visual/Header.js
@@ -3,6 +3,8 @@ import { types } from "mobx-state-tree";
 import { observer } from "mobx-react";
 import { Typography } from "antd";
 
+import Tree from "../../core/Tree";
+
 import Registry from "../../core/Registry";
 import ProcessAttrsMixin from "../mixins/ProcessAttrs";
 
@@ -13,25 +15,26 @@ import ProcessAttrsMixin from "../mixins/ProcessAttrs";
  * @name Header
  * @param {number} [size=4] Size of header
  * @param {string} value Text of header
+ * @param {style} style css style string
  * @param {boolean} [underline=false] Underline of header
  */
 const Model = types.model({
   type: "header",
-  size: types.optional(types.number, 4),
+  size: types.optional(types.string, "4"),
+  style: types.maybeNull(types.string),
   _value: types.optional(types.string, ""),
   value: types.optional(types.string, ""),
   underline: types.optional(types.boolean, false),
 });
 
-const HeaderModel = types.compose(
-  "HeaderModel",
-  Model,
-  ProcessAttrsMixin,
-);
+const HeaderModel = types.compose("HeaderModel", Model, ProcessAttrsMixin);
 
 const HtxHeader = observer(({ item }) => {
+  const size = parseInt(item.size);
+  const style = item.style ? Tree.cssConverter(item.style) : { margin: "10px 0" };
+
   return (
-    <Typography.Title underline={item.underline} level={item.size} style={{ margin: "10px 0" }}>
+    <Typography.Title underline={item.underline} level={size} style={style}>
       {item._value}
     </Typography.Title>
   );
