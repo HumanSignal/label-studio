9084f75e84712589e21e648ef36da33b1154135b
makseq-msi3 <makseq@gmail.com>
2020-01-08 22:43:57 +0300

    Something working.

--- backend/label_studio/static/js/helpers.js
+++ backend/label_studio/static/js/helpers.js
@@ -1,5 +1,17 @@
 // replace all
-String.prototype.replaceAll = function(search, replacement) {
-  var target = this;
-  return target.replace(new RegExp(search, "g"), replacement);
+String.prototype.replaceAll = function (search, replacement) {
+    var target = this;
+    return target.replace(new RegExp(search, 'g'), replacement);
 };
+
+function IsJsonString(str) {
+  if (typeof str !== 'string')
+    return false;
+
+  try {
+    let json = JSON.parse(str);
+    return typeof json === 'object';
+  } catch (e) {
+    return false;
+  }
+}
