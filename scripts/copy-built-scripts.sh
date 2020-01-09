0d3f7dc2ce19f40b3154dbf5ccebba41ab1241e3
nik <nik@heartex.net>
2020-01-09 19:40:59 +0300

    remove scripts

--- scripts/copy-built-scripts.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p backend/label_studio/static/editor/js
-mkdir -p backend/label_studio/static/editor/css
-
-cp -r build/static/js/* backend/label_studio/static/editor/js/
-cp -r build/static/css/* backend/label_studio/static/editor/css/
--- scripts/copy.sh
+++
@@ -1,1 +0,0 @@
-cp -r examples/* src/examples/
