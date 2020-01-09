d8fd65e4d9153b30d21476ddff6a3969dc126a25
niklub <lubimov.nicolas@gmail.com>
2020-01-09 19:51:15 +0300

    Restructure label studio package dir (#164)
    * get rid of frontend sources/pull out label_studio dir
    
    * remove scripts

--- scripts/run-demo.sh
+++ scripts/run-demo.sh
@@ -3,6 +3,6 @@ example_dir=$1
 
 python label_studio/server.py \
     -p ${PORT:-8200} \
-    -i ../examples/$example_dir/tasks.json \
-    -o ../examples/$example_dir/completions \
-    -l ../examples/$example_dir/config.xml
+    -i examples/$example_dir/tasks.json \
+    -o examples/$example_dir/completions \
+    -l examples/$example_dir/config.xml
--- src/tags/visual/index.js
+++
@@ -1,7 +0,0 @@
-import { DialogModel } from "./Dialog";
-import { HeaderModel } from "./Header";
-import { TableModel } from "./Table";
-import { ViewModel } from "./View";
-import { StyleModel } from "./Style";
-
-export { DialogModel, HeaderModel, TableModel, ViewModel, StyleModel };
