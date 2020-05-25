7530ea704b1443367c2381fa8568fc0c8b11e8c2
nik <nik@heartex.net>
2020-05-25 16:09:38 +0300

    register gcs storage

--- label_studio/storage/__init__.py
+++ label_studio/storage/__init__.py
@@ -1,8 +1,11 @@
 from .base import create_storage, register_storage
 from .filesystem import JSONStorage, DirJSONsStorage
 from .s3 import S3Storage, S3BlobStorage
+from .gcs import GCSStorage, GCSBlobStorage
 
 register_storage('json', JSONStorage)
 register_storage('dir-jsons', DirJSONsStorage)
 register_storage('s3', S3Storage)
 register_storage('s3blob', S3BlobStorage)
+register_storage('gcs', GCSStorage)
+register_storage('gcsblob', GCSBlobStorage)
