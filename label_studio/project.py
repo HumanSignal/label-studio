045c93da6ae16a650cd5c5b7fc0d2d3149caecf7
nik <nik@heartex.net>
2020-05-27 19:09:24 +0300

    fix can manage tasks permissions

--- label_studio/project.py
+++ label_studio/project.py
@@ -136,11 +136,11 @@ class Project(object):
 
     @property
     def can_manage_tasks(self):
-        return self.config['source']['type'] not in {'s3', 's3blob', 'gcs', 'gcsblob'}
+        return self.can_manage_completions and self.config['source']['type'] not in {'s3', 's3blob', 'gcs', 'gcsblob'}
 
     @property
     def can_manage_completions(self):
-        return self.can_manage_tasks and self.config['target']['type'] not in {'s3', 's3blob', 'gcs', 'gcsblob'}
+        return self.config['target']['type'] not in {'s3', 's3blob', 'gcs', 'gcsblob'}
 
     @property
     def data_types_json(self):
