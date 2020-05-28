f6a5f3b8e60d5c517a67a31ad474ecacca07df00
makseq-ubnt <makseq@gmail.com>
2020-05-28 03:45:35 +0300

    polyfill.

--- label_studio/static/js/helpers.js
+++ label_studio/static/js/helpers.js
@@ -204,10 +204,35 @@ function randomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// includes support for old browsers
+// string includes polyfill support for old browsers
 if (!String.prototype.includes) {
     String.prototype.includes = function() {
         'use strict';
         return String.prototype.indexOf.apply(this, arguments) !== -1;
     };
+}
+
+// array includes polyfill
+if(!Array.prototype.includes){
+   //or use Object.defineProperty
+   Array.prototype.includes = function(search){
+    return !!~this.indexOf(search);
+  }
+}
+if(!Array.prototype.indexOf){
+Array.prototype.indexOf = (function(Object, max, min){
+  "use strict";
+  return function indexOf(member, fromIndex) {
+    if(this===null||this===undefined)throw TypeError("Array.prototype.indexOf called on null or undefined");
+
+    var that = Object(this), Len = that.length >>> 0, i = min(fromIndex | 0, Len);
+    if (i < 0) i = max(0, Len+i); else if (i >= Len) return -1;
+
+    if(member===void 0){ for(; i !== Len; ++i) if(that[i]===void 0 && i in that) return i; // undefined
+    }else if(member !== member){   for(; i !== Len; ++i) if(that[i] !== that[i]) return i; // NaN
+    }else                           for(; i !== Len; ++i) if(that[i] === member) return i; // all else
+
+    return -1; // if the value was not found, then return -1
+  };
+})(Object, Math.max, Math.min);
 }
