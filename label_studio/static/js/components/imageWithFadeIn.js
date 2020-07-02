d03bb56e2caf4a9eb12dbbc75f60c4ffee8b3b46
makseq-ubnt <makseq@gmail.com>
2020-07-02 15:13:13 +0300

    Fixes.

--- label_studio/static/js/components/imageWithFadeIn.js
+++ label_studio/static/js/components/imageWithFadeIn.js
@@ -1,28 +1,42 @@
 var imageWithFadeIn = Vue.component('imageWithFadeIn', {props: ['src'],
     data: function () {
-        return {loaded: false}
+        return {
+            loaded: false,
+            initial_path: ''
+        }
     },
     methods: {
         onLoaded: function () {
             this.loaded = true;
         },
-        renderPreviewImage: function(path) {
+        renderTippyImage: function(path) {
           return "<div style='cursor:pointer' " +
               "z-inder='1000' class='full-preview-wrapper'><center>" +
 
             "<div onclick='copyToClipboard(\""+ path +"\");'>" +
-                "<img src='" + path + "' class='full-preview-image'><br/>" +
-                "<span>" + path + "</span><br/>" +
+                "<img src='" + this.initial_path + "' class='full-preview-image'><br/>" +
+                "<span style='word-break: break-word'>" + path + "</span><br/>" +
                 "click to copy" +
             "</div>" +
             "</center></div>"
+        },
+        updateInitialPath: function () {
+            // save initial path to show in tippy
+            if (this.initial_path.split('?')[0] !== this.src.split('?')[0]) {
+                this.initial_path = this.src;
+            }
         }
+    },
+    mounted: function() {
+        this.updateInitialPath();
     },
     watch: {
         src: function (value, old) {
+            this.updateInitialPath();
+
             // prevent updates when s3 or gsc regenerate new link to images
             if (value.split('?')[0] === old.split('?')[0]) {
-                return old;
+                return;
             }
             this.loaded = false;
         }
@@ -33,7 +47,7 @@ var imageWithFadeIn = Vue.component('imageWithFadeIn', {props: ['src'],
             'class="preview-image" ' +
             'v-show="loaded" ' +
             'v-on:load="onLoaded()" ' +
-            ':content="renderPreviewImage(src)" ' +
+            ':content="renderTippyImage(src)" ' +
             'v-tippy="{trigger: \'click\', duration: 0, allowHTML: true, delay: 0, maxWidth: 700, interactive: true}" ' +
         '>'+
         '</transition>'
