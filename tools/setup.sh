de51652d84d0ab6283b266ff22756eb8aa5e713a
Dave Moloney <dave@native.io>
2020-10-30 18:06:38 +0000

    Remove proxy-redirect
    Was causing URLs in browser to 301 to Docker URLs

--- tools/setup.sh
+++ tools/setup.sh
@@ -79,7 +79,6 @@ echo "    sub_filter 'localhost:8080' 'labels$i.$BASE_URL';" >> $FILE
 echo '    sub_filter_once off;' >> $FILE
 echo '    location / {' >> $FILE
 echo "      proxy_pass 'http://labels$i:8080';" >> $FILE
-echo "      proxy_redirect http://labels$i.$BASE_URL/welcome https://labels$i.$BASE_URL/welcome;" >> $FILE
 echo '      rewrite ^/(.*)$ /$1 break;' >> $FILE
 echo '    }' >> $FILE
 echo '' >> $FILE
