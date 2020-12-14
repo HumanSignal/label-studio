7766087251b20854e6be4579b7615a01812b2979
Nikita Skryabin <nr@fenelon.ru>
2020-12-14 15:08:58 +0300

    Update DM build

--- label_studio/static/js/modules/index.js
+++ label_studio/static/js/modules/index.js
@@ -6,65 +6,6 @@ const dmRoot = document.querySelector(".datamanager");
 if (dmRoot) {
   const dm = new DataManager({
     root: dmRoot,
-    api: {
-      gateway: "/api",
-      endpoints: {
-        project: "/project",
-        columns: "/project/columns",
-        tabs: "/project/tabs",
-        updateTab: {
-          path: "/project/tabs/:tabID",
-          method: "post",
-        },
-        deleteTab: {
-          path: "/project/tabs/:tabID",
-          method: "delete",
-        },
-
-        tasks: "/project/tabs/:tabID/tasks",
-        annotations: "/project/tabs/:tabID/annotations",
-
-        task: "/tasks/:taskID",
-        nextTask: "/project/next",
-
-        completion: "/tasks/:taskID/completions/:id",
-        skipTask: {
-          path: "/tasks/:taskID/completions",
-          method: "post",
-        },
-        submitCompletion: {
-          path: "/tasks/:taskID/completions",
-          method: "post",
-        },
-        updateCompletion: {
-          path: "/tasks/:taskID/completions/:completionID",
-          method: "post",
-        },
-        deleteCompletion: {
-          path: "/tasks/:taskID/completions/:completionID",
-          method: "delete",
-        },
-
-        setSelectedItems: {
-          path: "/project/tabs/:tabID/selected-items",
-          method: "post",
-        },
-        addSelectedItem: {
-          path: "/project/tabs/:tabID/selected-items",
-          method: "patch",
-        },
-        deleteSelectedItem: {
-          path: "/project/tabs/:tabID/selected-items",
-          method: "delete",
-        },
-
-        actions: "/project/actions",
-        invokeAction: {
-          path: "/project/tabs/:tabID/actions",
-          method: "post",
-        },
-      },
-    },
   });
 
   console.log(dm);
