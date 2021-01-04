ce00ea80ccc1e734ed0f80f49a3749a5331ffd1a
Hovhannes <34022986+ahovhannes@users.noreply.github.com>
2021-01-04 10:22:04 +0400

    Update ui_test_label_studio.py

--- test/ui_test_label_studio.py
+++ test/ui_test_label_studio.py
@@ -1,7 +1,7 @@
 #
-
-#
 # Author: Hovhannes Atoyan (hovhannes.atoyan@gmail.com)
+# UI automation for Label Studio
+# Automated base steps for Image labeling (Setup, Import, Start, Export)
 #
 
 import os
@@ -36,8 +36,6 @@ class myTestCase(unittest.TestCase):
         self.probel5 = '      '
         #
         self.site_url = 'http://localhost:8080/welcome'
-        #self.loginUrl = 'http://devshop.oneplanetops.com/customer/account/login/'
-        #self.headerLoginBtn = '//div[@class="signin"]/a[contains(text(), " Sign In")]'
         #
         self.site_logo = '//a[@id="logo"]/img'
         self.welcome_Setup  = '//a[contains(text(), "Setup")]'
@@ -52,13 +50,15 @@ class myTestCase(unittest.TestCase):
         #
         self.import_img_draganddrop_id = 'file-input'
         # self.import_img_draganddrop_text = '//p[contains(text(), "Drag and drop your files here or click for import")]'
-        self.import_img_src = 'qa-resources/tmp_images/120825952_2800299673523323_2768303406576699097_n.jpg'
+        self.import_img_src = 'img/imgForDragAndDrop.jpg'
+        # self.import_img_src = '/Users/ahovhannes/Desktop/Hovo/h/JOB/Job_Label_Studio/label_studio_project/label-studio/test/img/imgForDragAndDrop.jpg'
         #
         # self.start_label_car = '//span[contains(text(), "Car")]'
+        self.start_label_car = 'ant-tag'
         self.start_submit_btn = '//button/span[contains(text(), "Submit ")]'
         #
         self.export_submit_btn = '//a[contains(text(), "Export Completions")]'
-        
+        #
 
     def tearDown(self):
         self.driver.close()
@@ -101,7 +101,7 @@ class myTestCase(unittest.TestCase):
         # self.Setup_label_car = '/html/body/div/div/div/div/div/div/div[1]/div/div/div[2]/span[1]'
         # self.driver.find_element_by_xpath(self.Setup_label_car).click()
         #
-        
+        #
         # iframe_ = self.driver.find_elements_by_tag_name('iframe')[0]
         # iframe_ = self.driver.find_elements_by_tag_name('iframe')
         # iframe_ = self.driver.find_element_by_xpath('//iframe[@class="render-editor"]')
@@ -147,7 +147,7 @@ class myTestCase(unittest.TestCase):
         #
         # Choose image for dragAndDrop
         print('Direktory is: ' + str(os.getcwd()))
-        self.import_img_src = os.getcwd() +'/'+ self.import_img_src
+        self.import_img_src = str(os.getcwd()) +'/'+ self.import_img_src
         print('Image is: ' +str(self.import_img_src))
         self.driver.find_element_by_id(self.import_img_draganddrop_id).send_keys(self.import_img_src)
         print("Image choosed from system and placed into dragAndDrop area")
@@ -167,7 +167,7 @@ class myTestCase(unittest.TestCase):
         #
         # Click on Label Airplane
         # self.driver.find_element_by_xpath(self.start_label_car).click()
-        label_car_btn = self.wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "ant-tag")))
+        label_car_btn = self.wait.until(EC.element_to_be_clickable((By.CLASS_NAME, self.start_label_car)))
         label_car_btn.click()
         #
         # Doing 3 clicks for create area
@@ -209,153 +209,5 @@ class myTestCase(unittest.TestCase):
         self.driver.find_element_by_xpath(self.site_logo).click()
 
 
-
-'''
-    #Check SignIn button existance and click on it
-    def clickOnSignInBtn(self, testCaseName):
-        print("----- Begin TestCase: "+testCaseName+" -----")
-        if self.check_exists_by_xpath(self.headerLoginBtn):
-            print(self.probel5+"SUCCESS: SignIn button presented on page")
-            #self.driver.find_element_by_xpath(self.headerLoginBtn).click()
-            self.clickSignInBtn()
-        else:
-            print(self.probel5+"ERROR: SignIn button "+str(self.headerLoginBtn)+" doesn't exists")
-        print("----- END TestCase: "+testCaseName+" -----\n")
-
-    #Inputs Email, inputs Password and clicks on SignIn button
-    def doLogin(self, login, password):
-        self.driver.find_element_by_xpath(self.inputEmail).clear()
-        self.driver.find_element_by_xpath(self.inputPassword).clear()
-        self.driver.find_element_by_xpath(self.inputEmail).send_keys(login)
-        self.driver.find_element_by_xpath(self.inputPassword).send_keys(password)
-        time.sleep(2)
-        self.driver.find_element_by_xpath(self.submitVoiti).click()
-
-    #Do LogOut
-    def doLogOut(self, testCaseName):
-        print("..... Doing Sign Out .....")
-        #but_loc = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, self.myAccountIcon))) #hover over button
-        #ActionChains(self.driver).move_to_element(but_loc).perform()
-        #sec_but = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, self.myAccountHoverLogOut))) #click on contact
-        #sec_but.click()
-        #
-        #but_loc = self.wait.until(EC.visibility_of_element_located((By.XPATH, self.myAccountIcon))) #hover over button
-        #ActionChains(self.driver).move_to_element(but_loc).perform()
-        #sec_but = self.wait.until(EC.visibility_of_element_located((By.XPATH, self.myAccountHoverLogOut))) #click on contact
-        #sec_but.click()
-        #
-        loginButtonElement = self.wait.until(lambda driver: driver.find_element_by_xpath(self.myAccountIcon))
-        loginButtonElement = self.wait.until(lambda driver: driver.find_element_by_xpath(self.myAccountHoverLogOut))
-        time.sleep(5)
-
-
-    #Login via incorrect Email and incorrect Password
-    def tc1(self, testCaseName):
-        print("----- Begin TestCase: "+testCaseName+" -----")
-        self.doLogin(self.wrongUser, self.wrongPass)
-        #time.sleep(5)
-        currentUrl = self.driver.current_url
-        if currentUrl == self.loginUrl:
-        #if self.check_exists_by_xpath(self.wrongLoginPassMsgElement):
-            print(self.probel5+"SUCCESS: Can't "+testCaseName)
-        else:
-            print(self.probel5+"ERROR: tc1=>It is possible to "+testCaseName)
-        print("----- END TestCase: "+testCaseName+" -----\n")
-
-    #Login via incorrect Email and correct Password
-    def tc2(self, testCaseName):
-        print("----- Begin TestCase: "+testCaseName+" -----")
-        self.doLogin(self.wrongUser, self.truePass)
-        time.sleep(5)
-        currentUrl = self.driver.current_url
-        if currentUrl == self.loginUrl:
-        #if self.check_exists_by_xpath(self.wrongLoginPassMsgElement):
-            print(self.probel5+"SUCCESS: Can't "+testCaseName)
-        else:
-            print(self.probel5+"ERROR: tc2=>It is possible to "+testCaseName)
-        print("----- END TestCase: "+testCaseName+" -----\n")
-
-    #Login via correct Email and incorrect Password
-    def tc3(self, testCaseName):
-        print("----- Begin TestCase: "+testCaseName+" -----")
-        self.doLogin(self.trueUser, self.wrongPass)
-        time.sleep(5)
-        currentUrl = self.driver.current_url
-        if currentUrl == self.loginUrl:
-        #if self.check_exists_by_xpath(self.wrongLoginPassMsgElement):
-            print(self.probel5+"SUCCESS: Can't "+testCaseName)
-        else:
-            print(self.probel5+"ERROR: tc3=>It is possible to "+testCaseName)
-        print("----- END TestCase: "+testCaseName+" -----\n")
-
-    #Login via correct Email and correct Password
-    def tc4(self, testCaseName):
-        print("----- Begin TestCase: "+testCaseName+" -----")
-        self.doLogin(self.trueUser, self.truePass)
-        currentUrl = self.driver.current_url
-        if currentUrl == self.loginUrl:
-            print(self.probel5+"ERROR: User hadn't logined")
-        else:
-            print(self.probel5+"SUCCESS: "+testCaseName)
-        time.sleep(5)
-        print("----- END TestCase: "+testCaseName+" -----\n")
-
-    #Going to My Orders
-    def goToMyOrders(self, testCaseName):
-        print("..... "+testCaseName+" .....")
-        self.driver.find_element_by_xpath(self.myOrdersLink).click()
-        time.sleep(2)
-
-    #Going to All Products page
-    def goToAllProducts(self, testCaseName):
-        print("..... "+testCaseName+" .....")
-        self.driver.find_element_by_xpath(self.allProductsLink).click()
-        time.sleep(2)
-
-    #Click on Product
-    def clickOnProduct(self, testCaseName):
-        print("..... "+testCaseName+" .....")
-        self.driver.find_element_by_xpath(self.productFromAllProducts).click()
-        time.sleep(5)
-
-    #Add choosed product to Cart
-    def addChoosedProductToCart(self, testCaseName):
-        print("..... "+testCaseName+" .....")
-        #Choosing Color
-        self.driver.find_element_by_xpath(self.choosedProductColor).click()
-        #Choosing Size
-        self.driver.find_element_by_xpath(self.choosedProductSize).click()
-        #Choosing Quantity
-        self.driver.find_element_by_xpath(self.choosedProductQuantity).click()
-        time.sleep(2)
-        #Click on ADD TO CART
-        self.driver.find_element_by_xpath(self.choosedProductAddToCartBtn).click()
-        #Close after AddToCart pop-up
-        popupCloseIcon = '/html/body/div[2]/div/div[1]/div[2]/button'
-        self.wait.until(EC.element_to_be_clickable((By.XPATH, popupCloseIcon))).click()
-        #
-        #alert_obj = self.driver.switch_to.alert
-        #alert_obj.dismiss()
-        time.sleep(5)
-
-    #Going to MyAccount
-    def goToMyAccount(self, testCaseName):        
-        #but_loc = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, self.myAccountIcon)))#hover over button
-        #ActionChains(self.driver).move_to_element(but_loc).perform()
-        #sec_but = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, self.myAccountHoverMyAccount))) #click on contact
-        #sec_but.click()
-        #
-        but_loc = self.wait.until(EC.visibility_of_element_located((By.XPATH, self.myAccountIcon)))#hover over button
-        ActionChains(self.driver).move_to_element(but_loc).perform()
-        sec_but = self.wait.until(EC.visibility_of_element_located((By.XPATH, self.myAccountHoverMyAccount))) #click on contact
-        sec_but.click()
-        time.sleep(2)
-'''
-
 if __name__ == "__main__":
     unittest.main()
-
-
-
-
-
