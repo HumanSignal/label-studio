3a90f44774cc45ff5a10e48cd06f94bc999d6a66
hlomzik <hlomzik@gmail.com>
2021-01-21 00:30:27 +0300

    Main page mock-up fixes
    Gaps and overlays
    Outliers on mobile screens
    Simplify sliders and fix console error about missing one
    Fix github stars rounding
    Add new release with github link

--- docs/themes/htx/source/js/new-index.js
+++ docs/themes/htx/source/js/new-index.js
@@ -56,76 +56,37 @@ $(document).ready(function(){
     }
   });
 
-  const slider = document.querySelector('.content__label');
-  let isDown = false;
-  let startX;
-  let scrollLeft;
+  function activateSlider(slider) {
+    if (!slider) return;
 
-  slider.addEventListener('mousedown', (e) => {
-    isDown = true;
-    slider.classList.add('active');
-    startX = e.pageX - slider.offsetLeft;
-    scrollLeft = slider.scrollLeft;
-  });
-  slider.addEventListener('mouseleave', () => {
-    isDown = false;
-    slider.classList.remove('active');
-  });
-  slider.addEventListener('mouseup', () => {
-    isDown = false;
-    slider.classList.remove('active');
-  });
-  slider.addEventListener('mousemove', (e) => {
-    if(!isDown) return;
-    e.preventDefault();
-    const x = e.pageX - slider.offsetLeft;
-    const walk = (x - startX) * 3; //scroll-fast
-    slider.scrollLeft = scrollLeft - walk;
-  });
-  const slider2 = document.querySelector(".community__wrapper");
-  slider2.addEventListener('mousedown', (e) => {
-    isDown = true;
-    slider2.classList.add('active');
-    startX = e.pageX - slider2.offsetLeft;
-    scrollLeft = slider2.scrollLeft;
-  });
-  slider2.addEventListener('mouseleave', () => {
-    isDown = false;
-    slider2.classList.remove('active');
-  });
-  slider2.addEventListener('mouseup', () => {
-    isDown = false;
-    slider2.classList.remove('active');
-  });
-  slider2.addEventListener('mousemove', (e) => {
-    if(!isDown) return;
-    e.preventDefault();
-    const x = e.pageX - slider2.offsetLeft;
-    const walk = (x - startX) * 3; //scroll-fast
-    slider2.scrollLeft = scrollLeft - walk;
-  });
+    let isDown = false;
+    let startX;
+    let scrollLeft;
 
+    slider.addEventListener('mousedown', (e) => {
+      isDown = true;
+      slider.classList.add('active');
+      startX = e.pageX - slider.offsetLeft;
+      scrollLeft = slider.scrollLeft;
+    });
+    slider.addEventListener('mouseleave', () => {
+      isDown = false;
+      slider.classList.remove('active');
+    });
+    slider.addEventListener('mouseup', () => {
+      isDown = false;
+      slider.classList.remove('active');
+    });
+    slider.addEventListener('mousemove', (e) => {
+      if(!isDown) return;
+      e.preventDefault();
+      const x = e.pageX - slider.offsetLeft;
+      const walk = (x - startX) * 3; //scroll-fast
+      slider.scrollLeft = scrollLeft - walk;
+    });
+  }
 
-  const slider3 = document.querySelector(".content__video");
-  slider3.addEventListener('mousedown', (e) => {
-    isDown = true;
-    slider3.classList.add('active');
-    startX = e.pageX - slider3.offsetLeft;
-    scrollLeft = slider3.scrollLeft;
-  });
-  slider3.addEventListener('mouseleave', () => {
-    isDown = false;
-    slider3.classList.remove('active');
-  });
-  slider3.addEventListener('mouseup', () => {
-    isDown = false;
-    slider3.classList.remove('active');
-  });
-  slider3.addEventListener('mousemove', (e) => {
-    if(!isDown) return;
-    e.preventDefault();
-    const x = e.pageX - slider3.offsetLeft;
-    const walk = (x - startX) * 3; //scroll-fast
-    slider3.scrollLeft = scrollLeft - walk;
-  });
+  activateSlider(document.querySelector('.content__label'));
+  activateSlider(document.querySelector(".community__wrapper"));
+  activateSlider(document.querySelector(".content__video"));
 });
