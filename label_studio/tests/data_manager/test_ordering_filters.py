efc6358962dcd9b38d41512e701872858b6ea67b
Sergei Ivashchenko <triklozoid@gmail.com>
2021-03-16 13:44:29 +0300

    Add ordering and filters undefined data column tests

--- label_studio/tests/data_manager/test_ordering_filters.py
+++ label_studio/tests/data_manager/test_ordering_filters.py
@@ -6,6 +6,7 @@ import json
 from tests.utils import make_task, make_annotation, make_prediction
 from projects.models import Project
 from data_manager.models import View
+from django.conf import settings
 from django.utils.timezone import now
 
 
@@ -22,30 +23,33 @@ def project_id(business_client):
 
 
 @pytest.mark.parametrize(
-    "ordering, element_index",
+    "ordering, element_index, undefined",
     [
-        [["id"], 0],  # ordered by id ascending, first element api == first created
-        [["-id"], -1],  # ordered by id descending, first element api == last created
-        [["completed_at"], 0],
-        [["-completed_at"], -1],
-        [["total_annotations"], -1],
-        [["-total_annotations"], 0],
-        [["total_predictions"], 0],
-        [["-total_predictions"], -1],
-        [["cancelled_annotations"], 0],
-        [["-cancelled_annotations"], -1],
-        [["annotations_results"], 0],
-        [["-annotations_results"], -1],
-        [["predictions_results"], 0],
-        [["-predictions_results"], -1],
-        [["predictions_score"], 0],
-        [["-predictions_score"], -1],
-        [["data.text"], 0],
-        [["-data.text"], -1],
+        [["tasks:id"], 0, False],  # ordered by id ascending, first element api == first created
+        [["tasks:-id"], -1, False],  # ordered by id descending, first element api == last created
+        [["tasks:completed_at"], 0, False],
+        [["tasks:-completed_at"], -1, False],
+        [["tasks:total_annotations"], -1, False],
+        [["tasks:-total_annotations"], 0, False],
+        [["tasks:total_predictions"], 0, False],
+        [["tasks:-total_predictions"], -1, False],
+        [["tasks:cancelled_annotations"], 0, False],
+        [["tasks:-cancelled_annotations"], -1, False],
+        [["tasks:annotations_results"], 0, False],
+        [["tasks:-annotations_results"], -1, False],
+        [["tasks:predictions_results"], 0, False],
+        [["tasks:-predictions_results"], -1, False],
+        [["tasks:predictions_score"], 0, False],
+        [["tasks:-predictions_score"], -1, False],
+        [["tasks:data.text"], 0, False],
+        [["tasks:-data.text"], -1, False],
+        [["tasks:data.data"], 0, True],
+        [["-tasks:data.data"], 1, True],
     ],
 )
 @pytest.mark.django_db
-def test_views_ordering(ordering, element_index, business_client, project_id):
+def test_views_ordering(ordering, element_index, undefined, business_client, project_id):
+
     payload = dict(
         project=project_id,
         data={"test": 1, "ordering": ordering},
@@ -61,11 +65,16 @@ def test_views_ordering(ordering, element_index, business_client, project_id):
 
     project = Project.objects.get(pk=project_id)
 
-    task_id_1 = make_task({"data": {"text": 1}}, project).id
+    if undefined:
+        task_field_name = settings.DATA_UNDEFINED_NAME
+    else:
+        task_field_name = 'text'
+
+    task_id_1 = make_task({"data": {task_field_name: 1}}, project).id
     make_annotation({"result": [{"1": True}]}, task_id_1)
     make_prediction({"result": [{"1": True}], "score": 1}, task_id_1)
 
-    task_id_2 = make_task({"data": {"text": 2}}, project).id
+    task_id_2 = make_task({"data": {task_field_name: 2}}, project).id
     for _ in range(0, 2):
         make_annotation({"result": [{"2": True}], "was_cancelled": True}, task_id_2)
     for _ in range(0, 2):
@@ -242,6 +251,35 @@ def test_views_ordering(ordering, element_index, business_client, project_id):
                 "conjunction": "or",
                 "items": [
                     {
+                        "filter": "filter:tasks:data.data",
+                        "operator": "contains",
+                        "type": "String",
+                        "value": "text1",
+                    }
+                ],
+            },
+            [1, ],
+        ],
+        [
+            {
+                "conjunction": "and",
+                "items": [
+                    {
+                        "filter": "filter:tasks:data.data",  # undefined column test
+                        "operator": "contains",
+                        "type": "String",
+                        "value": "text",
+                    },
+                    {"filter": "filter:tasks:id", "operator": "equal", "value": 1, "type": "Number"},
+                ],
+            },
+            [1, ],
+        ],
+        [
+            {
+                "conjunction": "or",
+                "items": [
+                    {
                         "filter": "filter:tasks:annotations_results",
                         "operator": "not_contains",
                         "type": "String",
@@ -270,11 +308,13 @@ def test_views_filters(filters, ids, business_client, project_id):
 
     project = Project.objects.get(pk=project_id)
 
-    task_id_1 = make_task({"data": {"text": 1}}, project).id
+    task_data_field_name = settings.DATA_UNDEFINED_NAME
+
+    task_id_1 = make_task({"data": {task_data_field_name: "some text1"}}, project).id
     make_annotation({"result": [{"1_first": True}]}, task_id_1)
     make_prediction({"result": [{"1_first": True}], "score": 1}, task_id_1)
 
-    task_id_2 = make_task({"data": {"text": 2}}, project).id
+    task_id_2 = make_task({"data": {task_data_field_name: "some text2"}}, project).id
     for _ in range(0, 2):
         make_annotation({"result": [{"2_second": True}], "was_cancelled": True}, task_id_2)
     for _ in range(0, 2):
@@ -283,7 +323,7 @@ def test_views_filters(filters, ids, business_client, project_id):
     task_ids = [0, task_id_1, task_id_2]
 
     for _ in range(0, 2):
-        task_id = make_task({"data": {"text": 1}}, project).id
+        task_id = make_task({"data": {task_data_field_name: "some text_"}}, project).id
         task_ids.append(task_id)
 
     for item in filters['items']:
