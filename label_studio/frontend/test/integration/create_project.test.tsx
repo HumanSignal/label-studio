d94543ba2e43a9ef805edea069eabe5122936f59
Nikita Skryabin <nr@fenelon.ru>
2021-03-24 16:31:25 +0300

    Test server runner WIP

--- label_studio/frontend/test/integration/create_project.test.tsx
+++ label_studio/frontend/test/integration/create_project.test.tsx
@@ -1,3 +1,15 @@
+import { runTestServer, Server } from "../setup/run_dev_server";
+
+let server: Server;
+
+beforeEach(async () => {
+  server = await runTestServer();
+});
+
+afterEach(async () => {
+  await server.shutdown();
+});
+
 describe("Minitmal", () => {
   it("should pass", () => {
     expect(true).toBe(true);
--- label_studio/frontend/test/setup/run_dev_server.ts
+++ label_studio/frontend/test/setup/run_dev_server.ts
@@ -1,9 +1,51 @@
-export const runTestServer = async () => {
-  const RUNNING = 'Starting development server at';
+import { spawn } from 'child_process';
+import mkdirp from 'mkdirp';
+import os from 'os';
+import path from 'path';
+import rimraf from 'rimraf';
 
-  return {
-    shutdown: async() => {
-      return true;
-    },
-  };
+const LS_ROOT = path.resolve(process.env.PWD!, '../..');
+const LOCAL_HOSTNAME = "localhost:9191";
+const SERVER_TMP = path.join(os.tmpdir(), 'test-server');
+
+export type Server = {
+  hostname: string,
+  shutdown: () => Promise<boolean>,
+}
+
+export const runTestServer = async () => {
+  return new Promise<Server>((resolve) => {
+    let isRunning = false;
+
+    mkdirp.sync(SERVER_TMP);
+
+    const RUNNING_MESSAGE = `Starting development server at http://${LOCAL_HOSTNAME}/`;
+    const [host, port] = LOCAL_HOSTNAME.split(":");
+    const cmd = `label-studio --host ${host} --port ${port} --data-dir ${SERVER_TMP}`;
+
+    const server = spawn(`cd ${LS_ROOT} && source ${path.join(LS_ROOT, 'venv/htx/bin/activate')} && ${cmd}`, {
+      shell: true,
+    });
+
+    const handler = (data: any) => {
+      console.log(data);
+      isRunning = data.toString().match(new RegExp(`${RUNNING_MESSAGE}`));
+
+      if (isRunning) {
+        resolve({
+          hostname: `http://${LOCAL_HOSTNAME}`,
+          shutdown: async() => {
+            rimraf.sync(SERVER_TMP);
+            return server.emit('exit');
+          },
+        });
+
+        server.stdout.off('data', handler);
+      }
+    };
+
+    server.stdout.on("data", handler);
+
+    server.on("error", (err) => console.log(err));
+  });
 };
