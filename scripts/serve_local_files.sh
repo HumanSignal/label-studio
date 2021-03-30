8d07aada6a54c83e1ffef54ea512e993f356ba0a
nik <nik@heartex.net>
2021-03-30 19:39:29 +0300

    Fix simple server script

--- scripts/serve_local_files.sh
+++ scripts/serve_local_files.sh
@@ -1,7 +1,7 @@
 INPUT_DIR=$1
 WILDCARD=${2}
 OUTPUT_FILE=${3:-"files.txt"}
-PORT=${3:-8000}
+PORT=${3:-8081}
 
 FIND_CMD="find ${INPUT_DIR} -type f"
 if [ -z "$WILDCARD" ]; then
@@ -10,8 +10,10 @@ else
   FIND_CMD="${FIND_CMD} -name ${WILDCARD}"
 fi
 
-eval $FIND_CMD | sed "s/\.\//http:\/\/localhost:${PORT}\//g" > $OUTPUT_FILE
+eval $FIND_CMD | sed 's!.*/!!' | sed -e "s/^/http:\/\/localhost:${PORT}\//" > $OUTPUT_FILE
 
-echo "File list stored in ${OUTPUT_FILE}..."
+green=`tput setaf 2`
+reset=`tput sgr0`
+echo "${green}File list stored in '''${OUTPUT_FILE}'''. Now import it directly from Label Studio UI${reset}"
 
 python -m http.server $PORT -d $INPUT_DIR
