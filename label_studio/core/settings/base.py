334354f20a38512bd6a95d1cadad9140dc67401b
nik <nik@heartex.net>
2021-04-01 16:03:22 +0300

    Fix STATIC_URL

--- label_studio/core/settings/base.py
+++ label_studio/core/settings/base.py
@@ -275,8 +275,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+STATIC_URL = '/static/'
 if HOSTNAME:
-    STATIC_URL = HOSTNAME + '/static/'
+    STATIC_URL = HOSTNAME + STATIC_URL
 print(f'Static URL is set to {STATIC_URL}')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_build')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
