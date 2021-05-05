7f021fe662a851f25436fce1d0681f76ab81d73b
Sergei Ivashchenko <triklozoid@gmail.com>
2021-05-05 19:03:50 +0300

    Remove Http basic authentication (#879)
    * Remove Http basic authentication
    
    * Add redirect when API respond with 401 header
    
    Co-authored-by: Nikita Skryabin <nr@fenelon.ru>

--- label_studio/core/settings/base.py
+++ label_studio/core/settings/base.py
@@ -170,9 +170,8 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication'
     ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated'
