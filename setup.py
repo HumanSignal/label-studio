c633672113d811213a8e4cb92654b7fdc5178b0e
makseq-msi3 <makseq@gmail.com>
2021-05-22 22:29:15 +0300

    Fix license type

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setuptools.setup(
     include_package_data=True,
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
     ],
     install_requires=requirements,
