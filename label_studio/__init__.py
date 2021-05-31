8d8a51a73921779c3a2a5cdbe4d65df121bdfe6b
DmBershov <DmBershov@dsi-global.net>
2021-05-31 15:30:29 +0300

    Changes: 1. Changed in users>init package name: package_name = "label-studio-withoutsignin"

--- label_studio/__init__.py
+++ label_studio/__init__.py
@@ -1,7 +1,7 @@
 """This file and its contents are licensed under the Apache License 2.0. Please see the included NOTICE for copyright information and LICENSE for a copy of the license.
 """
 # Package name
-package_name = "label-studio"
+package_name = "label-studio-withoutsignin"
 
 # Package version
 __version__ = "1.0.2"
