55b0ff858db927716e526fbcba37646a23304e5d
Adnen <saadaoui.adnen@gmail.com>
2021-06-03 14:02:16 -0700

    Fix docker-compose startup (#1004) (#1017)

--- deploy/docker-entrypoint.sh
+++ deploy/docker-entrypoint.sh
@@ -5,8 +5,8 @@ set -e
 # if wait-for-postgres script is specified, we sleep in the loop until Postgre is up
 if [[ "$1" = "./deploy/wait-for-postgres.sh" ]]; then
     # ./deploy/wait-for-postgres.sh db
-    $1 $2
-    shift 2
+    $1
+    shift 1
 fi
 
 echo "=> Do database migrations..."
