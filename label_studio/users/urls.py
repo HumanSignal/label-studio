4f2718ddb902c96cf426a5051f1424d4c7fab593
makseq-ubnt <makseq@gmail.com>
2021-06-11 21:00:44 +0300

    Remove rests

--- label_studio/users/urls.py
+++ label_studio/users/urls.py
@@ -21,12 +21,6 @@ urlpatterns = [
     path('user/account/', views.user_account, name='user-account'),
     url(r'^logout/?$', views.logout, name='logout'),
 
-    # Password reset
-    url(r'^password-reset/$', views.FPasswordResetView.as_view(), name='password_reset'),
-    url(r'^password-reset/done/$', views.FPasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('password-reset/<uidb64>/<token>/', views.FPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    url(r'^password-reset/complete/$', views.FPasswordResetCompleteView.as_view(), name='password_reset_complete'),
-
     # avatars
     re_path(r'^data/' + settings.AVATAR_PATH + '/(?P<path>.*)$', serve,
             kwargs={'document_root': join(settings.MEDIA_ROOT, settings.AVATAR_PATH)}),
