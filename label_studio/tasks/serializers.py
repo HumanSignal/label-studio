6bac188a1cd353c4695e7583b7d8d4cf6bc05871
makseq-ubnt <makseq@gmail.com>
2021-06-30 02:31:39 +0300

    Fix space before username in draft

--- label_studio/tasks/serializers.py
+++ label_studio/tasks/serializers.py
@@ -430,7 +430,7 @@ class AnnotationDraftSerializer(ModelSerializer):
         if len(user.last_name):
             name = name + " " + user.last_name
 
-        name += f' {user.email}, {user.id}'
+        name += (' ' if name else '') + f'{user.email}, {user.id}'
         return name
 
     class Meta:
