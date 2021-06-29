f0ef46e5eb5861b2e91c808c987fc0e53d68bfdd
Nikita Skryabin <nr@fenelon.ru>
2021-06-29 15:18:39 +0300

    Extended sentry tracking

--- label_studio/frontend/src/config/Sentry.ts
+++ label_studio/frontend/src/config/Sentry.ts
@@ -5,6 +5,10 @@ import { Integrations } from "@sentry/tracing";
 import { Route } from 'react-router-dom';
 
 export const initSentry = (history: RouterHistory) => {
+  if (process.env.NODE_ENV !== 'production') return;
+
+  setTags();
+
   Sentry.init({
     dsn: "https://5f51920ff82a4675a495870244869c6b@o227124.ingest.sentry.io/5838868",
     integrations: [
@@ -18,6 +22,36 @@ export const initSentry = (history: RouterHistory) => {
     // We recommend adjusting this value in production
     tracesSampleRate: process.env.NODE_ENV === 'production' ? 0.1 : 1.0,
   });
+
+};
+
+const setTags = () => {
+  const tags: Record<string, any> = {};
+
+  if (APP_SETTINGS.user.email) {
+    Sentry.setUser({
+      email: APP_SETTINGS.user.email,
+      username: APP_SETTINGS.user.username,
+    });
+  }
+
+  if (APP_SETTINGS.version) {
+    Object.entries(APP_SETTINGS.version).forEach(([packageName, data]: [string, any]) => {
+      const {version, commit} = data ?? {};
+      console.log({packageName, version, commit, data});
+
+      if (version) {
+        tags['version-' + packageName] = version;
+      }
+      if (commit) {
+        tags['commit-' + packageName] = commit;
+      }
+    });
+  }
+
+  console.log(tags);
+
+  Sentry.setTags(tags);
 };
 
 export const SentryRoute = ReactSentry.withSentryRouting(Route);
