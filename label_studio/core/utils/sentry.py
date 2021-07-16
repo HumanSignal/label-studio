f7be78ce33e1606c96acb632659b744f7c96f29c
makseq-ubnt <makseq@gmail.com>
2021-07-16 04:42:40 +0300

    Add sentry transations cleanup

--- label_studio/core/utils/sentry.py
+++ label_studio/core/utils/sentry.py
@@ -1,13 +1,26 @@
 from django.conf import *
 
 
+def event_processor(event, hint):
+    # static loading
+    if event.get("transaction") == "/react-app/{path}":
+        return None
+    # Http 404 erros
+    if event.get('exception', {}).get('values', [{}])[-1].get('type') == 'Http404':
+        return None
+
+    return event  # to return all other events
+
+
 def init_sentry(release_name, release_version):
     if settings.SENTRY_DSN:
         import sentry_sdk
         from sentry_sdk.integrations.django import DjangoIntegration
+        from sentry_sdk.scope import add_global_event_processor
         from core.utils.exceptions import (
             LabelStudioErrorSentryIgnored, LabelStudioAPIExceptionSentryIgnored,
-            LabelStudioValidationErrorSentryIgnored)
+            LabelStudioValidationErrorSentryIgnored
+        )
 
         if settings.SENTRY_REDIS_ENABLED:
             from sentry_sdk.integrations.redis import RedisIntegration
@@ -15,6 +28,9 @@ def init_sentry(release_name, release_version):
             advanced = [RedisIntegration(), RqIntegration()]
         else:
             advanced = []
+
+        # define the event processor, this runs before before_send if enabled
+        sentry_sdk.scope.add_global_event_processor(event_processor)
 
         sentry_sdk.init(
             dsn=settings.SENTRY_DSN,
