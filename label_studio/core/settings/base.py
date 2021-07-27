7aafa9792cffd056671b11b695441aa08ce9faca
Sergei Ivashchenko <triklozoid@gmail.com>
2021-07-27 13:15:55 +0300

    [fix] Add min task lock

--- label_studio/core/settings/base.py
+++ label_studio/core/settings/base.py
@@ -323,6 +323,7 @@ TASKS_MAX_FILE_SIZE = DATA_UPLOAD_MAX_MEMORY_SIZE
 
 TASK_LOCK_TTL = int(get_env('TASK_LOCK_TTL')) if get_env('TASK_LOCK_TTL') else None
 TASK_LOCK_DEFAULT_TTL = int(get_env('TASK_LOCK_DEFAULT_TTL', 3600))
+TASK_LOCK_MIN_TTL = int(get_env('TASK_LOCK_MIN_TTL', 120))
 
 # Email backend
 FROM_EMAIL = get_env('FROM_EMAIL', 'Label Studio <hello@labelstud.io>')
