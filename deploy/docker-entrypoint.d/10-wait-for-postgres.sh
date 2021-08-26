080b47eb3ef8d53387528eb7ba187b1888a38ab0
Sergey Zhuk <sergey.zhuk@heartex.com>
2021-08-26 17:10:47 +0400

    Copy data to static volume during docker-compose startup

--- deploy/docker-entrypoint.d/10-wait-for-postgres.sh
+++ deploy/docker-entrypoint.d/10-wait-for-postgres.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
-# wait-for-postgres.sh
-
 set -e
 
-
+echo >&3 "=> Waiting for postgres..."
 until PGPASSWORD=$POSTGRE_PASSWORD psql -h "$POSTGRE_HOST" -p $POSTGRE_PORT -U "$POSTGRE_USER" -c '\q'; do
-  >&2 echo "Postgres is unavailable - sleeping"
+  echo >&3 "==> Postgres is unavailable - sleeping..."
   sleep 1
 done
-
->&2 echo "Postgres is up - executing command"
+echo >&3 "=> Postgres is up."
