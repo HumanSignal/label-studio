49a269c71321a1f77cef672e2e941ff2638e690e
Sergei Ivashchenko <triklozoid@gmail.com>
2021-08-27 16:29:44 +0300

    [fix] Remove credentials from storages API

--- label_studio/io_storages/s3/serializers.py
+++ label_studio/io_storages/s3/serializers.py
@@ -17,6 +17,12 @@ class S3ImportStorageSerializer(ImportStorageSerializer):
         model = S3ImportStorage
         fields = '__all__'
 
+    def to_representation(self, instance):
+        result = super().to_representation(instance)
+        result.pop('aws_access_key_id')
+        result.pop('aws_secret_access_key')
+        return result
+
     def validate(self, data):
         data = super(S3ImportStorageSerializer, self).validate(data)
         if not data.get('bucket', None):
@@ -39,6 +45,12 @@ class S3ImportStorageSerializer(ImportStorageSerializer):
 
 class S3ExportStorageSerializer(ExportStorageSerializer):
     type = serializers.ReadOnlyField(default=os.path.basename(os.path.dirname(__file__)))
+
+    def to_representation(self, instance):
+        result = super().to_representation(instance)
+        result.pop('aws_access_key_id')
+        result.pop('aws_secret_access_key')
+        return result
 
     class Meta:
         model = S3ExportStorage
