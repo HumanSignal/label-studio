004cde721ec8b74ccc49b9aeb1042408b4039f9b
Sergey Zhuk <sergey.zhuk@heartex.com>
2021-08-30 19:51:59 +0400

    Skip wait for postgres and db migrations scripts in case of using sqlite (#1377)

--- deploy/docker-entrypoint.d/20-run-db-migrations.sh
+++ deploy/docker-entrypoint.d/20-run-db-migrations.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
 
-echo >&3 "=> Do database migrations..."
-python3 /label-studio/label_studio/manage.py migrate >&3
-echo >&3 "=> Migrations completed."
+if [ -n "${POSTGRE_HOST:-}" ]; then
+  echo >&3 "=> Do database migrations..."
+  python3 /label-studio/label_studio/manage.py migrate >&3
+  echo >&3 "=> Migrations completed."
+else
+  echo >&3 "=> Skipping run db migrations."
+fi
