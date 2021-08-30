62ed361b88385ce24747a414452c0bd0020cc0d7
Sergey Zhuk <sergey.zhuk@heartex.com>
2021-08-30 19:46:41 +0400

    Skip wait for postgres and db migrations scripts in case of using sqlite

--- deploy/docker-entrypoint.d/10-wait-for-postgres.sh
+++ deploy/docker-entrypoint.d/10-wait-for-postgres.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 set -e
 
-echo >&3 "=> Waiting for postgres..."
-until PGPASSWORD=$POSTGRE_PASSWORD psql -h "$POSTGRE_HOST" -p $POSTGRE_PORT -U "$POSTGRE_USER" -c '\q'; do
-  echo >&3 "==> Postgres is unavailable - sleeping..."
-  sleep 1
-done
-echo >&3 "=> Postgres is up."
+if [ -n "${POSTGRE_HOST:-}" ]; then
+  echo >&3 "=> Waiting for postgres..."
+  until PGPASSWORD=$POSTGRE_PASSWORD psql -h "$POSTGRE_HOST" -p $POSTGRE_PORT -U "$POSTGRE_USER" -c '\q'; do
+    echo >&3 "==> Postgres is unavailable - sleeping..."
+    sleep 1
+  done
+  echo >&3 "=> Postgres is up."
+else
+  echo >&3 "=> Skipping wait for postgres."
+fi
