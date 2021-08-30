b5a05da2410ac82f4938054931abd812b68a64f8
Sergey Zhuk <sergey.zhuk@heartex.com>
2021-08-30 18:55:07 +0400

    Fix useless 'cat' on app startup

--- deploy/docker-entrypoint.d/20-run-db-migrations.sh
+++ deploy/docker-entrypoint.d/20-run-db-migrations.sh
@@ -2,6 +2,5 @@
 set -e
 
 echo >&3 "=> Do database migrations..."
-cat /label-studio/label_studio/manage.py
 python3 /label-studio/label_studio/manage.py migrate >&3
 echo >&3 "=> Migrations completed."
