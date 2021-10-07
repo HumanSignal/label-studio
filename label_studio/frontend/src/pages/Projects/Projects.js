b57c9fab43dd875528433b275f9635c6bdfb50bd
Sergei Ivashchenko <triklozoid@gmail.com>
2021-10-07 10:41:52 +0300

    [ext] Projects api pagination (#1504)
    * Pagination component
    
    * [ext] Projects api pagination
    
    * Rewrite projects retrieving from api
    
    * Fix project creation
    
    * Pagination components for projects list
    
    * Add page_size query param to projects list api
    
    * Page size and url
    
    * Proper switching between page sizes
    
    Co-authored-by: Nick <nr@fenelon.ru>
    Co-authored-by: makseq-msi3 <makseq@gmail.com>
    Co-authored-by: nik <nik@heartex.net>

--- label_studio/frontend/src/pages/Projects/Projects.js
+++ label_studio/frontend/src/pages/Projects/Projects.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams as useRouterParams } from 'react-router';
 import { Redirect } from 'react-router-dom';
 import { Button } from '../../components';
@@ -7,28 +7,46 @@ import { Spinner } from '../../components/Spinner/Spinner';
 import { ApiContext } from '../../providers/ApiProvider';
 import { useContextProps } from '../../providers/RoutesProvider';
 import { Block, Elem } from '../../utils/bem';
+import { isDefined } from '../../utils/helpers';
 import { CreateProject } from '../CreateProject/CreateProject';
 import { DataManagerPage } from '../DataManager/DataManager';
 import { SettingsPage } from '../Settings';
 import './Projects.styl';
 import { EmptyProjectsList, ProjectsList } from './ProjectsList';
 
+const getCurrentPage = () => {
+  const pageNumberFromURL = new URLSearchParams(location.search).get("page");
+
+  return pageNumberFromURL ? parseInt(pageNumberFromURL) : 1;
+};
+
 export const ProjectsPage = () => {
   const api = React.useContext(ApiContext);
   const [projectsList, setProjectsList] = React.useState([]);
   const [networkState, setNetworkState] = React.useState(null);
+  const [currentPage, setCurrentPage] = useState(getCurrentPage());
+  const [totalItems, setTotalItems] = useState(1);
   const setContextProps = useContextProps();
+  const defaultPageSize = parseInt(localStorage.getItem('pages:projects-list') ?? 30);
 
   const [modal, setModal] = React.useState(false);
   const openModal = setModal.bind(null, true);
   const closeModal = setModal.bind(null, false);
 
-  const fetchProjects = async () => {
+  const fetchProjects = async (page  = currentPage, pageSize = defaultPageSize) => {
     setNetworkState('loading');
-    const projects = await api.callApi("projects");
+    const data = await api.callApi("projects", {
+      params: { page, page_size: pageSize },
+    });
 
-    setProjectsList(projects ?? []);
+    setTotalItems(data?.count ?? 1);
+    setProjectsList(data.results ?? []);
     setNetworkState('loaded');
+  };
+
+  const loadNextPage = async (page, pageSize) => {
+    setCurrentPage(page);
+    await fetchProjects(page, pageSize);
   };
 
   React.useEffect(() => {
@@ -48,10 +66,17 @@ export const ProjectsPage = () => {
           <Spinner size={64}/>
         </Elem>
         <Elem name="content" case="loaded">
-          {projectsList.length
-            ? <ProjectsList projects={projectsList}/>
-            : <EmptyProjectsList openModal={openModal} />
-          }
+          {projectsList.length ? (
+            <ProjectsList
+              projects={projectsList}
+              currentPage={currentPage}
+              totalItems={totalItems}
+              loadNextPage={loadNextPage}
+              pageSize={defaultPageSize}
+            />
+          ) : (
+            <EmptyProjectsList openModal={openModal} />
+          )}
           {modal && <CreateProject onClose={closeModal} />}
         </Elem>
       </Oneof>
@@ -62,13 +87,14 @@ export const ProjectsPage = () => {
 ProjectsPage.title = "Projects";
 ProjectsPage.path = "/projects";
 ProjectsPage.exact = true;
-ProjectsPage.routes = ({store}) => [
+ProjectsPage.routes = ({ store }) => [
   {
     title: () => store.project?.title,
     path: "/:id(\\d+)",
     exact: true,
     component: () => {
       const params = useRouterParams();
+
       return <Redirect to={`/projects/${params.id}/data`}/>;
     },
     pages: {
--- label_studio/projects/api.py
+++ label_studio/projects/api.py
@@ -18,6 +18,7 @@ from rest_framework.exceptions import NotFound, ValidationError as RestValidatio
 from rest_framework.parsers import FormParser, JSONParser, MultiPartParser
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.views import exception_handler
 
 from core.utils.common import conditional_atomic, temporary_disconnect_all_signals
@@ -86,6 +87,11 @@ _task_data_schema = openapi.Schema(
 )
 
 
+class ProjectListPagination(PageNumberPagination):
+    page_size = 30
+    page_size_query_param = 'page_size'
+
+
 @method_decorator(name='get', decorator=swagger_auto_schema(
     tags=['Projects'],
     operation_summary='List your projects',
@@ -121,6 +127,7 @@ class ProjectListAPI(generics.ListCreateAPIView):
         POST=all_permissions.projects_create,
     )
     ordering = ['-created_at']
+    pagination_class = ProjectListPagination
 
     def get_queryset(self):
         projects = Project.objects.filter(organization=self.request.user.active_organization)
