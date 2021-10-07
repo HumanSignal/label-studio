ec7d36ce7028e2ccf92d5374ddaf3db87bd3c758
smoreface <smoreface@users.noreply.github.com>
2021-10-06 17:04:54 -0700

    [fix] Add ID param to org API

--- label_studio/organizations/api.py
+++ label_studio/organizations/api.py
@@ -10,6 +10,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 from django.utils.decorators import method_decorator
 
 from label_studio.core.permissions import all_permissions, ViewClassPermission
@@ -62,7 +63,14 @@ class OrganizationListAPI(generics.ListCreateAPIView):
 @method_decorator(name='get', decorator=swagger_auto_schema(
         tags=['Organizations'],
         operation_summary='Get organization members list',
-        operation_description='Retrieve a list of the organization members and their IDs.'
+        operation_description='Retrieve a list of the organization members and their IDs.',
+        manual_parameters=[
+            openapi.Parameter(
+                name='id',
+                type=openapi.TYPE_INTEGER,
+                in_=openapi.IN_PATH,
+                description='Organization ID.'),
+        ],
     ))
 class OrganizationMemberListAPI(generics.ListAPIView):
 
