3e28c167765c5a4030fbd309955d4eb64baf1841
niklub <lubimov.nicolas@gmail.com>
2021-11-19 16:31:06 +0000

    Release 1.4 (#1725)
    * Make 1.4rc0
    
    * Update frontend (#1721)
    
    * Update frontend
    
    * Pull latest changes from LSF
    
    Co-authored-by: Nick <nr@fenelon.ru>
    
    * Update frontend scripts
    
    * Update LSF
    
    * Update frontend
    
    * Bump 1.4rc1
    
    * Temporary disable py3.9 tests
    
    * [fix] [dm] onlyVirtualTabs of undefined
    
    * [fix] db naming
    
    * [fix] Empty annotations and predictions in DM TaskSerializer (#1733)
    
    * [fix] Serializer with empty annotations and predictions
    
    * Update serializers.py
    
    * Fix tests
    
    * [fix] Update LSF build with ctrl+z and not working label hotkey (DEV-1121) (#1748)
    
    * Fix quickstart example (#1726)
    
    * Fix brush mask to rle description (#1728)
    
    * Docs for custom agreement metric for private cloud LSE (#1729)
    
    * Update to add perms notes
    
    * Finish updating custom metric doc for perms
    
    * fixing link
    
    * updating custom metric doc for review feedback
    
    - restructured create policy steps
    - added a bunch of links to aws docs for creating roles
    - clarified order and links for things
    
    * Configure ssl certs in nginx (#1707)
    
    * [docs] Usecase with blocked internet access (#1732)
    
    * [docs] Usecase with blocked internet access
    
    * update k8s install for proxy/airgapped internet
    
    added more links and keywords and clarity
    
    Co-authored-by: smoreface <smoreface@users.noreply.github.com>
    
    * Add docs how to enable tls on pgsql (#1737)
    
    * Update LSF build with ctrl+z and not working label hotkeys
    
    Co-authored-by: smoreface <smoreface@users.noreply.github.com>
    Co-authored-by: Sergey Zhuk <sergey.zhuk@heartex.com>
    
    * DEV-1154: Rotation fixes (#1756)
    
    * Update LSF
    
    * Update LSF to master
    
    Co-authored-by: 4iGAN <4iGAN.yO@gmail.com>
    
    * DEV-438: SDK tutorial (#1711)
    
    * Create sdk.md
    
    * Update sdk.md
    
    * Update sdk.md
    
    * Update sdk.md
    
    * Add some fixes
    
    * review feedback on sdk doc
    
    * Update docs header with SDK reference link
    
    * Add more prediction examples
    
    * Update sdk.md
    
    * Update header.ejs
    
    * fix imports in doc
    
    * fix links to jupyter notebooks
    
    * [docs] update filters header per review feedback
    
    Co-authored-by: Max Tkachenko <makseq@gmail.com>
    Co-authored-by: nik <nik@heartex.net>
    
    * DEV-819: Fix image size problems with appearance of scrollbar (#1758)
    
    * Make get-build works with long branch names
    
    (cherry picked from commit 4c8329e380abae71f80af3b557942222892d1e59)
    (cherry picked from commit 56d42dab2152b03d4fe4599169b9bb910c45de41)
    
    * [fix] [lsf] Add observing of image size for special cases
    
    * Update LSF
    
    Co-authored-by: makseq-ubnt <makseq@gmail.com>
    
    * Update DM from master commit
    
    * Update LS version
    
    * Update converter and LS version
    
    * Update converter to 0.0.36
    
    * Add verify flag to sqlite download
    
    * Fix --ml-backend option
    
    * [fix] fix code editor vertical scroll after #1669 (#1765)
    
    * Fix changing avatar permission (DEV-1183) (#1764)
    
    * Get only task ids via drf-flexfields (DEV-1102) (#1747)
    
    * Change version to rc6
    
    * Change version to 1.4
    
    Co-authored-by: nik <nik@heartex.net>
    Co-authored-by: Nick <nr@fenelon.ru>
    Co-authored-by: hlomzik <hlomzik@gmail.com>
    Co-authored-by: makseq-ubnt <makseq@gmail.com>
    Co-authored-by: smoreface <smoreface@users.noreply.github.com>
    Co-authored-by: Sergey Zhuk <sergey.zhuk@heartex.com>
    Co-authored-by: Nick Skriabin <767890+nicholasrq@users.noreply.github.com>
    Co-authored-by: 4iGAN <4iGAN.yO@gmail.com>
    Co-authored-by: Sergey <Gondragos@gmail.com>
    Co-authored-by: Sergei Ivashchenko <triklozoid@gmail.com>

--- label_studio/core/settings/base.py
+++ label_studio/core/settings/base.py
@@ -67,11 +67,12 @@ logger.info('=> Database and media directory: %s', BASE_DATA_DIR)
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 DJANGO_DB_MYSQL = 'mysql'
 DJANGO_DB_SQLITE = 'sqlite'
+DJANGO_DB_POSTGRESQL = 'postgresql'
 DJANGO_DB = 'default'
 DATABASE_NAME_DEFAULT = os.path.join(BASE_DATA_DIR, 'label_studio.sqlite3')
 DATABASE_NAME = get_env('DATABASE_NAME', DATABASE_NAME_DEFAULT)
 DATABASES_ALL = {
-    'default': {
+    DJANGO_DB_POSTGRESQL: {
         'ENGINE': 'django.db.backends.postgresql',
         'USER': get_env('POSTGRE_USER', 'postgres'),
         'PASSWORD': get_env('POSTGRE_PASSWORD', 'postgres'),
@@ -95,6 +96,7 @@ DATABASES_ALL = {
         },
     },
 }
+DATABASES_ALL['default'] = DATABASES_ALL[DJANGO_DB_POSTGRESQL]
 DATABASES = {'default': DATABASES_ALL.get(get_env('DJANGO_DB', 'default'))}
 
 LOGGING = {
@@ -448,3 +450,7 @@ import mimetypes
 
 mimetypes.add_type("application/javascript", ".js", True)
 mimetypes.add_type("image/png", ".png", True)
+
+
+# fields name was used in DM api before
+REST_FLEX_FIELDS = {"FIELDS_PARAM": "include"}
