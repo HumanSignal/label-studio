1c5002588f2fef2f342178a57ef03a8bc5cdeba1
KonstantinKorotaev <konstantin@heartex.com>
2021-12-01 16:51:46 +0300

    Update requirements for label_studio_tools

--- label_studio/data_export/serializers.py
+++ label_studio/data_export/serializers.py
@@ -1,7 +1,6 @@
 """This file and its contents are licensed under the Apache License 2.0. Please see the included NOTICE for copyright information and LICENSE for a copy of the license.
 """
 from django.conf import settings
-from postprocessing.video import extract_key_frames
 from rest_flex_fields import FlexFieldsModelSerializer
 from rest_framework import serializers
 
@@ -11,6 +10,7 @@ from tasks.models import Annotation, Task
 from tasks.serializers import AnnotationDraftSerializer, PredictionSerializer
 from users.models import User
 from users.serializers import UserSimpleSerializer
+from label_studio_tools.postprocessing.video import extract_key_frames
 
 from .models import Export
 
