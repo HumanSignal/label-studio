30f6ef6b1e36f47f0a4b6ed12759983cd766bceb
Max Tkachenko <makseq@gmail.com>
2021-12-15 04:38:45 +0300

    [fix] [os] Cloud storage support for app persistence (DEV-1313) (#1828)
    * [fix][os] S3 storage support for app persistence (DEV-1313)
    
    * Add upload perm checks
    
    * Add project check instead of file check
    
    Co-authored-by: Sergei Ivashchenko <triklozoid@gmail.com>

--- label_studio/core/settings/base.py
+++ label_studio/core/settings/base.py
@@ -401,8 +401,7 @@ ALLOW_ORGANIZATION_WEBHOOKS = get_bool_env('ALLOW_ORGANIZATION_WEBHOOKS', False)
 CONVERTER_DOWNLOAD_RESOURCES = get_bool_env('CONVERTER_DOWNLOAD_RESOURCES', True)
 EXPERIMENTAL_FEATURES = get_bool_env('EXPERIMENTAL_FEATURES', False)
 USE_ENFORCE_CSRF_CHECKS = get_bool_env('USE_ENFORCE_CSRF_CHECKS', True)  # False is for tests
-
-EXPERIMENTAL_FEATURES = get_bool_env('EXPERIMENTAL_FEATURES', default=False)
+CLOUD_FILE_STORAGE_ENABLED = False
 
 CREATE_ORGANIZATION = 'organizations.functions.create_organization'
 GET_OBJECT_WITH_CHECK_AND_LOG = 'core.utils.get_object.get_object_with_check_and_log'
