ced871e565af99df88a00383485da8f24d686d20
nik <nik@heartex.net>
2022-01-19 19:36:53 +0000

    Close up urls

--- label_studio/core/urls.py
+++ label_studio/core/urls.py
@@ -84,7 +84,7 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
     path('django-rq/', include('django_rq.urls')),
-    path('feature-flags/', views.feature_flags, name='feature_flags'),
+    # path('feature-flags/', views.feature_flags, name='feature_flags'),
     re_path(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 
