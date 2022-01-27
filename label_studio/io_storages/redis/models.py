72b77d56c45e168f02b15217bc3650691a67b561
nik <nik@heartex.net>
2022-01-27 12:30:02 +0000

    Fix LocalFiles and Redis

--- label_studio/io_storages/redis/models.py
+++ label_studio/io_storages/redis/models.py
@@ -4,12 +4,11 @@ import logging
 import redis
 import json
 
-from django.db import models, transaction
+from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from io_storages.base_models import ImportStorage, ImportStorageLink, ExportStorage, ExportStorageLink
-from io_storages.serializers import StorageAnnotationSerializer
 from tasks.models import Annotation
 
 logger = logging.getLogger(__name__)
