656a0ea7e109d73a3d85bd2adfb05cc31daa07a9
Konstantin Korotaev <42615530+KonstantinKorotaev@users.noreply.github.com>
2022-02-11 21:52:15 +0300

    fix: DEV-1694: Make update_tasks_states async (#2014)
    * fix: DEV-1694: Make update_tasks_states async
    
    * fix: DEV-1694: Disconnect signals and update state after
    
    * refactor: DEV-1694: Update summary in delete specific tasks
    
    * fix: DEV-1693: Add disconnect a list of signals and refactor signal delete annotations from S3
    
    * fix: DEV-1694: Move project summary recalculation to async call
    
    * fix: DEV-1694: Change delete_annotation_from_s3_storages signal to pre_delete
    
    * docs: DEV-1694: Fix example in temporary_disconnect_list_signal

--- label_studio/data_manager/actions/basic.py
+++ label_studio/data_manager/actions/basic.py
@@ -2,15 +2,17 @@
 """
 import logging
 
-from django.db.models import signals
 from datetime import datetime
+from django.db.models.signals import post_delete, pre_delete
 
 from core.permissions import AllPermissions
 from core.redis import start_job_async_or_sync
-from core.utils.common import temporary_disconnect_signal, temporary_disconnect_all_signals
+from core.utils.common import temporary_disconnect_list_signal
+from projects.models import Project
+
 from tasks.models import (
-    Annotation, Prediction, Task, update_is_labeled_after_removing_annotation,
-    bulk_update_stats_project_tasks
+    Annotation, Prediction, Task, bulk_update_stats_project_tasks, update_is_labeled_after_removing_annotation,
+    update_all_task_states_after_deleting_task, remove_data_columns, remove_project_summary_annotations
 )
 from webhooks.utils import emit_webhooks_for_instance
 from webhooks.models import WebhookAction
@@ -38,25 +40,36 @@ def delete_tasks(project, queryset, **kwargs):
     """
     tasks_ids = list(queryset.values('id'))
     count = len(tasks_ids)
+    tasks_ids_list = [task['id'] for task in tasks_ids]
+    # signals to switch off
+    signals = [
+        (post_delete, update_is_labeled_after_removing_annotation, Annotation),
+        (post_delete, update_all_task_states_after_deleting_task, Task),
+        (pre_delete, remove_data_columns, Task),
+        (pre_delete, remove_project_summary_annotations, Annotation)
+    ]
 
     # delete all project tasks
     if count == project.tasks.count():
-        with temporary_disconnect_all_signals():
+        with temporary_disconnect_list_signal(signals):
             queryset.delete()
-
         project.summary.reset()
-        project.update_tasks_states(
-            maximum_annotations_changed=False,
-            overlap_cohort_percentage_changed=False,
-            tasks_number_changed=True
-        )
 
     # delete only specific tasks
     else:
-        # this signal re-save the task back
-        with temporary_disconnect_signal(signals.post_delete, update_is_labeled_after_removing_annotation, Annotation):
+        # update project summary
+        start_job_async_or_sync(async_project_summary_recalculation, tasks_ids_list, project.id)
+
+        with temporary_disconnect_list_signal(signals):
             queryset.delete()
 
+    project.update_tasks_states(
+        maximum_annotations_changed=False,
+        overlap_cohort_percentage_changed=False,
+        tasks_number_changed=True
+    )
+
+    # emit webhooks for project
     emit_webhooks_for_instance(project.organization, project, WebhookAction.TASKS_DELETED, tasks_ids)
 
     # remove all tabs if there are no tasks in project
@@ -101,6 +114,13 @@ def delete_tasks_predictions(project, queryset, **kwargs):
     count = predictions.count()
     predictions.delete()
     return {'processed_items': count, 'detail': 'Deleted ' + str(count) + ' predictions'}
+
+
+def async_project_summary_recalculation(tasks_ids_list, project_id):
+    queryset = Task.objects.filter(id__in=tasks_ids_list)
+    project = Project.objects.get(id=project_id)
+    project.summary.remove_created_annotations_and_labels(Annotation.objects.filter(task__in=queryset))
+    project.summary.remove_data_columns(queryset)
 
 
 actions = [
