fe5ca7eb89e0e561e5a4a26138f33bcbc91d5b9c
makseq-ubnt <makseq@gmail.com>
2022-02-17 02:22:40 +0300

    Fix path to feature flags file

--- label_studio/core/feature_flags/base.py
+++ label_studio/core/feature_flags/base.py
@@ -17,9 +17,13 @@ if settings.FEATURE_FLAGS_FROM_FILE:
     if not settings.FEATURE_FLAGS_FILE:
         raise ValueError('When "FEATURE_FLAGS_FROM_FILE" is set, you have to specify a valid path for feature flags file, e.g.'
                          'FEATURE_FLAGS_FILE=my_flags.yml')
-    
+
     package_name = 'label_studio' if settings.VERSION_EDITION == 'Community Edition' else 'label_studio_enterprise'
-    feature_flags_file = find_node(package_name, settings.FEATURE_FLAGS_FILE, 'file')
+    if settings.FEATURE_FLAGS_FILE.startswith('/'):
+        feature_flags_file = settings.FEATURE_FLAGS_FILE
+    else:
+        feature_flags_file = find_node(package_name, settings.FEATURE_FLAGS_FILE, 'file')
+
     logger.info(f'Read flags from file {feature_flags_file}')
     data_source = Files.new_data_source(paths=[feature_flags_file])
     config = Config(
