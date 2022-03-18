25dc2a00a5d04328c54bac4060e5a9e1e743eb43
makseq-ubnt <makseq@gmail.com>
2022-03-18 03:09:25 +0300

    Fix pagination class

--- label_studio/core/settings/base.py
+++ label_studio/core/settings/base.py
@@ -221,6 +221,7 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': ('rest_framework.renderers.JSONRenderer',),
     'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.NamespaceVersioning',
     'PAGE_SIZE': 100,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination'
 }
 
 # CORS & Host settings
