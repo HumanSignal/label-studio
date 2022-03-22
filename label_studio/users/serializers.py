3022e098ebaa58705c266e4c94b668ade3fe83c7
Max Tkachenko <makseq@gmail.com>
2022-03-22 03:45:34 +0300

    Update serializers.py

--- label_studio/users/serializers.py
+++ label_studio/users/serializers.py
@@ -21,7 +21,7 @@ class BaseUserSerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
         """ Returns user with cache, this helps to avoid multiple s3/gcs links resolving for avatars """
 
-        uid = instance.pk
+        uid = instance.id
         key = 'user_cache'
 
         if key not in self.context:
