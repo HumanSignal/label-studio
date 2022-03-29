62b5eea3b33e44aad8bea289bffa7c022008fcb2
Konstantin Korotaev <42615530+KonstantinKorotaev@users.noreply.github.com>
2022-03-29 20:55:23 +0300

    Revert "Change pagenition"
    This reverts commit 1a3f7f3aedeef53f926f9e59a07440198e7d45ab.

--- label_studio/data_manager/api.py
+++ label_studio/data_manager/api.py
@@ -193,8 +193,6 @@ class TaskListAPI(generics.ListCreateAPIView):
         # get project
         view_pk = int_from_request(request.GET, 'view', 0) or int_from_request(request.data, 'view', 0)
         project_pk = int_from_request(request.GET, 'project', 0) or int_from_request(request.data, 'project', 0)
-        page_number = int_from_request(request.GET, 'page', 0)
-        page_size = int_from_request(request.GET, 'page_size', 0)
         if project_pk:
             project = get_object_with_check_and_log(request, Project, pk=project_pk)
             self.check_object_permissions(request, project)
@@ -204,25 +202,23 @@ class TaskListAPI(generics.ListCreateAPIView):
             self.check_object_permissions(request, project)
         else:
             return Response({'detail': 'Neither project nor view id specified'}, status=404)
+
         # get prepare params (from view or from payload directly)
         prepare_params = get_prepare_params(request, project)
         queryset = self.get_task_queryset(request, prepare_params)
         context = self.get_task_serializer_context(self.request, project)
-        # get counters 
-        total_predictions = queryset.aggregate(Count('predictions'))['predictions__count']
-        total_annotations = queryset.aggregate(Sum('total_annotations'))['total_annotations__sum']
-        total_tasks = queryset.count()
+
         # paginated tasks
-        if page_number:
-            page = queryset[page_size*(page_number-1):page_size*page_number]
-        else:
-            page = queryset
+        self.pagination_class = TaskPagination
+        page = self.paginate_queryset(queryset)
         all_fields = 'all' if request.GET.get('fields', None) == 'all' else None
         fields_for_evaluation = get_fields_for_evaluation(prepare_params, request.user)
+
         review = bool_from_request(self.request.GET, 'review', False)
         if review:
             fields_for_evaluation = ['annotators', 'reviewed']
             all_fields = None
+
         if page is not None:
             ids = [task.id for task in page]  # page is a list already
             tasks = list(
@@ -235,6 +231,7 @@ class TaskListAPI(generics.ListCreateAPIView):
                 )
             )
             tasks_by_ids = {task.id: task for task in tasks}
+
             # keep ids ordering
             page = [tasks_by_ids[_id] for _id in ids]
 
@@ -244,12 +241,8 @@ class TaskListAPI(generics.ListCreateAPIView):
                 evaluate_predictions(tasks_for_predictions)
 
             serializer = self.task_serializer_class(page, many=True, context=context)
-            return Response({
-                "total_annotations": total_annotations,
-                "total_predictions": total_predictions,
-                "total": total_tasks,
-                "tasks": serializer.data,
-            })
+            return self.get_paginated_response(serializer.data)
+
         # all tasks
         if project.evaluate_predictions_automatically:
             evaluate_predictions(queryset.filter(predictions__isnull=True))
