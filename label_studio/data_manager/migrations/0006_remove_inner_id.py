ef9864704c68956cf6b174d55ed99d97ce8d4f00
Konstantin Korotaev <42615530+KonstantinKorotaev@users.noreply.github.com>
2022-04-12 20:36:07 +0300

    Remove feature flags from migrations

--- label_studio/data_manager/migrations/0006_remove_inner_id.py
+++ label_studio/data_manager/migrations/0006_remove_inner_id.py
@@ -1,43 +1,38 @@
 """This file and its contents are licensed under the Apache License 2.0. Please see the included NOTICE for copyright information and LICENSE for a copy of the license.
 """
 from django.db import migrations
-from core.feature_flags import flag_set
-from django.contrib.auth.models import AnonymousUser
+
 
 def remove(apps, schema_editor):
-    user = AnonymousUser()
-    if flag_set('ff_back_2070_inner_id_12052022_short', user):
-        View = apps.get_model('data_manager', 'View')
-        views = View.objects.all()
+    View = apps.get_model('data_manager', 'View')
+    views = View.objects.all()
 
-        for view in views:
-            if 'hiddenColumns' in view.data:
-                if 'explore' in view.data['hiddenColumns']:
-                    view.data['hiddenColumns']['explore'].append('tasks:inner_id')
-                    view.data['hiddenColumns']['explore'] = list(set(view.data['hiddenColumns']['explore']))
-                if 'labeling' in view.data['hiddenColumns']:
-                    view.data['hiddenColumns']['labeling'].append('tasks:inner_id')
-                    view.data['hiddenColumns']['labeling'] = list(set(view.data['hiddenColumns']['labeling']))
+    for view in views:
+        if 'hiddenColumns' in view.data:
+            if 'explore' in view.data['hiddenColumns']:
+                view.data['hiddenColumns']['explore'].append('tasks:inner_id')
+                view.data['hiddenColumns']['explore'] = list(set(view.data['hiddenColumns']['explore']))
+            if 'labeling' in view.data['hiddenColumns']:
+                view.data['hiddenColumns']['labeling'].append('tasks:inner_id')
+                view.data['hiddenColumns']['labeling'] = list(set(view.data['hiddenColumns']['labeling']))
 
-            view.save()
+        view.save()
 
 
 def backwards(apps, schema_editor):
-    user = AnonymousUser()
-    if flag_set('ff_back_2070_inner_id_12052022_short', user):
-        View = apps.get_model('data_manager', 'View')
-        views = View.objects.all()
+    View = apps.get_model('data_manager', 'View')
+    views = View.objects.all()
 
-        for view in views:
-            if 'hiddenColumns' in view.data:
-                if 'explore' in view.data['hiddenColumns']:
-                    view.data['hiddenColumns']['explore'].remove('tasks:inner_id')
-                    view.data['hiddenColumns']['explore'] = list(set(view.data['hiddenColumns']['explore']))
-                if 'labeling' in view.data['hiddenColumns']:
-                    view.data['hiddenColumns']['labeling'].remove('tasks:inner_id')
-                    view.data['hiddenColumns']['labeling'] = list(set(view.data['hiddenColumns']['labeling']))
+    for view in views:
+        if 'hiddenColumns' in view.data:
+            if 'explore' in view.data['hiddenColumns']:
+                view.data['hiddenColumns']['explore'].remove('tasks:inner_id')
+                view.data['hiddenColumns']['explore'] = list(set(view.data['hiddenColumns']['explore']))
+            if 'labeling' in view.data['hiddenColumns']:
+                view.data['hiddenColumns']['labeling'].remove('tasks:inner_id')
+                view.data['hiddenColumns']['labeling'] = list(set(view.data['hiddenColumns']['labeling']))
 
-            view.save()
+        view.save()
 
 
 class Migration(migrations.Migration):
--- label_studio/tasks/migrations/0015_task_fill_inner_id.py
+++ label_studio/tasks/migrations/0015_task_fill_inner_id.py
@@ -1,31 +1,27 @@
 """This file and its contents are licensed under the Apache License 2.0. Please see the included NOTICE for copyright information and LICENSE for a copy of the license.
 """
 from django.db import migrations
-from django.db.models import F, Window, Subquery, OuterRef
+from django.db.models import F, Window
 from django.db.models.functions import RowNumber
-from django.contrib.auth.models import AnonymousUser
 
 from core.bulk_update_utils import bulk_update
-from core.feature_flags import flag_set
 
 
 def remove(apps, schema_editor):
-    user = AnonymousUser()
-    if flag_set('ff_back_2070_inner_id_12052022_short', user):
-        Project = apps.get_model('projects', 'Project')
-        projects = Project.objects.all()
-        for project in projects:
-            project_tasks = project.tasks.all()
-            if project_tasks:
-                obj = []
-                results = project_tasks.annotate(row_number=Window(
-                    expression=RowNumber(),
-                    partition_by=[('project')],
-                    order_by=F('id').asc()))
-                for task in results:
-                    task.inner_id = task.row_number
-                    obj.append(task)
-                bulk_update(obj, batch_size=1000)
+    Project = apps.get_model('projects', 'Project')
+    projects = Project.objects.all()
+    for project in projects:
+        project_tasks = project.tasks.all()
+        if project_tasks:
+            obj = []
+            results = project_tasks.annotate(row_number=Window(
+                expression=RowNumber(),
+                partition_by=[('project')],
+                order_by=F('id').asc()))
+            for task in results:
+                task.inner_id = task.row_number
+                obj.append(task)
+            bulk_update(obj, batch_size=1000)
 
 
 def backwards(apps, schema_editor):
