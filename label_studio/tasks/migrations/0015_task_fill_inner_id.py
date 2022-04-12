20d9544ca28b0a95d2483a90b384740b15510752
makseq-ubnt <makseq@gmail.com>
2022-04-13 01:16:48 +0300

    Fixes for s3

--- label_studio/tasks/migrations/0015_task_fill_inner_id.py
+++ label_studio/tasks/migrations/0015_task_fill_inner_id.py
@@ -7,13 +7,17 @@ from django.db.models import F, Window
 from django.db.models.functions import RowNumber
 from django.db.utils import NotSupportedError
 from core.bulk_update_utils import bulk_update
-
+from core.feature_flags import flag_set
+from django.contrib.auth.models import AnonymousUser
 
 logger = logging.getLogger(__name__)
 
 
 def remove(apps, schema_editor):
-    Project = apps.get_model('projects', 'Project')
+    # if not flag_set('ff_back_2070_inner_id_12052022_short', user=AnonymousUser()):
+    return  # we don't want to apply this migration to all projects
+
+    """ Project = apps.get_model('projects', 'Project')
     projects = Project.objects.all()
     for project in projects:
         logger.info(f'Evaluate inner id for {project}')
@@ -23,7 +27,7 @@ def remove(apps, schema_editor):
             try:
                 results = project_tasks.annotate(row_number=Window(
                     expression=RowNumber(),
-                    partition_by=[('project')],
+                    partition_by=['project'],
                     order_by=F('id').asc()))
                 for task in results:
                     task.inner_id = task.row_number
@@ -33,7 +37,7 @@ def remove(apps, schema_editor):
             # not window is not supported
             except NotSupportedError:
                 first_id = project_tasks.order_by('id').first().id
-                project_tasks.update(inner_id=F('id') - first_id)
+                project_tasks.update(inner_id=F('id') - first_id)"""
 
 
 def backwards(apps, schema_editor):
