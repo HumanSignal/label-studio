2e2ff4259f9f3013ea3f9dc3b147ab1c10e57602
makseq-ubnt <makseq@gmail.com>
2022-04-12 21:46:31 +0300

    Add support for sqlite

--- label_studio/tasks/migrations/0015_task_fill_inner_id.py
+++ label_studio/tasks/migrations/0015_task_fill_inner_id.py
@@ -1,31 +1,44 @@
 """This file and its contents are licensed under the Apache License 2.0. Please see the included NOTICE for copyright information and LICENSE for a copy of the license.
 """
+import logging
+
 from django.db import migrations
 from django.db.models import F, Window
 from django.db.models.functions import RowNumber
-
+from django.db.utils import NotSupportedError
 from core.bulk_update_utils import bulk_update
+
+
+logger = logging.getLogger(__name__)
 
 
 def remove(apps, schema_editor):
     Project = apps.get_model('projects', 'Project')
     projects = Project.objects.all()
     for project in projects:
+        logger.info(f'Evaluate inner id for {project}')
         project_tasks = project.tasks.all()
-        if project_tasks:
+        if project_tasks.exists():
             obj = []
-            results = project_tasks.annotate(row_number=Window(
-                expression=RowNumber(),
-                partition_by=[('project')],
-                order_by=F('id').asc()))
-            for task in results:
-                task.inner_id = task.row_number
-                obj.append(task)
-            bulk_update(obj, batch_size=1000)
+            try:
+                results = project_tasks.annotate(row_number=Window(
+                    expression=RowNumber(),
+                    partition_by=[('project')],
+                    order_by=F('id').asc()))
+                for task in results:
+                    task.inner_id = task.row_number
+                    obj.append(task)
+                bulk_update(obj, batch_size=1000)
+
+            # not window is not supported
+            except NotSupportedError:
+                first_id = project_tasks.order_by('id').first().id
+                project_tasks.update(inner_id=F('id') - first_id)
 
 
 def backwards(apps, schema_editor):
     pass
+
 
 class Migration(migrations.Migration):
     dependencies = [
