82b429fbd36fa95afa905d9d7753ef5606aaa45a
Konstantin Korotaev <42615530+KonstantinKorotaev@users.noreply.github.com>
2022-04-19 20:58:21 +0300

    fix: DEV-2173: Dynamic labels with value="options" as list don't pass backend validation (#2249)
    * fix: DEV-2173: Dynamic labels with value="options" as list don't pass backend validation
    
    * Add tests
    
    Co-authored-by: Max Tkachenko <makseq@gmail.com>

--- label_studio/tasks/validation.py
+++ label_studio/tasks/validation.py
@@ -24,8 +24,16 @@ _DATA_TYPES = {
     'TimeSeriesChannel': [dict, list, str],
     'List': [list],
     'Choices': [str, list],
-    'Taxonomy': [list],
-    'PolygonLabels': [list]
+    'PolygonLabels': [str, list],
+    'Labels': [str, list],
+    'BrushLabels': [str, list],
+    'EllipseLabels': [str, list],
+    'HyperTextLabels': [str, list],
+    'KeyPointLabels': [str, list],
+    'ParagraphLabels': [str, list],
+    'RectangleLabels': [str, list],
+    'TimeSeriesLabels': [str, list],
+    'Taxonomy': [str, list],
 }
 logger = logging.getLogger(__name__)
 
