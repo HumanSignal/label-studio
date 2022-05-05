0f7a573f162da926498ad14c158c1caa6d37aeca
Konstantin Korotaev <42615530+KonstantinKorotaev@users.noreply.github.com>
2022-05-05 16:38:30 +0300

    fix: DEV-2296: Fix tasks deletion speed

---
+++ label_studio/tasks/migrations/0016_task_alter_foreign_key_actions.py
@@ -0,0 +1,41 @@
+import logging
+
+from django.db import migrations
+from django.contrib.postgres.operations import TrigramExtension
+
+logger = logging.getLogger(__name__)
+
+
+def forwards(apps, schema_editor):
+    if not schema_editor.connection.vendor.startswith('postgres'):
+        logger.info('Database vendor: {}'.format(schema_editor.connection.vendor))
+        logger.info('Skipping migration without attempting to CREATE INDEX')
+        return
+    changes = ['ALTER TABLE "prediction" DROP CONSTRAINT "prediction_task_id_fkey", ADD FOREIGN KEY ("task_id") REFERENCES "task" ("id") ON DELETE CASCADE ON UPDATE NO ACTION',
+               'ALTER TABLE "task_completion" DROP CONSTRAINT "task_completion_task_id_fkey", ADD FOREIGN KEY ("task_id") REFERENCES "task" ("id") ON DELETE CASCADE ON UPDATE NO ACTION']
+    for change in changes:
+        schema_editor.execute(change)
+
+
+def backwards(apps, schema_editor):
+    if not schema_editor.connection.vendor.startswith('postgres'):
+        logger.info('Database vendor: {}'.format(schema_editor.connection.vendor))
+        logger.info('Skipping migration without attempting to DROP INDEX')
+        return
+
+    changes = [
+        'ALTER TABLE "prediction" DROP CONSTRAINT "prediction_task_id_fkey", ADD FOREIGN KEY ("task_id") REFERENCES "task" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION',
+        'ALTER TABLE "task_completion" DROP CONSTRAINT "task_completion_task_id_fkey", ADD FOREIGN KEY ("task_id") REFERENCES "task" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION']
+    for change in changes:
+        schema_editor.execute(change)
+
+
+class Migration(migrations.Migration):
+    atomic = False
+
+    dependencies = [('tasks', '0015_task_fill_inner_id')]
+
+    operations = [
+        TrigramExtension(),
+        migrations.RunPython(forwards, backwards),
+    ]
