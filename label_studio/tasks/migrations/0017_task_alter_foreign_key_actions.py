51226f23485233942f82788db108c4e5aba82663
Konstantin Korotaev <42615530+KonstantinKorotaev@users.noreply.github.com>
2022-05-05 19:31:00 +0300

    Update 0017_task_alter_foreign_key_actions.py

--- label_studio/tasks/migrations/0017_task_alter_foreign_key_actions.py
+++ label_studio/tasks/migrations/0017_task_alter_foreign_key_actions.py
@@ -11,8 +11,10 @@ def forwards(apps, schema_editor):
         logger.info('Database vendor: {}'.format(schema_editor.connection.vendor))
         logger.info('Skipping migration without attempting to CREATE INDEX')
         return
-    changes = ['ALTER TABLE "prediction" DROP CONSTRAINT "prediction_task_id_fkey", ADD FOREIGN KEY ("task_id") REFERENCES "task" ("id") ON DELETE CASCADE ON UPDATE NO ACTION',
-               'ALTER TABLE "task_completion" DROP CONSTRAINT "task_completion_task_id_fkey", ADD FOREIGN KEY ("task_id") REFERENCES "task" ("id") ON DELETE CASCADE ON UPDATE NO ACTION']
+    prediction_con = 'SELECT con.conname FROM pg_catalog.pg_constraint con INNER JOIN pg_catalog.pg_class rel ON rel.oid = con.conrelid INNER JOIN pg_catalog.pg_namespace nsp ON nsp.oid = connamespace WHERE conname like "prediction_task_id%"'
+    task_completion_con = 'SELECT con.conname FROM pg_catalog.pg_constraint con INNER JOIN pg_catalog.pg_class rel ON rel.oid = con.conrelid INNER JOIN pg_catalog.pg_namespace nsp ON nsp.oid = connamespace WHERE conname like "task_completion_task_id%"'
+    changes = [f'ALTER TABLE "prediction" DROP CONSTRAINT {prediction_con}, ADD FOREIGN KEY ("task_id") REFERENCES "task" ("id") ON DELETE CASCADE ON UPDATE NO ACTION',
+               f'ALTER TABLE "task_completion" DROP CONSTRAINT {task_completion_con}, ADD FOREIGN KEY ("task_id") REFERENCES "task" ("id") ON DELETE CASCADE ON UPDATE NO ACTION']
     for change in changes:
         schema_editor.execute(change)
 
@@ -22,10 +24,12 @@ def backwards(apps, schema_editor):
         logger.info('Database vendor: {}'.format(schema_editor.connection.vendor))
         logger.info('Skipping migration without attempting to DROP INDEX')
         return
+    prediction_con = 'SELECT con.conname FROM pg_catalog.pg_constraint con INNER JOIN pg_catalog.pg_class rel ON rel.oid = con.conrelid INNER JOIN pg_catalog.pg_namespace nsp ON nsp.oid = connamespace WHERE conname like "prediction_task_id%"'
+    task_completion_con = 'SELECT con.conname FROM pg_catalog.pg_constraint con INNER JOIN pg_catalog.pg_class rel ON rel.oid = con.conrelid INNER JOIN pg_catalog.pg_namespace nsp ON nsp.oid = connamespace WHERE conname like "task_completion_task_id%"'
 
     changes = [
-        'ALTER TABLE "prediction" DROP CONSTRAINT "prediction_task_id_fkey", ADD FOREIGN KEY ("task_id") REFERENCES "task" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION',
-        'ALTER TABLE "task_completion" DROP CONSTRAINT "task_completion_task_id_fkey", ADD FOREIGN KEY ("task_id") REFERENCES "task" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION']
+        f'ALTER TABLE "prediction" DROP CONSTRAINT {prediction_con}, ADD FOREIGN KEY ("task_id") REFERENCES "task" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION',
+        f'ALTER TABLE "task_completion" DROP CONSTRAINT {task_completion_con}, ADD FOREIGN KEY ("task_id") REFERENCES "task" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION']
     for change in changes:
         schema_editor.execute(change)
 
