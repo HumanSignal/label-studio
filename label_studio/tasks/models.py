69c3d4cbae53a18d2c9a2cdfe810c24715dda45a
Nikita Belonogov <nikita.belonogov@heartex.com>
2022-05-18 12:00:05 +0400

    fix: DEV-2406: Fix counters for skipped annotations (#2366)
    * fix: DEV-2406: Fix counters for skipped annotations
    
    * Fixes
    
    Co-authored-by: makseq-ubnt <makseq@gmail.com>
    
    Co-authored-by: Konstantin Korotaev <42615530+KonstantinKorotaev@users.noreply.github.com>
    Co-authored-by: makseq-ubnt <makseq@gmail.com>

--- label_studio/tasks/models.py
+++ label_studio/tasks/models.py
@@ -589,6 +589,23 @@ def delete_project_summary_annotations_before_updating_annotation(sender, instan
         return
     old_annotation.decrease_project_summary_counters()
 
+    # update task counters if annotation changes it's was_cancelled status
+    task = instance.task
+    if old_annotation.was_cancelled != instance.was_cancelled:
+        if instance.was_cancelled:
+            task.cancelled_annotations = task.cancelled_annotations + 1
+            task.total_annotations = task.total_annotations - 1
+        else:
+            task.cancelled_annotations = task.cancelled_annotations - 1
+            task.total_annotations = task.total_annotations + 1
+        task.update_is_labeled()
+
+        Task.objects.filter(id=instance.task.id).update(
+            is_labeled=task.is_labeled,
+            total_annotations=task.total_annotations,
+            cancelled_annotations=task.cancelled_annotations
+        )
+
 
 @receiver(post_save, sender=Annotation)
 def update_project_summary_annotations_and_is_labeled(sender, instance, created, **kwargs):
