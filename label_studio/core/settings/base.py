1b417b741c3817226caf287811e0362217deeda6
David Wright <191410+dvwright@users.noreply.github.com>
2022-05-18 02:17:59 -0700

    fix: DEV-2213: Refactor and Optimize Redis Queues (#2264)
    * explore redis performance
    
    * rename queues
    
    * queue to default
    
    * ignore files
    
    * dont ignore config
    
    * try figure out failing tests...
    
    * bandit test failing even on develop branch...

--- label_studio/core/settings/base.py
+++ label_studio/core/settings/base.py
@@ -280,7 +280,25 @@ TEMPLATES = [
 
 # RQ
 RQ_QUEUES = {
+    'critical': {
+        'HOST': 'localhost',
+        'PORT': 6379,
+        'DB': 0,
+        'DEFAULT_TIMEOUT': 180,
+    },
+    'high': {
+        'HOST': 'localhost',
+        'PORT': 6379,
+        'DB': 0,
+        'DEFAULT_TIMEOUT': 180,
+    },
     'default': {
+        'HOST': 'localhost',
+        'PORT': 6379,
+        'DB': 0,
+        'DEFAULT_TIMEOUT': 180,
+    },
+    'low': {
         'HOST': 'localhost',
         'PORT': 6379,
         'DB': 0,
