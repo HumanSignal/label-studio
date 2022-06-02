0e254cf052c3ea28a4d4d1fa161c7106e0224dbd
Konstantin Korotaev <42615530+KonstantinKorotaev@users.noreply.github.com>
2022-06-02 16:23:09 +0300

    Add from_scratch param

--- label_studio/tasks/management/commands/calculate_stats_all_orgs.py
+++ label_studio/tasks/management/commands/calculate_stats_all_orgs.py
@@ -4,7 +4,10 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     help = 'Recalculate project stats (total_annotations, etc) for all organizations'
 
+    def add_arguments(self, parser):
+        parser.add_argument('from_scratch', type=bool, help='Start recalculation from scratch')
+
     def handle(self, *args, **options):
         from tasks.functions import calculate_stats_all_orgs
 
-        calculate_stats_all_orgs(from_scratch=False)  # @Konstantine: please add command line param for from_scratch
+        calculate_stats_all_orgs(from_scratch=options['from_scratch']) 
