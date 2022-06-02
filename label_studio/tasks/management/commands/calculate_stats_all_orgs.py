1a5e0ff20b44279be3141702aa3a1c5abdb2a20e
Konstantin Korotaev <42615530+KonstantinKorotaev@users.noreply.github.com>
2022-06-02 16:39:12 +0300

    Update calculate_stats_all_orgs.py

--- label_studio/tasks/management/commands/calculate_stats_all_orgs.py
+++ label_studio/tasks/management/commands/calculate_stats_all_orgs.py
@@ -5,9 +5,12 @@ class Command(BaseCommand):
     help = 'Recalculate project stats (total_annotations, etc) for all organizations'
 
     def add_arguments(self, parser):
-        parser.add_argument('from_scratch', type=bool, help='Start recalculation from scratch')
+        parser.add_argument('from_scratch',
+                            type=int,
+                            help='Start recalculation from scratch, 0 for continuation',
+                            default=0)
 
     def handle(self, *args, **options):
         from tasks.functions import calculate_stats_all_orgs
 
-        calculate_stats_all_orgs(from_scratch=options['from_scratch']) 
+        calculate_stats_all_orgs(from_scratch=options['from_scratch'])
