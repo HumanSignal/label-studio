d7051ed60fc2077030044d65e5509e15db92df06
Konstantin Korotaev <42615530+KonstantinKorotaev@users.noreply.github.com>
2022-06-10 15:27:43 +0300

    fix: DEV-2589: Move calculate_stats_all_orgs to rq_workers

--- label_studio/tasks/functions.py
+++ label_studio/tasks/functions.py
@@ -1,5 +1,6 @@
 import logging
 
+from core.redis import start_job_async_or_sync
 from organizations.models import Organization
 from projects.models import Project
 
@@ -12,16 +13,25 @@ def calculate_stats_all_orgs(from_scratch):
     for org in orgs:
         logger.debug(f"Start recalculating stats for Organization {org.id}.")
         projects = Project.objects.filter(organization=org).order_by('-updated_at')
-
-        for project in projects:
-            logger.debug(f"Start processing stats project {project.id} "
-                         f"with task count {project.tasks.count()} and updated_at {project.updated_at}")
-
-            tasks = project.update_tasks_counters(project.tasks.all(), from_scratch=from_scratch)
-
-            logger.debug(f"End processing stats project {project.id}."
-                         f"Processed {str(tasks)} tasks.")
+        # start async calculation job on redis
+        start_job_async_or_sync(redis_job_for_calculation, projects, from_scratch)
 
         logger.debug(f"Organization {org.id} stats were recalculated.")
 
     logger.debug("All organizations were recalculated.")
+
+
+def redis_job_for_calculation(projects, from_scratch):
+    """
+    Recalculate counters for projects list
+    :param projects: List of projects
+    :param from_scratch: Start calculation from scratch or skip calculated tasks
+    """
+    for project in projects:
+        logger.debug(f"Start processing stats project {project.id} "
+                     f"with task count {project.tasks.count()} and updated_at {project.updated_at}")
+
+        tasks = project.update_tasks_counters(project.tasks.all(), from_scratch=from_scratch)
+
+        logger.debug(f"End processing stats project {project.id}."
+                     f"Processed {str(tasks)} tasks.")
