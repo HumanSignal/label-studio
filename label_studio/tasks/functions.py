81eae1a1c2849cdf04e906dbcf8d8d58575f1b5d
Konstantin Korotaev <42615530+KonstantinKorotaev@users.noreply.github.com>
2022-06-10 15:40:24 +0300

    Update functions.py

--- label_studio/tasks/functions.py
+++ label_studio/tasks/functions.py
@@ -12,21 +12,22 @@ def calculate_stats_all_orgs(from_scratch):
 
     for org in orgs:
         logger.debug(f"Start recalculating stats for Organization {org.id}.")
-        projects = Project.objects.filter(organization=org).order_by('-updated_at')
+
         # start async calculation job on redis
-        start_job_async_or_sync(redis_job_for_calculation, projects, from_scratch)
+        start_job_async_or_sync(redis_job_for_calculation, org, from_scratch)
 
         logger.debug(f"Organization {org.id} stats were recalculated.")
 
     logger.debug("All organizations were recalculated.")
 
 
-def redis_job_for_calculation(projects, from_scratch):
+def redis_job_for_calculation(org, from_scratch):
     """
     Recalculate counters for projects list
-    :param projects: List of projects
+    :param org: Organization to recalculate
     :param from_scratch: Start calculation from scratch or skip calculated tasks
     """
+    projects = Project.objects.filter(organization=org).order_by('-updated_at')
     for project in projects:
         logger.debug(f"Start processing stats project {project.id} "
                      f"with task count {project.tasks.count()} and updated_at {project.updated_at}")
