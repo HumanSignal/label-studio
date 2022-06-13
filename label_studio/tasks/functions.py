fc44e1647ed79815bbebad20c51e5b858b02a5ae
Konstantin Korotaev <42615530+KonstantinKorotaev@users.noreply.github.com>
2022-06-13 21:31:16 +0300

    Add AsyncMigrationStatus in functions

--- label_studio/tasks/functions.py
+++ label_studio/tasks/functions.py
@@ -1,5 +1,6 @@
 import logging
 
+from core.models import AsyncMigrationStatus
 from organizations.models import Organization
 from projects.models import Project
 
@@ -14,11 +15,18 @@ def calculate_stats_all_orgs(from_scratch):
         projects = Project.objects.filter(organization=org).order_by('-updated_at')
 
         for project in projects:
+            migration = AsyncMigrationStatus.objects.create(
+                project=project,
+                name='0018_manual_migrate_counters',
+                status='STARTED',
+                )
             logger.debug(f"Start processing stats project {project.id} "
                          f"with task count {project.tasks.count()} and updated_at {project.updated_at}")
 
             tasks = project.update_tasks_counters(project.tasks.all(), from_scratch=from_scratch)
-
+            migration.status = 'FINISHED'
+            migration.meta = {'tasks_counted': tasks}
+            migration.save()
             logger.debug(f"End processing stats project {project.id}."
                          f"Processed {str(tasks)} tasks.")
 
