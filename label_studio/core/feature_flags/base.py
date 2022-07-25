86b2f5adcf5fe25cbb0c0ecbb2f696f230b357bd
makseq-ubnt <makseq@gmail.com>
2022-07-25 21:40:28 +0300

    Add fixes

--- label_studio/core/feature_flags/base.py
+++ label_studio/core/feature_flags/base.py
@@ -103,7 +103,9 @@ def all_flags(user):
 
     env_ff = get_all_env_with_prefix('ff_', is_bool=True)
     env_fflag = get_all_env_with_prefix('fflag_', is_bool=True)
+    env_fflag2 = get_all_env_with_prefix('fflag-', is_bool=True)
     env_ff.update(env_fflag)
+    env_ff.update(env_fflag2)
 
     logger.debug(f'Override by flags from env: {env_ff}')
     for env_flag_name, env_flag_on in env_ff.items():
