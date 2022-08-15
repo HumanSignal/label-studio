7019e3fb57bc0361ff4cebac7a7c17ca2047d051
Konstantin Korotaev <42615530+KonstantinKorotaev@users.noreply.github.com>
2022-08-15 13:46:24 +0300

    Update 0029_task_alter_foreign_key_actions.py

--- label_studio/tasks/migrations/0029_task_alter_foreign_key_actions.py
+++ label_studio/tasks/migrations/0029_task_alter_foreign_key_actions.py
@@ -6,10 +6,53 @@ from django.db import connection
 
 logger = logging.getLogger(__name__)
 
+CONSTRAINTS = [
+    {"SQL": "SELECT conname FROM pg_catalog.pg_constraint con "
+        "INNER JOIN pg_catalog.pg_class rel ON rel.oid = con.conrelid "
+        "INNER JOIN pg_catalog.pg_namespace nsp ON nsp.oid = connamespace "
+        "WHERE conname LIKE 'prediction_task_id%'",
+     "TABLE": "prediction",
+     "KEY": "task_id",
+     "REF": '"task" ("id")'},
+    {"SQL": "SELECT conname FROM pg_catalog.pg_constraint con "
+        "INNER JOIN pg_catalog.pg_class rel ON rel.oid = con.conrelid "
+        "INNER JOIN pg_catalog.pg_namespace nsp ON nsp.oid = connamespace "
+        "WHERE conname LIKE 'task_completion_task_id%'",
+     "TABLE": "task_completion",
+     "KEY": "task_id",
+     "REF": '"task" ("id")'},
+    {"SQL": "SELECT conname FROM pg_catalog.pg_constraint con "
+        "INNER JOIN pg_catalog.pg_class rel ON rel.oid = con.conrelid "
+        "INNER JOIN pg_catalog.pg_namespace nsp ON nsp.oid = connamespace "
+        "WHERE conname LIKE 'tasks_tasklock_task_id%'",
+     "TABLE": "tasks_tasklock",
+     "KEY": "task_id",
+     "REF": '"task" ("id")'},
+    {"SQL": "SELECT conname FROM pg_catalog.pg_constraint con "
+        "INNER JOIN pg_catalog.pg_class rel ON rel.oid = con.conrelid "
+        "INNER JOIN pg_catalog.pg_namespace nsp ON nsp.oid = connamespace "
+        "WHERE conname LIKE 'tasks_annotationdraft_task_id%'",
+     "TABLE": "tasks_annotationdraft",
+     "KEY": "task_id",
+     "REF": '"task" ("id")'}
+]
+
 
 def forwards(apps, schema_editor):
     """
     Add ON DELETE CASCADE action for predictions and annotations when deleting tasks
+    """
+    change_constraint(schema_editor)
+
+
+def backwards(apps, schema_editor):
+    # Rollback ON DELETE CASCADE action for predictions and annotations
+    change_constraint(schema_editor, action='NO ACTION')
+
+
+def change_constraint(schema_editor, action='CASCADE'):
+    """
+    Change constraints to be ON DELETE CASCADE or NO ACTION
     """
     # Check if postgres db is used
     if not schema_editor.connection.vendor.startswith('postgres'):
@@ -18,32 +61,11 @@ def forwards(apps, schema_editor):
         return
     # find indexes for prediction_task_id and annotation_task_id
     cursor = connection.cursor()
-    cursor.execute("SELECT conname FROM pg_catalog.pg_constraint con INNER JOIN pg_catalog.pg_class rel ON rel.oid = con.conrelid INNER JOIN pg_catalog.pg_namespace nsp ON nsp.oid = connamespace WHERE conname LIKE 'prediction_task_id%'")
-    prediction_con = cursor.fetchone()
-    cursor.execute("SELECT conname FROM pg_catalog.pg_constraint con INNER JOIN pg_catalog.pg_class rel ON rel.oid = con.conrelid INNER JOIN pg_catalog.pg_namespace nsp ON nsp.oid = connamespace WHERE conname LIKE 'task_completion_task_id%'")
-    task_completion_con = cursor.fetchone()
     # alter indexes to casdade deletion on tasks deletion
-    changes = [f'ALTER TABLE "prediction" DROP CONSTRAINT {prediction_con[0]}, ADD FOREIGN KEY ("task_id") REFERENCES "task" ("id") ON DELETE CASCADE ON UPDATE NO ACTION',
-               f'ALTER TABLE "task_completion" DROP CONSTRAINT {task_completion_con[0]}, ADD FOREIGN KEY ("task_id") REFERENCES "task" ("id") ON DELETE CASCADE ON UPDATE NO ACTION']
-    for change in changes:
-        schema_editor.execute(change)
-
-
-def backwards(apps, schema_editor):
-    # Rollback ON DELETE CASCADE action for predictions and annotations
-    if not schema_editor.connection.vendor.startswith('postgres'):
-        logger.info('Database vendor: {}'.format(schema_editor.connection.vendor))
-        logger.info('Skipping migration without attempting to DROP INDEX')
-        return
-    cursor = connection.cursor()
-    cursor.execute("SELECT conname FROM pg_catalog.pg_constraint con INNER JOIN pg_catalog.pg_class rel ON rel.oid = con.conrelid INNER JOIN pg_catalog.pg_namespace nsp ON nsp.oid = connamespace WHERE conname LIKE 'prediction_task_id%'")
-    prediction_con = cursor.fetchone()
-    cursor.execute("SELECT conname FROM pg_catalog.pg_constraint con INNER JOIN pg_catalog.pg_class rel ON rel.oid = con.conrelid INNER JOIN pg_catalog.pg_namespace nsp ON nsp.oid = connamespace WHERE conname LIKE 'task_completion_task_id%'")
-    task_completion_con = cursor.fetchone()
-    changes = [
-        f'ALTER TABLE "prediction" DROP CONSTRAINT {prediction_con[0]}, ADD FOREIGN KEY ("task_id") REFERENCES "task" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION',
-        f'ALTER TABLE "task_completion" DROP CONSTRAINT {task_completion_con[0]}, ADD FOREIGN KEY ("task_id") REFERENCES "task" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION']
-    for change in changes:
+    for c in CONSTRAINTS:
+        cursor.execute(c['SQL'])
+        con = cursor.fetchone()
+        change = f'ALTER TABLE "{c["TABLE"]}" DROP CONSTRAINT {con[0]}, ADD FOREIGN KEY ("{c["KEY"]}") REFERENCES {c["REF"]} ON DELETE {action} ON UPDATE NO ACTION'
         schema_editor.execute(change)
 
 
