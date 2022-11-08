055371c807439278a1770d105e2d2a7507a8b8c9
Hugues Tennier <hugues@okteto.com>
2022-11-07 23:03:08 -0500

    Add GH stars

--- docs/themes/v2/source/js/header.js
+++ docs/themes/v2/source/js/header.js
@@ -11,5 +11,17 @@
 
   mobileHeaderButtons.forEach(button => button.addEventListener("click", toggleMenu));
 
+
+  const githubstarsContainer  = document.querySelector(".github-stars-count");
+
+  fetch("https://api.github.com/repos/heartexlabs/label-studio")
+  .then((response) => response.json()
+  .then((data) => {
+    const stars = data.stargazers_count?.toLocaleString('en-US');
+    if(stars) githubstarsContainer.textContent = stars;
+  }))
+  .catch((err) => {
+    console.log(err)
+  });
 })();
 
