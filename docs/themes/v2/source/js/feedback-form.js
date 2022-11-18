b4dec65dc4f98ce40f99e85072ff44131ca0c04f
Hugues Tennier <hugues@okteto.com>
2022-11-18 10:03:29 -0500

    Add ntl functions to gather feedback

--- docs/themes/v2/source/js/feedback-form.js
+++ docs/themes/v2/source/js/feedback-form.js
@@ -14,10 +14,32 @@
     moreDetails.style.display = "block"
   }
 
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+
+    const data = {
+      feedback: formData.get('feedback'),
+      helpful: formData.get('helpful'),
+      email: formData.get('email'),
+      pageURL: window.location.href.split('#')[0],
+      pageTitle: document.title,
+      submittedOn: new Date().toLocaleDateString('en-CA')
+    }
+
+
+    fetch('/.netlify/functions/gather-feedback', { method: 'POST', body: JSON.stringify(data) }).then(response => {
+      if(response.status === 200) setFeedback({submitted: true, helpful: data.helpful})
+      console.log(response.status)
+    })
+  }
+
 
   yesNoButtons.forEach(button => button.addEventListener("change", openForm));
 
   cancelButton.addEventListener("click", closeForm);
+  feedbackForm.addEventListener("submit", submitForm)
 
 })();
 
