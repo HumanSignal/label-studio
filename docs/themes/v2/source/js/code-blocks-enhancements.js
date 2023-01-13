731fe26e5e71e15012a8f05da41dfc1af5fb6d1b
Hugues Tennier <huguestennier@gmail.com>
2023-01-13 15:21:33 -0500

    Log the response

--- docs/themes/v2/source/js/code-blocks-enhancements.js
+++ docs/themes/v2/source/js/code-blocks-enhancements.js
@@ -70,6 +70,7 @@ function insert_render_editor(config, modal, full) {
     body: formData,
   })
   .then((response) => {
+    console.log(response);
     editor_iframe(response, modal, full)
   })
   .catch((error) => {
