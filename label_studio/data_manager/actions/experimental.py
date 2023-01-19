62ad159dc39bb462ba4386e90777db4f571b9c62
makseq-ubnt <makseq@gmail.com>
2023-01-19 23:46:45 +0000

    feat: Experimental features add range expression

--- label_studio/data_manager/actions/experimental.py
+++ label_studio/data_manager/actions/experimental.py
@@ -56,7 +56,7 @@ def propagate_annotations(project, queryset, **kwargs):
 
 
 def propagate_annotations_form(user, project):
-    first_annotation = Annotation.objects.filter(task__project=project).first()
+    first_annotation = Annotation.objects.filter(project=project).first()
     field = {
         'type': 'number',
         'name': 'source_annotation_id',
@@ -248,6 +248,7 @@ def process_arrays(params):
 
 
 add_data_field_examples = (
+    'range(2) or '
     'sample() or '
     'random(<min_int>, <max_int>) or '
     'choices(["<value1>", "<value2>", ...], [<weight1>, <weight2>, ...]) or '
@@ -267,8 +268,16 @@ def add_expression(queryset, size, value, value_name):
 
     tasks = list(queryset.only('data'))
 
+    # range
+    if command == 'range':
+        assert len(args) == 1, "range(start:int) should have start argument "
+        start = int(args[0])
+        values = range(start, start + size)
+        for i, v in enumerate(values):
+            tasks[i].data[value_name] = v
+
     # permutation sampling
-    if command == 'sample':
+    elif command == 'sample':
         assert len(args) == 0, "sample() doesn't have arguments"
         values = random.sample(range(0, size), size)
         for i, v in enumerate(values):
@@ -276,14 +285,15 @@ def add_expression(queryset, size, value, value_name):
 
     # uniform random
     elif command == 'random':
-        assert len(args) == 2, 'random() should have 2 args: min & max'
+        assert len(args) == 2, 'random(min, max) should have 2 args: min & max'
         minimum, maximum = int(args[0]), int(args[1])
         for i in range(size):
             tasks[i].data[value_name] = random.randint(minimum, maximum)
 
     # sampling with choices and weights
     elif command == 'choices':
-        assert 0 < len(args) < 3, 'choices() should have 1 or 2 args: values & weights (default=None)'
+        assert 0 < len(args) < 3, 'choices(values:list, weights:list) ' \
+                                  'should have 1 or 2 args: values & weights (default=None)'
         weights = json.loads(args[1]) if len(args) == 2 else None
         values = random.choices(
             population=json.loads(args[0]),
@@ -295,7 +305,7 @@ def add_expression(queryset, size, value, value_name):
 
     # replace
     elif command == 'replace':
-        assert len(args) == 2, 'replace() should have 2 args: old value & new value'
+        assert len(args) == 2, 'replace(old_value:str, new_value:str) should have 2 args: old value & new value'
         old_value, new_value = json.loads(args[0]), json.loads(args[1])
         for task in tasks:
             if value_name in task.data:
