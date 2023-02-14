3f65f2d7440a2e8e5c03f01b51435bfdbbbc6396
robot-ci-heartex <87703623+robot-ci-heartex@users.noreply.github.com>
2023-02-14 20:52:31 +0400

    feat: LSDV-3936: Task history is stored on the backend (#3663)
    * [submodules] Build static heartexlabs/dm2
    
    * [submodules] Build static heartexlabs/dm2
    
    * [submodules] Build static heartexlabs/label-studio-frontend
    
    * feat: LSDV-3936: Task history is stored on the backend (#3642)
    
    * feat: LSDV-3936: Task history is stored on the backend
    
    * Add tests for stream history (LSDV-3936)
    
    * Fix all_urls test
    
    * Removed tasks disappear from history
    
    * Fix filling annotation history (LSDV-3936)
    
    * [frontend] Get build lsf
    
    * [submodules] Build static heartexlabs/dm2
    
    * [submodules] Build static heartexlabs/label-studio-frontend
    
    * [submodules] Build static heartexlabs/dm2
    
    * [submodules] Build static heartexlabs/label-studio-frontend
    
    ---------
    
    Co-authored-by: Julio Sgarbi <julio.sgarbi@hotmail.com>
    Co-authored-by: Sergei Ivashchenko <triklozoid@gmail.com>
    Co-authored-by: juliosgarbi <juliosgarbi@users.noreply.github.com>

--- label_studio/tasks/api.py
+++ label_studio/tasks/api.py
@@ -24,6 +24,7 @@ from data_manager.functions import evaluate_predictions
 from data_manager.models import PrepareParams
 from data_manager.serializers import DataManagerTaskSerializer
 from projects.models import Project
+from projects.functions.stream_history import fill_history_annotation
 from tasks.models import Annotation, AnnotationDraft, Prediction, Task
 from tasks.serializers import (
     AnnotationDraftSerializer,
@@ -421,6 +422,8 @@ class AnnotationsListAPI(generics.ListCreateAPIView):
 
         if self.request.data.get('ground_truth'):
             annotation.task.ensure_unique_groundtruth(annotation_id=annotation.id)
+
+        fill_history_annotation(user, task, annotation)
 
         return annotation
 
