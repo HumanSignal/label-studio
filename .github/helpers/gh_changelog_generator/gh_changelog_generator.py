0dfbcf0bc17381e26d3184143fda64749912011d
Nikita Belonogov <nikita.belonogov@heartex.com>
2023-02-27 17:18:51 +0400

    ci: Changelog 1.1 (#3741)

--- .github/helpers/gh_changelog_generator/gh_changelog_generator.py
+++ .github/helpers/gh_changelog_generator/gh_changelog_generator.py
@@ -1,25 +1,39 @@
-import re
-import subprocess
-import os
-from jira import JIRA
-from jira.exceptions import JIRAError
 import json
+import os
+import re
 
-commit_pattern = re.compile(r'^(\w*):\s*(.*?)?:\s*(.*?)\s*(\(#(\d+)\))?$')
+import requests
+from github import Github
+from jira import JIRA
 
+COMMIT_PATTERN = re.compile(r'^(\w*):\s*(.*?)?:\s*(.*?)\s*(\(#(\d+)\))?$')
+
+RELEASE_VERSION = os.getenv("RELEASE_VERSION").strip('\"')
+CURRENT_REF = os.getenv("CURRENT_REF").strip('\"')
 PREVIOUS_REF = os.getenv("PREVIOUS_REF").strip('\"')
-BASE_BRANCH = os.getenv("BASE_BRANCH", "origin/develop").strip('\"')
 
+JIRA_SERVER = os.getenv("JIRA_SERVER", "https://heartex.atlassian.net").strip('\"')
 JIRA_USERNAME = os.getenv("JIRA_USERNAME").strip('\"')  # in email format, e.g username@domain.com
 JIRA_TOKEN = os.getenv("JIRA_TOKEN").strip('\"')  # https://id.atlassian.com/manage-profile/security/api-tokens
-JIRA_SERVER = os.getenv("JIRA_SERVER", "https://heartex.atlassian.net").strip('\"')
+JIRA_PROJECT = os.getenv("JIRA_PROJECT", "DEV").strip('\"')
 JIRA_RN_FIELD = os.getenv("JIRA_RN_FIELD", "customfield_10064").strip('\"')
-OUTPUT_FILE_MD = os.getenv("OUTPUT_FILE_MD", None)
-OUTPUT_FILE_JSON = os.getenv("OUTPUT_FILE_JSON", None)
 
-jira = JIRA(JIRA_SERVER, basic_auth=(JIRA_USERNAME, JIRA_TOKEN))
+AHA_SERVER = os.getenv("AHA_SERVER", "https://labelstudio.aha.io").strip('\"')
+AHA_TOKEN = os.getenv("AHA_TOKEN").strip('\"')
+AHA_PRODUCT = os.getenv("AHA_PRODUCT", "LSDV").strip('\"')
+AHA_RN_FIELD = os.getenv("AHA_RN_FIELD", "release_notes").strip('\"')
 
-title_map = {
+GH_REPO = os.getenv("GH_REPO", "heartexlabs/label-studio").strip('\"')
+GH_TOKEN = os.getenv("GH_TOKEN").strip('\"')  # https://github.com/settings/tokens/new
+
+LAUNCHDARKLY_SDK_KEY = os.getenv("LAUNCHDARKLY_SDK_KEY", '').strip('\"')
+
+OUTPUT_FILE_MD = os.getenv("OUTPUT_FILE_MD", 'output.md')
+OUTPUT_FILE_JSON = os.getenv("OUTPUT_FILE_JSON", 'output.json')
+
+WORKFLOW_RUN_LINK = os.getenv("WORKFLOW_RUN_LINK", '')
+
+COMMIT_LABEL_MAP = {
     'feat': 'New Features',
     'fix': 'Bug Fixes',
     'perf': 'Performance Improvements',
@@ -27,121 +41,393 @@ title_map = {
     'docs': 'Documentation'
 }
 
+DEFAULT_LABEL = "Other"
 
-def execute_git_cmd(cmd):
-    git_command = " ".join(["git", cmd])
-    return execute_cmd(git_command)
+LABEL_SORT = [
+    "New Feature",
+    "Improvement",
+    "Customer Bug Fix",
+    "Bug Fix",
+    "Refactor",
+    "Research",
+    DEFAULT_LABEL,
+]
 
 
-def execute_cmd(cmd):
+class AHA:
+    def __init__(self, server: str, token: str):
+        self.server = server
+        self.token = token
+
+    def query(self, url: str, data: dict = None):
+        if data:
+            response = requests.put(
+                f"{self.server}/{url}",
+                headers={
+                    "Authorization": f"Bearer {self.token}",
+                    "Content-Type": "application/json",
+                    "Accept": "application/json",
+                },
+                json=data,
+            )
+            return response.json()
+        else:
+            response = requests.get(
+                f"{self.server}/{url}",
+                headers={
+                    "Authorization": f"Bearer {self.token}",
+                },
+            )
+            return response.json()
+
+
+github_client = Github(GH_TOKEN)
+github_repo = github_client.get_repo(GH_REPO)
+jira_client = JIRA(JIRA_SERVER, basic_auth=(JIRA_USERNAME, JIRA_TOKEN))
+aha_client = AHA(AHA_SERVER, AHA_TOKEN)
+
+
+class AhaFeature:
+    pr = None
+
+    def __init__(self, feature_num: str, pr: int = None):
+        self.type = "Aha! Feature"
+        self.pr = pr
+        feature = aha_client.query(f'api/v1/features/{feature_num}').get('feature')
+        self.key = str(feature.get('reference_num'))
+        self.status = str(feature.get('workflow_status').get('name'))
+        self.label = feature.get('workflow_kind', {}).get('name', DEFAULT_LABEL)
+        self.summary = str(feature.get('name'))
+        self.release_note = next(
+            (f.get('value') for f in feature.get('custom_fields', []) if f.get('key') == AHA_RN_FIELD), None)
+        self.desc = self.release_note if self.release_note else self.summary
+        self.link = str(feature.get('url'))
+
+    def __str__(self):
+        return f"<{self.link}|[{self.key}]>: {self.desc} -- *{self.status}*"
+
+    def __key(self):
+        return self.key
+
+    def __dict__(self):
+        return {
+            "desc": self.desc,
+            "link": self.link,
+            "key": self.key,
+            "status": self.status,
+            "pr": self.pr
+        }
+
+
+class JiraIssue(AhaFeature):
+    def __init__(self, issue_number: str, pr: int = None):
+        self.type = "Jira Issue"
+        self.pr = pr
+        issue = jira_client.issue(issue_number)
+        self.key = str(issue)
+        self.status = str(issue.fields.status)
+        self.label = DEFAULT_LABEL
+        self.summary = str(issue.raw['fields']['summary'])
+        self.release_note = issue.get_field(JIRA_RN_FIELD)
+        self.desc = self.release_note if self.release_note else self.summary
+        self.link = f"{JIRA_SERVER}/browse/{self.key}"
+
+
+TASK_CACHE = {}
+
+
+def get_task(task_number: str, pr: int = None) -> AhaFeature or JiraIssue:
+    if task_number in TASK_CACHE.keys():
+        return TASK_CACHE.get(task_number)
     try:
-        out = subprocess.check_output(cmd, shell=True, universal_newlines=True, stderr=subprocess.STDOUT)
-    except subprocess.CalledProcessError as err:
-        print(f"Aborting: Got error while calling \"{cmd}\"")
-        print(f"Details: {err}")
-        print(f"Details: {err.output}")
-        exit(1)
-    return out.strip()
-
-
-def execute_cmd_visible(cmd):
-    with subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,
-                          universal_newlines=True, stderr=subprocess.STDOUT) as proc:
-        for line in proc.stdout:
-            print(line, end='')
-    if proc.returncode != 0:
-        print(f"Aborting: Got error while calling \"{cmd}\"")
-        exit(1)
-
-
-def get_head_sha():
-    return execute_git_cmd("rev-parse HEAD")
-
-
-def generate_changelog(tag_new, previous_release_id):
-    out = execute_git_cmd(f"log --reverse --pretty=format:%s {tag_new}..{previous_release_id}")
-    return out
-
-
-def get_branch_diverge_commit(branch, base_branch):
-    out = execute_git_cmd(f"rev-list --boundary {branch}...{base_branch} | grep '^-' | cut -c2- | tail -n1")
-    return out
-
-
-def changelog_list(raw_changelog) -> list:
-    changelog_list = []
-    for line in raw_changelog.splitlines():
-        if (match := re.match(commit_pattern, line)) is not None:
-            if match.group(1) in title_map.keys():
-                title = match.group(1)
-                jira = match.group(2)
-                changelog_list.append(title + jira)
-    return list(set(changelog_list))
-
-
-def jira_release_note(ticket):
+        task = AhaFeature(task_number, pr)
+        TASK_CACHE[task_number] = task
+        return task
+    except Exception as e:
+        print(f'Could not find Feature {task_number} in Aha!: {e}')
     try:
-        issue = jira.issue(ticket)
-        return issue.get_field(JIRA_RN_FIELD)
-    except JIRAError:
-        return ''
+        task = JiraIssue(task_number, pr)
+        TASK_CACHE[task_number] = task
+        return task
+    except Exception as e:
+        print(f'Could not find Issue   {task_number} in Jira: {e}')
+    return None
 
 
-def changelog_map(raw_changelog, tasks_list):
-    changelog_dict = {}
-    changelog_dup_dict = {}
-    for line in raw_changelog.splitlines():
-        if (match := re.match(commit_pattern, line)) is not None:
-            if match.group(1) in title_map.keys():
-                title = match.group(1)
-                ticket = match.group(2)
-                msg = match.group(3)
-                pr_id = match.group(5) if match.group(5) else ''
-                jira_rn = jira_release_note(ticket)
-                desc = jira_rn.replace("\n", " ") if jira_rn else msg
-                list_key = title + ticket
-                entry = {'desc': desc, 'pr': pr_id}
-                if list_key in tasks_list:
-                    if title in changelog_dup_dict:
-                        changelog_dup_dict[title].update({ticket: entry})
-                    else:
-                        changelog_dup_dict[title] = {ticket: entry}
-                elif title in changelog_dict:
-                    changelog_dict[title].update({ticket: entry})
-                else:
-                    changelog_dict[title] = {ticket: entry}
-    return changelog_dict, changelog_dup_dict
+def get_aha_release(product: str, version: str):
+    aha_releases = aha_client.query(f'api/v1/products/{product}/releases').get('releases', [])
+    aha_sorted_releases = sorted(aha_releases, key=lambda x: x.get('name'), reverse=True)
+    return next((e for e in aha_sorted_releases if version in e.get('name')), None)
 
 
-def format_changelog(changelog) -> str:
-    result_lines = []
-    for key in title_map.keys():
-        if key in changelog.keys():
-            result_lines.append(f"### {title_map[key]}")
-            for jira, line in changelog[key].items():
-                result_lines.append(f"- {line['desc']} [{jira.upper()}]")
-    return '\n'.join(result_lines)
+def get_aha_release_features(release_num: str) -> list[AhaFeature]:
+    features = aha_client.query(f'api/v1/releases/{release_num}/features').get('features', [])
+    tasks = set()
+    for feature in features:
+        if task := get_task(feature.get('reference_num')):
+            tasks.add(task)
+    return list(tasks)
+
+
+def get_jira_release(project: str, version: str):
+    jira_project_versions = jira_client.project_versions(project=project)
+    jira_sorted_project_versions = sorted(jira_project_versions, key=lambda x: x.name, reverse=True)
+    return next((e for e in jira_sorted_project_versions if version in e.name), None)
+
+
+def get_jira_release_issues(project_id: str, release_id: str) -> list[JiraIssue]:
+    issues = jira_client.search_issues(
+        f"project = {project_id} AND fixVersion = {release_id} ORDER BY priority DESC, key ASC")
+    tasks = set()
+    for issue in issues:
+        if task := get_task(issue.key):
+            tasks.add(task)
+    return list(tasks)
+
+
+def get_github_release(previous_ref: str, current_ref: str):
+    return github_repo.compare(previous_ref, current_ref)
+
+
+def get_github_release_tasks(commits) -> list[AhaFeature]:
+    tasks = set()
+    for commit in commits:
+        message_first_line = commit.commit.message.split("\n")[0]
+        if (match := re.match(COMMIT_PATTERN, message_first_line)) is not None:
+            label = match.group(1)
+            if label in COMMIT_LABEL_MAP.keys():
+                task_key = match.group(2)
+                pr = None
+                try:
+                    pr = int(match.group(5))
+                except Exception as e:
+                    print(f'Could no parse pr from "{message_first_line}": {str(e)}')
+                if task := get_task(task_key, pr):
+                    tasks.add(task)
+    return list(tasks)
+
+
+def get_feature_flags() -> list[str]:
+    result = []
+    if LAUNCHDARKLY_SDK_KEY:
+        response = requests.get(
+            url="https://sdk.launchdarkly.com/sdk/latest-all",
+            headers={
+                "Authorization": LAUNCHDARKLY_SDK_KEY,
+            },
+            timeout=30,
+        )
+        for key, flag in response.json().get('flags', {}).items():
+            if not flag.get('on'):
+                result.append(f'- [{key}](https://app.launchdarkly.com/default/onpremise/features/{key}/targeting)')
+    return result
+
+
+def missing_tasks(left: list[AhaFeature], right: list[AhaFeature]) -> list[AhaFeature]:
+    r_keys = [x.key for x in right]
+    missing = [task for task in left if task.key not in r_keys]
+    missing_sorted = sorted(missing, key=lambda x: int(x.key.split('-')[-1]))
+    return missing_sorted
+
+
+def sort_task_by_label(tasks: list[AhaFeature]) -> dict[str, list[AhaFeature]]:
+    result = {}
+    for task in tasks:
+        result[task.label] = result.get(task.label, []) + [task]
+    return result
+
+
+def render_tasks_md(tasks: list[AhaFeature]) -> list[str]:
+    result = []
+    for task in tasks:
+        result.append(f'- {task.desc} [{task.key}]({task.link})')
+    return result
+
+
+def render_add_quote_md(lines: list[str]) -> list[str]:
+    return [f'> {line}' for line in lines]
+
+
+def render_add_spoiler_md(title: str, lines: list[str]) -> list[str]:
+    result = ["<details>", f"<summary>{title}</summary>", ""]
+    result.extend(lines)
+    result.append("")
+    result.append("</details>")
+    return result
+
+
+def render_add_header_md(title: str, lines: list[str]) -> list[str]:
+    result = [f"### {title}"]
+    result.extend(lines)
+    result.append('')
+    return result
+
+
+def render_output_md(
+        gh_release,
+        jira_release,
+        aha_release,
+        sorted_release_tasks: dict[str, list[AhaFeature]],
+        missing_in_gh: list[AhaFeature],
+        missing_in_tracker: list[AhaFeature],
+        missing_release_note_field: list[AhaFeature],
+        turned_off_feture_flags: list[str],
+) -> str:
+    release_notes_lines = []
+
+    for label, tasks in sorted(sorted_release_tasks.items(),
+                               key=lambda x: LABEL_SORT.index(x[0]) if x[0] in LABEL_SORT else 100):
+        release_notes_lines.extend(
+            render_add_header_md(
+                label,
+                render_tasks_md(tasks)
+            )
+        )
+
+    comment = []
+
+    comment.append(f'Full Changelog: [{PREVIOUS_REF}...{RELEASE_VERSION}]({gh_release.diff_url})')
+    comment.append(
+        f'This changelog was updated in response to a push of {CURRENT_REF} [Workflow run]({WORKFLOW_RUN_LINK})')
+    comment.append('')
+    if jira_release:
+        comment.append(
+            f'[Jira Release {RELEASE_VERSION}]({JIRA_SERVER}/projects/{JIRA_PROJECT}/versions/'
+            f'{jira_release.id}/tab/release-report-all-issues)')
+    else:
+        comment.append(f'Jira Release not found')
+    if aha_release:
+        comment.append(f'[Aha! Release {RELEASE_VERSION}]({aha_release.get("url", "")})')
+    else:
+        comment.append(f'Aha! Release not found')
+
+    if missing_in_gh:
+        comment.extend(
+            render_add_spoiler_md(
+                f'Missing in GitHub release ({len(missing_in_gh)})',
+                render_tasks_md(missing_in_gh)
+            )
+        )
+    if missing_in_tracker:
+        comment.extend(
+            render_add_spoiler_md(
+                f'Missing in Task Tracker release ({len(missing_in_tracker)})',
+                render_tasks_md(missing_in_tracker)
+            )
+        )
+    if missing_release_note_field:
+        comment.extend(
+            render_add_spoiler_md(
+                f'Missing Release note field ({len(missing_release_note_field)})',
+                render_tasks_md(missing_release_note_field)
+            )
+        )
+    if turned_off_feture_flags:
+        comment.extend(
+            render_add_spoiler_md(
+                f'Turned off Feature Flags ({len(turned_off_feture_flags)})',
+                turned_off_feture_flags
+            )
+        )
+
+    comment.append('')
+    comment.append('**ALL LINES STARTING FROM QUOTE WILL BE IGNORED**')
+
+    release_notes_lines.extend(
+        render_add_quote_md(comment)
+    )
+
+    return '\n'.join(release_notes_lines)
+
+
+def render_output_json(
+        sorted_release_tasks: dict[str, list[AhaFeature]],
+) -> dict:
+    sorted_release_tasks_json = {}
+    for label, tasks in sorted_release_tasks.items():
+        sorted_release_tasks_json[label] = [t.__dict__() for t in tasks]
+    result = {
+        'sorted_release_tasks': sorted_release_tasks_json
+    }
+    return result
 
 
 def main():
-    new_tag = os.getenv("NEW_TAG", get_head_sha())
+    gh_release = get_github_release(PREVIOUS_REF, CURRENT_REF)
+    print(f"{gh_release.html_url}")
+    print(f"Ahead by {gh_release.ahead_by}")
+    print(f"Behind by {gh_release.behind_by}")
+    print(f"Merge base commit: {gh_release.merge_base_commit}")
+    print(f"Commits: {gh_release.commits}")
+    gh_release_tasks = get_github_release_tasks(gh_release.commits)
 
-    previous_tag_base_commit = get_branch_diverge_commit(PREVIOUS_REF, BASE_BRANCH)
-    branch_diff_changelog = generate_changelog(previous_tag_base_commit, PREVIOUS_REF)
-    previous_tag_tasks_list = changelog_list(branch_diff_changelog)
+    aha_release = get_aha_release(AHA_PRODUCT, RELEASE_VERSION)
+    aha_release_features = []
+    if aha_release:
+        aha_release_features = get_aha_release_features(aha_release.get("reference_num", None))
+        print(
+            f"Aha! Release {aha_release.get('url', '')}")
+    else:
+        print(f"Aha! Release not found")
 
-    new_changelog = generate_changelog(previous_tag_base_commit, new_tag)
-    changelog_new, changelog_dup = changelog_map(new_changelog, previous_tag_tasks_list)
+    jira_release = get_jira_release(JIRA_PROJECT, RELEASE_VERSION)
+    jira_release_issues = []
+    if jira_release:
+        jira_release_issues = get_jira_release_issues(jira_release.projectId, jira_release.id)
+        print(
+            f"Jira Release {JIRA_SERVER}/projects/{JIRA_PROJECT}/versions/"
+            f"{jira_release.id}/tab/release-report-all-issues]")
+    else:
+        print(f"Jira Release not found")
 
+    tracker_release_tasks = jira_release_issues + aha_release_features
+
+    if tracker_release_tasks:
+        print(f"{len(tracker_release_tasks)} tasks fount in Task Tracker")
+        print("Using Task Tracker as a source")
+        sorted_release_tasks = sort_task_by_label(tracker_release_tasks)
+        missing_in_gh = missing_tasks(tracker_release_tasks, gh_release_tasks)
+        missing_in_tracker = missing_tasks(gh_release_tasks, tracker_release_tasks)
+        missing_release_note_field = [x for x in tracker_release_tasks if not x.release_note]
+    else:
+        print(f"No tasks fount in Task Tracker")
+        print("Using GitHub as a source")
+        sorted_release_tasks = sort_task_by_label(gh_release_tasks)
+        missing_in_gh = []
+        missing_in_tracker = []
+        missing_release_note_field = [x for x in tracker_release_tasks if not x.release_note]
+    turned_off_feture_flags = []
+    try:
+        turned_off_feture_flags = get_feature_flags()
+    except Exception as e:
+        print(f'Failed to fetch Feature Flags: {e}')
+
+    output_md = render_output_md(
+        gh_release,
+        jira_release,
+        aha_release,
+        sorted_release_tasks,
+        missing_in_gh,
+        missing_in_tracker,
+        missing_release_note_field,
+        turned_off_feture_flags,
+    )
     if OUTPUT_FILE_MD:
         with open(OUTPUT_FILE_MD, 'w') as f:
             print(f"Creating a markdown output file: '{OUTPUT_FILE_MD}'")
-            f.write(format_changelog(changelog_new))
+            f.write(output_md)
+    else:
+        print(f"OUTPUT_FILE_MD is not specified")
+    print(output_md)
 
+    output_json = render_output_json(sorted_release_tasks)
     if OUTPUT_FILE_JSON:
         with open(OUTPUT_FILE_JSON, 'w') as f:
             print(f"Creating a json output file: '{OUTPUT_FILE_JSON}'")
-            json.dump(changelog_new, f)
+            json.dump(output_json, f)
+    else:
+        print(f"OUTPUT_FILE_JSON is not specified")
+    print(output_json)
 
 
 if __name__ == "__main__":
