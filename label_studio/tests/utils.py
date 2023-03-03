74ea8be4f65b2d950e7278eeb6315a13a5a03fc3
Sergei Ivashchenko <triklozoid@gmail.com>
2023-03-03 12:50:34 +0000

    fix: LSDV-4568: Fix csv import from url

--- label_studio/tests/utils.py
+++ label_studio/tests/utils.py
@@ -3,6 +3,8 @@
 import ujson as json
 
 import json
+import re
+import io
 import pytest
 import requests_mock
 import requests
@@ -45,6 +47,18 @@ def register_ml_backend_mock(m, url='http://localhost:9090', predictions=None, h
     m.post(f'{url}/webhook', text=json.dumps({}))
     m.post(f'{url}/versions', text=json.dumps({'versions': ["1", "2"]}))
     return m
+
+
+@contextmanager
+def import_from_url_mock(**kwargs):
+    with requests_mock.Mocker(real_http=True) as m:
+        url='https://data.heartextest.net'
+
+        with open('./tests/test_suites/samples/test_1.csv', 'rb') as f:
+            matcher = re.compile('data.heartextest.net/test_1.csv')
+
+            m.get(matcher, body=f, headers={'Content-Length': '100'})
+            yield m
 
 
 class _TestJob(object):
