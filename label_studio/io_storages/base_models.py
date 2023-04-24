c63ece1d55a61d761e3aa71a2f015b51b742ab98
robot-ci-heartex <87703623+robot-ci-heartex@users.noreply.github.com>
2023-04-24 18:33:01 +0400

    fix: LSDV-4711: CORS error accessing previously annotated data (#3991)
    * [submodules] Build static heartexlabs/label-studio-frontend
    
    * ci: Update Feature Flags
    
    * create a presign proxy for task uris which use cloud storage and presigned urls
    
    * ci: Update Feature Flags
    
    * [submodules] Build static heartexlabs/dm2
    
    * ci: Update Feature Flags
    
    * [submodules] Build static heartexlabs/label-studio-frontend
    
    * [submodules] Build static heartexlabs/label-studio-frontend
    
    * [submodules] Build static heartexlabs/label-studio-frontend
    
    * [submodules] Build static heartexlabs/dm2
    
    * update all_urls
    
    * Update label_studio/tasks/models.py
    
    Co-authored-by: yyassi-heartex <104568407+yyassi-heartex@users.noreply.github.com>
    
    * ensure url is including server hostname and can be directly accessed if consumed or used directly
    
    * add tests for presign changes
    
    * [submodules] Build static heartexlabs/label-studio-frontend
    
    * [submodules] Build static heartexlabs/dm2
    
    * [submodules] Build static heartexlabs/label-studio-frontend
    
    ---------
    
    Co-authored-by: Brandon Martel <brandonmartel@gmail.com>
    Co-authored-by: robot-ci-heartex <robot-ci-heartex@users.noreply.github.com>
    Co-authored-by: yyassi-heartex <104568407+yyassi-heartex@users.noreply.github.com>

--- label_studio/io_storages/base_models.py
+++ label_studio/io_storages/base_models.py
@@ -4,8 +4,11 @@ import logging
 import django_rq
 import json
 
+from urllib.parse import urljoin, quote
+
 from django.utils import timezone
 from django.db import models, transaction
+from django.shortcuts import reverse
 from django.utils.translation import gettext_lazy as _
 from django.conf import settings
 from django_rq import job
@@ -71,12 +74,12 @@ class ImportStorage(Storage):
         # if not found any occurrences - this Storage can't resolve url
         return False
 
-    def resolve_uri(self, uri):
+    def resolve_uri(self, uri, task=None):
         #  list of objects
         if isinstance(uri, list):
             resolved = []
             for item in uri:
-                result = self.resolve_uri(item)
+                result = self.resolve_uri(item, task)
                 resolved.append(result if result else item)
             return resolved
 
@@ -84,7 +87,7 @@ class ImportStorage(Storage):
         elif isinstance(uri, dict):
             resolved = {}
             for key in uri.keys():
-                result = self.resolve_uri(uri[key])
+                result = self.resolve_uri(uri[key], task)
                 resolved[key] = result if result else uri[key]
             return resolved
 
@@ -96,8 +99,14 @@ class ImportStorage(Storage):
                 if not extracted_storage:
                     logger.debug(f'No storage info found for URI={uri}')
                     return
-                # resolve uri to url using storages
-                http_url = self.generate_http_url(extracted_uri)
+
+                if self.presign and task is not None:
+                    proxy_url = urljoin(settings.HOSTNAME, reverse("data_import:storage-data-presign", kwargs={ "task_id": task.id }) + f'?uri={quote(extracted_uri)}')
+                    return uri.replace(extracted_uri, proxy_url)
+                else:
+                    # resolve uri to url using storages
+                    http_url = self.generate_http_url(extracted_uri)
+
                 return uri.replace(extracted_uri, http_url)
             except Exception as exc:
                 logger.info(f'Can\'t resolve URI={uri}', exc_info=True)
