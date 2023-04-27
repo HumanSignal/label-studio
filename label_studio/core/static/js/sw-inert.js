94ed0458de82c841f90934de5cfeb658af104a5f
Brandon Martel <brandonmartel@gmail.com>
2023-04-27 09:08:35 -0500

    leave the sw-inert empty in the fetch, so network is used entirely untouched

--- label_studio/core/static/js/sw-inert.js
+++ label_studio/core/static/js/sw-inert.js
@@ -1,6 +1,5 @@
-self.addEventListener("fetch", (event) => {
-  // Fallback service worker simply fetches the network request without any caching
+self.addEventListener("fetch", () => {
+  // Fallback service worker that does nothing.
   // This is here to make sure if the application cannot work properly with the standard service worker
   // we can shut it down and fallback to this one and still have a working application.
-  event.respondWith(fetch(event.request));
 });
