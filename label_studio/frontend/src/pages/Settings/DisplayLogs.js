4c2253127f13a57121a75d9b43c44360d70d7777
hadiyouness10 <hadi.youness@lau.edu>
2023-05-08 12:17:09 +0200

    fix logs

--- label_studio/frontend/src/pages/Settings/DisplayLogs.js
+++ label_studio/frontend/src/pages/Settings/DisplayLogs.js
@@ -67,12 +67,13 @@ export const DisplayLogs = () => {
     }
   return (
       <div style={{ width: 1000 }}>
-      {type == "tao" ? <h3>Tao Trainer Logs</h3> : <div><h3>Segmentation Logs</h3>
+      {type == "tao" || type=="object_detection" ? <h3>Object Detection Logs</h3> : <h3>Segmentation Logs</h3>}
+      <div>
               <label>
-              Select a label:
+              Select a model version:
               <select value={selectedModelVersion} onChange={handleSelect} style={{marginLeft: 10}}>
                 <option value="" disabled>
-                  Select a label
+                  model version
                 </option>
                 {Object.keys(modelVersions).map((key) => (
                   <option key={modelVersions[key]['label']} value={modelVersions[key]['label']}>
@@ -82,13 +83,13 @@ export const DisplayLogs = () => {
               </select>
         </label>
         </div>
-      }
+      
         <div>
 
       </div>
           <div className="row">
               <div className="col-10">
-                  <p>These are the logs, check here when you train or predict</p>
+                  <p>These are the logs, check here when you train or predict (all tao projects will be displayed directly)</p>
                 </div>
                 <div onClick={() => getLogs()} style={{marginBottom: 10}} className="col-2"><button className="btn btn-success float-right">Update Logs</button></div>
           </div>
