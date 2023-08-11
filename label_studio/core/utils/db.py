HumanSignal/label-studio

commit 6523f9688d3bd627c03d4583cd5373bd7d163e68

fix: LSDV-5425-1: Harden task/annotation permissions (#4588)
* fix: LSDV-5425-1: Harden task and annotation permissions

* Implement AnnotationMixin

label_studio/tasks/mixins.py
@@ -1,20 +1,22 @@
-from django.db import models
-
 
 class TaskMixin:
-    def _get_is_labeled_value(self):
+    def has_permission(self, user: "User") -> bool:
+        """Called by Task#has_permission"""
+        return True
+
+    def _get_is_labeled_value(self) -> bool:
         n = self.completed_annotations.count()
         return n >= self.overlap
 
-    def update_is_labeled(self, *args, **kwargs):
+    def update_is_labeled(self, *args, **kwargs) -> None:
         self.is_labeled = self._get_is_labeled_value()
 
     @classmethod
-    def post_process_bulk_update_stats(cls, tasks):
+    def post_process_bulk_update_stats(cls, tasks) -> None:
         pass
 
 
-class AnnotationMixin(models.Model):
-    class Meta:
-        abstract = True
-        indexes = []
+class AnnotationMixin:
+    def has_permission(self, user: "User") -> bool:
+        """Called by Annotation#has_permission"""
+        return True

commit d88e904f5b3813271b17e1c41c0849fb6f63499b

fix: LSDV-5425-1: Fix possible performance regression introduced in #4588

label_studio/core/utils/db.py
@@ -1,9 +1,9 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional, TypeVar
 import logging
 
 from core.feature_flags import flag_set
 from django.db import models
-from django.db.models import Subquery
+from django.db.models import Model, QuerySet, Subquery
 
 if TYPE_CHECKING:
     from users.models import User
@@ -16,14 +16,16 @@ class SQCount(Subquery):
     output_field = models.IntegerField()
 
 
-def fast_first(queryset):
+ModelType = TypeVar('ModelType', bound=Model)
+
+def fast_first(queryset: QuerySet[ModelType]) -> Optional[ModelType]:
     """Replacement for queryset.first() when you don't need ordering,
     queryset.first() works slowly in some cases
     """
-    try:
-        return queryset.all()[0]
-    except IndexError:
-        return None
+
+    if queryset[:1]:
+        return queryset[0]
+    return None
 
 
 def should_run_bulk_update_in_transaction(organization_created_by_user: "User") -> bool:

label_studio/tasks/models.py
@@ -22,7 +22,7 @@ from core.utils.common import (
     string_is_url,
     temporary_disconnect_list_signal,
 )
-from core.utils.db import should_run_bulk_update_in_transaction
+from core.utils.db import fast_first, should_run_bulk_update_in_transaction
 from core.utils.params import get_env
 from data_import.models import FileUpload
 from data_manager.managers import PreparedTaskManager, TaskManager
@@ -189,13 +189,15 @@ class Task(TaskMixin, models.Model):
         """Retrieve the task locked by specified user. Returns None if the specified user didn't lock anything."""
         lock = None
         if project is not None:
-            lock = TaskLock.objects.filter(
-                user=user, expire_at__gt=now(), task__project=project
-            ).first()
+            lock = fast_first(
+                TaskLock.objects.filter(
+                    user=user, expire_at__gt=now(), task__project=project
+                )
+            )
         elif tasks is not None:
-            locked_task = tasks.filter(
-                locks__user=user, locks__expire_at__gt=now()
-            ).first()
+            locked_task = fast_first(
+                tasks.filter(locks__user=user, locks__expire_at__gt=now())
+            )
             if locked_task:
                 return locked_task
         else:
@@ -357,9 +359,11 @@ class Task(TaskMixin, models.Model):
                 ):
                     # permission check: resolve uploaded files to the project only
                     file_upload = None
-                    file_upload = FileUpload.objects.filter(
-                        project=project, file=prepared_filename
-                    ).first()
+                    file_upload = fast_first(
+                        FileUpload.objects.filter(
+                            project=project, file=prepared_filename
+                        )
+                    )
                     if file_upload is not None:
                         if flag_set(
                             "ff_back_dev_2915_storage_nginx_proxy_26092022_short",