13a8957602fc7ca80966a9988b5ddedea16da2d4
Andreas Divaris <dredivaris@gmail.com>
2023-08-16 10:30:29 -0700

    Update to avoid magic number and add reasoning behind size choice

--- label_studio/tasks/migrations/0042_auto_20230810_2304.py
+++ label_studio/tasks/migrations/0042_auto_20230810_2304.py
@@ -4,6 +4,9 @@ from django.db import migrations
 from django.core.paginator import Paginator
 from django.db import connection
 
+# chosen to be small enough to not cause memory issues, but large enough to not cause too many queries
+# and avoid locking the entire table for too long
+BATCH_SIZE = 1000
 
 def set_project_from_task(apps, schema_editor):
     Prediction = apps.get_model('tasks', 'Prediction')
@@ -13,7 +16,7 @@ def set_project_from_task(apps, schema_editor):
     predictions_with_task = Prediction.objects.all().select_related('task')
 
     # Break the update into batches using Paginator
-    paginator = Paginator(predictions_with_task, 1000)
+    paginator = Paginator(predictions_with_task, BATCH_SIZE)
 
     for page_num in paginator.page_range:
         page = paginator.page(page_num)
