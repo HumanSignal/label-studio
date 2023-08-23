d9285373e238c5611cda9938df57a2b0e89b8446
Nick Skriabin <767890+nicholasrq@users.noreply.github.com>
2023-08-23 16:28:34 +0100

    feat: LSDV-5519: Organization page tip (Label Studio Community) (#4658)
    * Introduce Heidi tips
    
    * ci: Build frontend
    
    Workflow run: https://github.com/HumanSignal/label-studio/actions/runs/5901625542
    
    * Fix random tip selection
    
    * ci: Build frontend
    
    Workflow run: https://github.com/HumanSignal/label-studio/actions/runs/5902201016
    
    * Cookie to dismiss tips
    
    * ci: Build frontend
    
    Workflow run: https://github.com/HumanSignal/label-studio/actions/runs/5903825521
    
    * Caption component
    
    * Fix caption styles
    
    * Use caption in project creation
    
    * ci: Build frontend
    
    Workflow run: https://github.com/HumanSignal/label-studio/actions/runs/5904073950
    
    * Tip on the org page
    
    * Option to add URL params to the links
    
    * ci: Build frontend
    
    Workflow run: https://github.com/HumanSignal/label-studio/actions/runs/5904701057
    
    * Add feature flag
    
    * ci: Build frontend
    
    Workflow run: https://github.com/HumanSignal/label-studio/actions/runs/5904791829
    
    * Add feature flag
    
    * ci: Build frontend
    
    Workflow run: https://github.com/HumanSignal/label-studio/actions/runs/5905202714
    
    * fix some lint issues
    
    * ci: Build frontend
    
    Workflow run: https://github.com/HumanSignal/label-studio/actions/runs/5907055489
    
    * add new GA params
    
    * add new GA params
    
    * ci: Build frontend
    
    Workflow run: https://github.com/HumanSignal/label-studio/actions/runs/5907116794
    
    * move the code to be defined before HeidiTip
    
    * ci: Build frontend
    
    Workflow run: https://github.com/HumanSignal/label-studio/actions/runs/5907139364
    
    * add link to workspace dropdown
    
    * ci: Build frontend
    
    Workflow run: https://github.com/HumanSignal/label-studio/actions/runs/5907221213
    
    * Fix typo
    
    * Fix link behavior
    
    * ci: Build frontend
    
    Workflow run: https://github.com/HumanSignal/label-studio/actions/runs/5925171669
    
    * Fix link behavior
    
    * ci: Build frontend
    
    Workflow run: https://github.com/HumanSignal/label-studio/actions/runs/5925205863
    
    * Add server_id and user_id to createURL
    
    * Remove unnecessary server_id from content
    
    * Remove unnecessary server and user ids from project creation tip
    
    * ci: Build frontend
    
    Workflow run: https://github.com/HumanSignal/label-studio/actions/runs/5925352703
    
    * Support SERVER_ID
    
    * Remove placeholder
    
    * ci: Build frontend
    
    Workflow run: https://github.com/HumanSignal/label-studio/actions/runs/5926601530
    
    * Mark Org page tip links
    
    * ci: Build frontend
    
    Workflow run: https://github.com/HumanSignal/label-studio/actions/runs/5926735655
    
    * Move server_id assignment
    
    * Fix typos
    
    ---------
    
    Co-authored-by: robot-ci-heartex <robot-ci-heartex@users.noreply.github.com>
    Co-authored-by: Julio Sgarbi <julio.sgarbi@hotmail.com>

--- label_studio/frontend/src/components/HeidiTips/content.ts
+++ label_studio/frontend/src/components/HeidiTips/content.ts
@@ -74,6 +74,79 @@ export const TipsCollection: Record<string, Tip[]> = {
       },
     },
   }],
+  organizationPage: [{
+    title: "It looks like your team is growing!",
+    content: "Assign roles to your team using Label Studio Enterprise and control access to sensitive data at the project and workspace levels.",
+    closable: true,
+    link: {
+      label: "Learn more",
+      url: "https://docs.humansignal.com/guide/manage_users#Roles-in-Label-Studio-Enterprise",
+      params: {
+        experiment: "organization_page_tip",
+        treatment: "team_growing",
+      },
+    }
+  }, {
+    title: "Want to simplify and secure logging in?",
+    content: "Enable Single Sign-On for your team using SAML, SCIM2 or LDAP with Label Studio Enterprise.",
+    closable: true,
+    link: {
+      label: "Learn more",
+      url: "https://docs.humansignal.com/guide/auth_setup",
+      params: {
+        experiment: "organization_page_tip",
+        treatment: "enable_sso",
+      },
+    }
+  }, {
+    title: "Want to improve your labeling team’s efficiency?",
+    content: "Gain insight into your annotators’ productivity with project performance dashboards in Label Studio Enterprise.",
+    closable: true,
+    link: {
+      label: "Learn more",
+      url: "https://docs.humansignal.com/guide/dashboards",
+      params: {
+        experiment: "organization_page_tip",
+        treatment: "project_performance",
+      },
+    }
+  }, {
+    title: "Want to automate task distribution?",
+    content: "Create rules, automate how tasks are distributed to annotators, and only show tasks assigned to each annotator in their view.and control task visibility for each annotator.",
+    closable: true,
+    link: {
+      label: "Learn more",
+      url: "https://docs.humansignal.com/guide/setup_project#Set-up-annotation-settings-for-your-project",
+      params: {
+        experiment: "organization_page_tip",
+        treatment: "automate_distribution",
+      },
+    }
+  }, {
+    title: "Share knowledge with the community",
+    content: "Have questions or a tip to share with other Label Studio users? Join the community slack channel for the latest updates. ",
+    closable: true,
+    link: {
+      label: "Join the community",
+      url: "https://label-studio.slack.com",
+      params: {
+        experiment: "organization_page_tip",
+        treatment: "share_knowledge",
+      },
+    }
+  }, {
+    title: "Did you know?",
+    content: "Label Studio supports multiple points of integration with cloud storage, machine learning models, and popular tools to automate your machine learning pipeline.",
+    closable: true,
+    link: {
+      label: "Check out the integrations directory",
+      url: "https://labelstud.io/integrations/",
+      params: {
+        experiment: "organization_page_tip",
+        treatment: "integration_points",
+      },
+    }
+  }],
   projectSettings: [{
     title: "Did you know?",
     content: "You can automatically label and sort tasks by prediction score to maximize labeling efficiency in Label Studio Enterprise.",
