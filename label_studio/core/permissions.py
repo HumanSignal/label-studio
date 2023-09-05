b6faa047f11953ce894b300a4f1fdf45653b1792
Andreas Divaris <dredivaris@gmail.com>
2023-09-05 16:54:26 -0700

    Add noqa ignore to get by inconsistent linting issues

--- label_studio/core/permissions.py
+++ label_studio/core/permissions.py
@@ -1,11 +1,11 @@
 """This file and its contents are licensed under the Apache License 2.0. Please see the included NOTICE for copyright information and LICENSE for a copy of the license.
 """
-import logging
+import logging   # noqa: I001
 from typing import Optional
 
 from pydantic import BaseModel
 
-import rules  # noqa: I001
+import rules
 
 logger = logging.getLogger(__name__)
 
