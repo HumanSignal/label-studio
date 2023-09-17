5f24d342fcedfd9e6b05d86488614ce6317526d3
Rob van der Duim <105631641+RobvdDuim@users.noreply.github.com>
2023-09-17 14:05:18 +0200

    add code from v1 branch

--- label_studio/taskgeneration/models.py
+++ label_studio/taskgeneration/models.py
@@ -4,13 +4,13 @@ from sensormodel.models import Subject
 from sensordata.models import SensorData
 
 
-class VideoImuOverlap(models.Model):
-    video = models.ForeignKey(SensorData, on_delete=models.CASCADE, null=True,related_name='video')
-    imu = models.ForeignKey(SensorData, on_delete=models.CASCADE, null=True,related_name='imu')
+class SensorOverlap(models.Model):
+    sensordata_A = models.ForeignKey(SensorData, on_delete=models.CASCADE, null=True,related_name='sensor_A')
+    sensordata_B = models.ForeignKey(SensorData, on_delete=models.CASCADE, null=True,related_name='sensor_B')
     project = models.ForeignKey(Project, on_delete=models.CASCADE, null=True)
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE, null=True)
 
-    start_video = models.FloatField(blank=True,null=True)
-    end_video = models.FloatField(blank=True,null=True)
-    start_imu = models.FloatField(blank=True,null=True)
-    end_imu = models.FloatField(blank=True,null=True)
+    start_A = models.FloatField(blank=True,null=True)
+    end_A = models.FloatField(blank=True,null=True)
+    start_B = models.FloatField(blank=True,null=True)
+    end_B = models.FloatField(blank=True,null=True)
--- label_studio/taskgeneration/views.py
+++ label_studio/taskgeneration/views.py
@@ -2,11 +2,22 @@ from django.shortcuts import render, redirect
 from django.db.models import Q
 from subjectannotation.views import parse_subject_presence_annotations
 from datetime import timedelta
+from tempfile import NamedTemporaryFile
+from sensordata.views import upload_sensor_data
+from django.conf import settings
+from django.apps import apps
+from django.http import HttpResponse
 
+import subprocess
+import os
+import pandas as pd
+import json
+
+from projects.models import Project
 from sensormodel.models import Deployment
 from sensordata.models import SensorData, SensorOffset
 from subjectannotation.models import SubjectPresence
-from taskgeneration.models import VideoImuOverlap
+from taskgeneration.models import SensorOverlap
 from taskgeneration.forms import TaskGenerationForm
 
 
@@ -14,89 +25,185 @@ def task_generation_page(request):
     taskgenerationform = TaskGenerationForm()
     return render(request, 'taskgeneration.html', {'taskgenerationform':taskgenerationform})
 
-def create_task_pairs(request, project, subject, duration):
+def create_task_pairs(request, project, subject, sensortype_B):
     # Load data for given project and subject
     subject_presences = SubjectPresence.objects.filter(project=project, subject=subject)
-    file_uploads = subject_presences.values('file_upload').distinct() # Get all unique files that contain subject
-    video_sensordata = SensorData.objects.filter(file_upload__in=file_uploads) # Get all SensorData related to these FileUploads
+    distinct_file_uploads = subject_presences.values('file_upload').distinct() # Get all unique files that contain subject
+    sensor_A_sensordata = SensorData.objects.filter(project=project,file_upload__in=distinct_file_uploads) # Get all SensorData related to these FileUploads
     # Load IMU deployments in the project that contain the subject
-    imu_deployments = Deployment.objects.filter(project=project,subject=subject,sensor__sensortype__sensortype='I')
-    imu_sensordata = SensorData.objects.filter(sensor__in=imu_deployments.values('sensor')) # find all imu sensordata that has a sensor in imu_deployments
-    # Iterate over unique video sensordata objects, this reduces computations
-    for video in video_sensordata:
-        vid_beg_dt = video.begin_datetime #begin_datetime video sensordata
-        vid_end_dt = video.end_datetime #end_datetime video sensordata
-        # Iterate over all subject_presences for this video
-        subject_presences_video = subject_presences.filter(file_upload=video.file_upload)
-        for subj_pres in subject_presences_video:
+    sensor_B_deployments = Deployment.objects.filter(project=project,subject=subject,sensor__sensortype__sensortype=sensortype_B.sensortype)
+    sensor_B_sensordata = SensorData.objects.filter(sensor__in=sensor_B_deployments.values('sensor')) # find all sensordata (type B) that has a sensor in sensor_B_deployments
+    # Iterate over unique sendata_A sensordata objects, this reduces computations
+    for sendata_A in sensor_A_sensordata:
+        vid_beg_dt = sendata_A.begin_datetime #begin_datetime sendata_A
+        # Iterate over all subject_presences for this sendata_A
+        subject_presences_sendata_A = subject_presences.filter(file_upload=sendata_A.file_upload)
+        for subj_pres in subject_presences_sendata_A:
             b = vid_beg_dt + timedelta(seconds=subj_pres.start_time) #begin_datetime of subj. pres. annotation
-            e = vid_end_dt + timedelta(seconds=subj_pres.end_time) #end_datetime of subj. pres. annotation
-            # Iterate over imu sensordata of imu's that have been deployed with subject
-            for imu_sens_dat in imu_sensordata:
-
-                imu_sensor = imu_sens_dat.sensor
-                camera_sensor = video.sensor
-                # Check if there is an offset between camera and IMU
-                if SensorOffset.objects.filter(sensor_A=camera_sensor,sensor_B=imu_sensor):
-                    # Take the latest instance of offset before the begin_datetime of the video sensordata 
-                    offset = SensorOffset.objects.filter(sensor_A=camera_sensor,sensor_B=imu_sensor,
+            e = vid_beg_dt + timedelta(seconds=subj_pres.end_time) #end_datetime of subj. pres. annotation
+            # Iterate over sensordata of type B that have been deployed with subject
+            for sendata_B in sensor_B_sensordata:
+                sensor_A = sendata_A.sensor
+                sensor_B = sendata_B.sensor
+                # Check if there is an offset between sensor_A and sensor_B
+                if SensorOffset.objects.filter(sensor_A=sensor_A,sensor_B=sensor_B):
+                    # Take the latest instance of offset before the begin_datetime of the sendata_A 
+                    offset = SensorOffset.objects.filter(sensor_A=sensor_A,sensor_B=sensor_B,
                                                         offset_Date__lt=vid_beg_dt).order_by('-offset_Date').first().offset
                 else:
                     # If there is no SensorOffset defined set offset=0
                     offset = 0
                 offset_delta = timedelta(milliseconds=offset) # Difference in datetime because of sensor offset
-                # Check if either the begin or end of imu are in the subj. pres. segment or the begin (of imu) is before and the end (of imu) is after the start of subj. pres.
-                begin_inside =  b <= imu_sens_dat.begin_datetime-offset_delta <= e
-                end_inside =   b <= imu_sens_dat.end_datetime-offset_delta <= e
-                begin_before_and_end_after_start =  (imu_sens_dat.begin_datetime-offset_delta <= b) and (imu_sens_dat.end_datetime-offset_delta >= b)
+                # Check if either the begin or end of sendata_B are in the subj. pres. segment or the begin (of sendata_B) is before and the end (of sendata_B) is after the start of subj. pres.
+                begin_inside =  b <= sendata_B.begin_datetime-offset_delta <= e
+                end_inside =   b <= sendata_B.end_datetime-offset_delta <= e
+                begin_before_and_end_after_start =  (sendata_B.begin_datetime-offset_delta <= b) and (sendata_B.end_datetime-offset_delta >= b)
                 if begin_inside or end_inside or begin_before_and_end_after_start:
                     # Find the start and end datetime of overlap
-                    if b >= imu_sens_dat.begin_datetime-offset_delta:
+                    if b >= sendata_B.begin_datetime-offset_delta:
                         begin_overlap_dt = b
                     else:
-                        begin_overlap_dt = imu_sens_dat.begin_datetime-offset_delta
-                    if e <= imu_sens_dat.end_datetime-offset_delta:
+                        begin_overlap_dt = sendata_B.begin_datetime-offset_delta
+                    if e <= sendata_B.end_datetime-offset_delta:
                         end_overlap_dt = e
                     else:
-                        end_overlap_dt = imu_sens_dat.end_datetime-offset_delta    
-                    # Determine amount of segments that fit inside overlap
-                    amount_of_segments = int((end_overlap_dt - begin_overlap_dt).total_seconds()//duration)
-                    for segment in range(amount_of_segments):
-                        begin_segment_dt = begin_overlap_dt + timedelta(seconds= duration*segment)
-                        end_segment_dt = begin_segment_dt + timedelta(seconds= duration)
-                        # Create overlap object for each segment, this is used to create tasks
-                        VideoImuOverlap.objects.create(video=video,
-                                                    imu=imu_sens_dat,
+                        end_overlap_dt = sendata_B.end_datetime-offset_delta    
+                    # Create overlap object, this is used to create tasks
+                    if not SensorOverlap.objects.filter(sensordata_A=sendata_A,
+                                                sensordata_B=sendata_B,
+                                                project=project,
+                                                subject=subject,
+                                                start_A= (begin_overlap_dt-vid_beg_dt).total_seconds(),
+                                                end_A = (end_overlap_dt-vid_beg_dt).total_seconds(),
+                                                start_B = (begin_overlap_dt-sendata_B.begin_datetime+offset_delta).total_seconds(),
+                                                end_B = (end_overlap_dt-sendata_B.begin_datetime+offset_delta).total_seconds()
+                                                ).exists():
+                        SensorOverlap.objects.create(sensordata_A=sendata_A,
+                                                    sensordata_B=sendata_B,
                                                     project=project,
                                                     subject=subject,
-                                                    start_video= (begin_segment_dt-vid_beg_dt).total_seconds(),
-                                                    end_video = (end_segment_dt-vid_beg_dt).total_seconds(),
-                                                    start_imu = (begin_segment_dt-imu_sens_dat.begin_datetime+offset_delta).total_seconds(),
-                                                    end_imu = (end_segment_dt-imu_sens_dat.begin_datetime+offset_delta).total_seconds()
-                                                   )
-            
+                                                    start_A= (begin_overlap_dt-vid_beg_dt).total_seconds(),
+                                                    end_A = (end_overlap_dt-vid_beg_dt).total_seconds(),
+                                                    start_B = (begin_overlap_dt-sendata_B.begin_datetime+offset_delta).total_seconds(),
+                                                    end_B = (end_overlap_dt-sendata_B.begin_datetime+offset_delta).total_seconds()
+                                                    )
+                    
         
 
-def create_annotation_data_chunks(request):
-    pass
+def create_annotation_data_chunks(request, project, subject, duration):
+    # Get all overlap for given subject and video
+    sensor_overlap = SensorOverlap.objects.filter(project=project, subject=subject)
+    # Find distinct video sensordata in all the overlap
+    distinct_sensor_data_A = sensor_overlap.values_list('sensordata_A').distinct()
+    distinct_sensor_data_ids = [sendata_A[0] for sendata_A in distinct_sensor_data_A]
+    # Iterate over distinct sensordata_A and find the sensordata_B with the longest overlap
+    for sendata_A_id in distinct_sensor_data_ids: 
+        all_overlap_for_sen_A  = sensor_overlap.filter(sensordata_A=sendata_A_id)
+        longest_duration = 0
+        for overlap_A in all_overlap_for_sen_A:
+            if overlap_A.end_A - overlap_A.start_A > longest_duration:
+                longest_duration = overlap_A.end_A - overlap_A.start_A
+                longest_overlap = overlap_A
 
-def parse_data_to_task(request):
-    pass
+
+        sensortype_A = longest_overlap.sensordata_A.sensor.sensortype.sensortype
+        sensortype_B = longest_overlap.sensordata_B.sensor.sensortype.sensortype
+        if sensortype_A == 'C' and sensortype_B == 'I':
+
+            # Determine amount of segments that fit inside overlap
+            amount_of_segments = int(longest_duration // duration) + (longest_duration % duration > 0)
+            for i, segment in enumerate(range(amount_of_segments)):
+                # Determine start and end of segment in seconds for both sensors
+                if i == amount_of_segments: # Remaining segment
+                    begin_segment_A = longest_overlap.start_A+ duration*segment
+                    end_segment_A = longest_overlap.end_A
+                    begin_segment_B = longest_overlap.start_B+ duration*segment
+                    end_segment_B = longest_overlap.end_B
+                else:
+                    begin_segment_A = longest_overlap.start_A+ duration*segment
+                    end_segment_A = begin_segment_A +duration
+                    begin_segment_B = longest_overlap.start_B+ duration*segment
+                    end_segment_B = begin_segment_B +duration
+
+    
+                with NamedTemporaryFile(suffix=".mp4", delete=False, mode='w') as temp_video,\
+                    NamedTemporaryFile(suffix=".csv", delete=False, mode='w') as temp_imu:
+                    video_file_path = longest_overlap.sensordata_A.file_upload.file.path
+                    ### Cut out video using ffmpeg ###
+                    ffmpeg_command = [
+                    "ffmpeg",
+                    "-y",
+                    "-i", video_file_path,
+                    "-ss", str(begin_segment_A),
+                    "-to", str(end_segment_A),
+                    "-c:v", "copy",
+                    "-c:a", "copy",
+                    temp_video.name
+                    ]
+                    subprocess.run(ffmpeg_command, shell=True)
+                    
+                    
+                    ### Cut out csv file using pandas ### 
+                    # Load data from SensorType and the csv file
+                    imu_file_path = longest_overlap.sensordata_B.file_upload.file.path
+                    timestamp_column = longest_overlap.sensordata_B.sensor.sensortype.timestamp_column
+                    imu_df = pd.read_csv(imu_file_path,skipfooter=1, engine='python')
+                    # Convert all time entries to float
+                    imu_df.iloc[:-1,timestamp_column] = imu_df.iloc[:-1,timestamp_column].astype(float)
+                    # Find the indeces of the timestamp instances closest to begin and end of segment
+                    start_index = imu_df[imu_df.iloc[:, timestamp_column]>= begin_segment_B].index[0] # First timestamp after begin_segment
+                    end_index = imu_df[imu_df.iloc[:, timestamp_column] > end_segment_B].index[1] # First timestamp after end_segment
+                    # Only keep the rows in between the obtained indeces
+                    segment_imu_df = imu_df.iloc[start_index:end_index]
+                    # Create temporary file and save new csv to this file
+                    segment_imu_df.to_csv(temp_imu, index=False)
+                    
+                    upload_sensor_data(request=request, name=f'imu_segment_{i}', file_path=temp_imu.name ,project=project)
+                    fileupload_model = apps.get_model(app_label='data_import', model_name='FileUpload')
+                    imu_file_upload = fileupload_model.objects.latest('id')
+                    upload_sensor_data(request=request, name=f'video_segment_{i}', file_path=temp_video.name ,project=project)               
+                    fileupload_model = apps.get_model(app_label='data_import', model_name='FileUpload')
+                    video_file_upload = fileupload_model.objects.latest('id')
+                    
+
+                    activity_annotation_project = Project.objects.get(id=project.id+2)
+                    task_json_template = {
+                        "csv": f"{imu_file_upload.file.url}?time=time_column&values=first_column",
+                        "video": f"<video src='{video_file_upload.file.url}' width='100%' controls onloadeddata=\"setTimeout(function(){{ts=Htx.annotationStore.selected.names.get('ts');t=ts.data.time_column;v=document.getElementsByTagName('video')[0];w=parseInt(t.length*(5/v.duration));l=t.length-w;ts.updateTR([t[0], t[w]], 1.001);r=$=>ts.brushRange.map(n=>(+n).toFixed(2));_=r();setInterval($=>r().some((n,i)=>n!==_[i])&&(_=r())&&(v.currentTime=v.duration*(r()[0]-t[0])/(t.slice(-1)[0]-t[0]-(r()[1]-r()[0]))),300); console.log('video is loaded, starting to sync with time series')}}, 3000); \" />"
+                    }
+                    with NamedTemporaryFile(prefix=f'segment_{i}_', suffix='.json',mode='w',delete=False) as task_json_file:
+                        json.dump(task_json_template,task_json_file,indent=4)
+
+                    upload_sensor_data(request, name=f'segment: {i}', file_path=task_json_file.name, project=activity_annotation_project)
+                os.remove(temp_imu.name)
+                os.remove(temp_video.name)                
+                os.remove(task_json_file.name)
+        else:
+            return HttpResponse('Sensor combination not yet supported ', content_type='text/plain')
 
 def generate_activity_tasks(request):
     if request.method == 'POST':
         taskgenerationform = TaskGenerationForm(request.POST)
-        if taskgenerationform.is_valid():
+        if taskgenerationform.is_valid() and request.session['']==True:
             # Get data from Form
             project = taskgenerationform.cleaned_data.get("project")
             subject = taskgenerationform.cleaned_data.get("subject")
             duration = taskgenerationform.cleaned_data.get("segment_duration")
+            sensortype_B = taskgenerationform.cleaned_data.get("sensortype_B")
+            if sensortype_B.sensortype == 'I':
+                # Step 1: Get columns names of sensortype
+                # Step 2: Go to new page where the user can select one of the column names in a dropdown
+                # Step 3: Save column name as variable
+
+                pass
             # Fill SubjectPresence objects
             parse_subject_presence_annotations(request= request,project=project)
-
-            create_task_pairs(request= request,project=project, subject=subject, duration=duration)
+            # Fill VideoImuOverlap objects
+            create_task_pairs(request= request,project=project, subject=subject,sensortype_B=sensortype_B)
+            # Create annotation data chunks (video and imu), this automatically creates tasks
+            create_annotation_data_chunks(request=request, project=project, subject=subject, duration=duration)
             
-    overlap = VideoImuOverlap.objects.all()      
+    overlap = SensorOverlap.objects.all()      
     return render(request, 'showoverlap.html', {'overlap':overlap})    
 
     
