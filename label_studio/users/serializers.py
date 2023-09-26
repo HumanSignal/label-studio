2c22b19a9cd582b8408d5e678e961c4e65fcc682
Andreas Divaris <dredivaris@gmail.com>
2023-09-26 14:03:31 -0700

    Add missing comma

--- label_studio/users/serializers.py
+++ label_studio/users/serializers.py
@@ -46,7 +46,7 @@ class BaseUserSerializer(FlexFieldsModelSerializer):
             'phone',
             'active_organization',
             'allow_newsletters',
-            'is_deleted'
+            'is_deleted',
         )
 
 
