53cbe7c3139242cb6c51ec8478c9e169768fcee3
Andreas Divaris <dredivaris@gmail.com>
2023-09-28 16:28:03 -0700

    Remove all fixture code to validate tests go green

--- label_studio/tests/conftest.py
+++ label_studio/tests/conftest.py
@@ -275,35 +275,6 @@ def mock_s3_resource_aes(mocker):
     mocker.patch('boto3.Session.resource', return_value=mock_s3_resource)
 
 
-def mock_put_kms(*args, **kwargs):
-    if (
-        'ServerSideEncryption' not in kwargs
-        or kwargs['ServerSideEncryption'] != 'aws:kms'
-        or 'SSEKMSKeyId' not in kwargs
-    ):
-        raise ClientError(
-            error_response={'Error': {'Code': 'AccessDenied', 'Message': 'Access Denied'}}, operation_name='PutObject'
-        )
-
-
-@pytest.fixture()
-def mock_s3_resource_kms(mocker):
-    mock_object = MagicMock()
-    mock_object.put = mock_put_kms
-
-    mock_object_constructor = MagicMock()
-    mock_object_constructor.return_value = mock_object
-
-    mock_s3_resource = MagicMock()
-    mock_s3_resource.Object = mock_object_constructor
-    resource = boto3.Session.resource
-    mocker.patch('boto3.Session.resource', new=MagicMock(return_value=mock_s3_resource))
-
-    yield
-
-    boto3.Session.resource = resource
-
-
 @pytest.fixture(autouse=True)
 def gcs_client():
     with gcs_client_mock():
