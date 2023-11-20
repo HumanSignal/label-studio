4d01c3a0cd15b354b0f7702234a2795a79da8122
Julio Sgarbi <julio.sgarbi@hotmail.com>
2023-11-20 15:19:11 +0100

    feat: LEAP-239: Move ls-frontend-test to monorepo and make cypress tests work (#5051)
    * feat: LEAP-54: Move editor e2e tests to the new LS monorepo
    
    * feat: LEAP-239: Move ls-frontend-test to monorepo and make cypress tests work
    
    * add cypress
    
    * move e2e folder to test folder
    
    * remove nx cypress and config default cypress
    
    * add the frontend-test as a lib
    
    * add frontend-test as a lib
    
    * fix cypress tests
    
    * remove dev build
    
    * delete package lock

---
+++ web/libs/frontend-test/src/cypress/support/e2e.ts
@@ -0,0 +1,8 @@
+// Custom commands can be executed with `cy.[command_name]`
+import './commands';
+
+import '@cypress/code-coverage/support';
+
+// Output spec steps
+require('cypress-terminal-report/src/installLogsCollector')();
+
