c29ff78043566fa66fee22c770ba729a678bea67
hlomzik <hlomzik@gmail.com>
2023-11-21 14:20:21 +0000

    Use $all-model-versions$ magic value

--- label_studio/frontend/src/pages/Settings/MachineLearningSettings/ProjectModelVersionSelector.js
+++ label_studio/frontend/src/pages/Settings/MachineLearningSettings/ProjectModelVersionSelector.js
@@ -1,8 +1,11 @@
 import { useCallback, useContext, useEffect, useState } from 'react';
 import { useAPI } from '../../../providers/ApiProvider';
-import { Button } from '../../../components';
 import { Form, Label, Select } from '../../../components/Form';
 import { ProjectContext } from '../../../providers/ProjectProvider';
+
+const ALL_MODEL_VERSIONS = "$all-model-versions$";
+const ALL_VERSIONS_OPTION = { value: ALL_MODEL_VERSIONS, label: "<All model versions>" };
+const EMPTY_VERSION = "<Empty string>";
 
 export const ProjectModelVersionSelector = ({
   name = "model_version",
@@ -12,47 +15,43 @@ export const ProjectModelVersionSelector = ({
   ...props
 }) => {
   const api = useAPI();
-  const { project, updateProject } = useContext(ProjectContext);
+  const { project } = useContext(ProjectContext);
   const [loading, setLoading] = useState(true);
-  const [versions, setVersions] = useState([]);
-  const [version, setVersion] = useState(null);
+  const [versions, setVersions] = useState([ALL_VERSIONS_OPTION]);
+  const [version, setVersion] = useState(project?.[valueName] ?? ALL_MODEL_VERSIONS);
 
   useEffect(() => {
-    setVersion(project?.[valueName] || null);
+    setVersion(project?.[valueName] || ALL_MODEL_VERSIONS);
   }, [project?.[valueName], versions]);
-
-  const resetMLVersion = useCallback(async (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-
-    setVersion(null);
-    await updateProject({
-      model_version: null,
-    });
-  }, [updateProject]);
 
   const fetchMLVersions = useCallback(async () => {
     const pk = project?.id;
 
     if (!pk) return;
 
-    const modelVersions = await api.callApi(apiName, {
-      params: {
-        pk,
-      },
-    });
+    const modelVersions = await api.callApi(apiName, { params: { pk } });
+    const versions = [ALL_VERSIONS_OPTION];
 
     if (modelVersions) {
-      setVersions(Object.entries(modelVersions).reduce((v, [key, value]) => [...v, {
-        value: key,
-        label: `${key} (${value} predictions)`,
-      }], []));
+      Object.entries(modelVersions).forEach(([key, value]) => {
+        if (key === ALL_MODEL_VERSIONS) return;
+        versions.push({ value: key, label: `${key || EMPTY_VERSION} (${value} predictions)` });
+      });
     }
 
+    setVersions(versions);
     setLoading(false);
   }, [project?.id, apiName]);
 
   useEffect(fetchMLVersions, [fetchMLVersions]);
+
+  let version_text = null;
+
+  if (version === ALL_MODEL_VERSIONS || version === null) {
+    version_text = <b>All model versions will be used</b>;
+  } else {
+    version_text = <b>Current project model version: {version || EMPTY_VERSION}</b>;
+  }
 
   return (
     <Form.Row columnCount={1}>
@@ -61,36 +60,34 @@ export const ProjectModelVersionSelector = ({
         description={(
           <>
             Model version allows you to specify which prediction will be shown to the annotators.
-            {version && (
-              <>
-                <br />
-                <b>Current project model version: {version}</b>
-              </>
-            )}
+            <br />
+            {version_text}
           </>
         )}
         style={{ marginTop: 16 }}
         large
       />
 
-      <div style={{ display: 'flex', alignItems: 'center', width: 400, paddingLeft: 16 }}>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          width: 400,
+          paddingLeft: 16,
+        }}
+      >
         <div style={{ flex: 1, paddingRight: 16 }}>
           <Select
             name={name}
             disabled={!versions.length}
             value={version}
-            onChange={e => setVersion(e.target.value)}
+            onChange={(e) => setVersion(e.target.value)}
             options={versions}
             placeholder={loading ? "Loading ..." : placeholder}
             {...props}
           />
         </div>
-
-        <Button onClick={resetMLVersion}>
-          Reset
-        </Button>
       </div>
     </Form.Row>
   );
 };
-
