c521fd388d485367d46c3e92af0d1ca3d86f0041
Nikita Belonogov <nikita.belonogov@humansignal.com>
2023-11-23 15:12:21 +0000

    [submodules] Copy src HumanSignal/label-studio-frontend
    Workflow run: https://github.com/HumanSignal/label-studio/actions/runs/6971479552

--- web/libs/editor/src/types/React.d.ts
+++
@@ -1,8 +0,0 @@
-import "react";
-
-type CustomProp = { [key in `--${string}`]: string };
-
-declare module "react" {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  export interface CSSProperties extends CustomProp {}
-}
