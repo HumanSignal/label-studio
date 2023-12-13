1e857968bfd4ed428787db2cc3113a7351379d70
Yousif Yassi <yousif@heartex.com>
2023-12-13 06:34:07 -0400

    feat: DIA-787: Add Zendesk widget

--- label_studio/data_manager/views.py
+++ label_studio/data_manager/views.py
@@ -1,5 +1,6 @@
 """This file and its contents are licensed under the Apache License 2.0. Please see the included NOTICE for copyright information and LICENSE for a copy of the license.
 """
+from core.feature_flags import flag_set
 from core.utils.common import find_editor_files
 from core.version import get_short_version
 from django.contrib.auth.decorators import login_required
@@ -8,6 +9,10 @@ from django.shortcuts import render
 
 @login_required
 def task_page(request, pk):
-    response = {'version': get_short_version()}
+    ff_zendesk_widget = flag_set('fflag_feat_dia_787_zendesk_widget_integration', user='auto')
+    response = {
+        'version': get_short_version(), 
+        'ff_zendesk_widget': ff_zendesk_widget,
+    }
     response.update(find_editor_files())
     return render(request, 'base.html', response)
