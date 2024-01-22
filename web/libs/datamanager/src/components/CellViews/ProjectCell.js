ef3c5aa5cce6c224dd6a0e0de296d2b110931efa
yyassi-heartex <104568407+yyassi-heartex@users.noreply.github.com>
2024-01-22 23:08:26 +0000

    [submodules] Copy src HumanSignal/dm2
    Workflow run: https://github.com/HumanSignal/label-studio/actions/runs/7618518612

--- web/libs/datamanager/src/components/CellViews/ProjectCell.js
+++ web/libs/datamanager/src/components/CellViews/ProjectCell.js
@@ -1,25 +1,13 @@
 import { getRoot } from "mobx-state-tree";
-import { Fragment } from "react";
-
-const ProjectLink = ({ project }) => {
-  const projectID = project.id;
-  const onClick = (e) => {
-    e.stopPropagation();
-
-  };
-
-  return (
-    <a href={`/projects/${projectID}/data`} onClick={onClick}>
-      {project.title}
-    </a>
-  );
-};
 
 export const ProjectCell = (cell) => {
   const { original, value } = cell;
   const root = getRoot(original);
+  // TODO: turn this into a link to open the project later
   const projectList = value
-    .map(projectRef => root.taskStore.associatedList.find(proj => proj.id === projectRef.project_id))
+    .map((projectRef) => (
+      root.taskStore.associatedList.find(proj => proj.id === projectRef.project_id)?.title
+    ))
     .filter(Boolean);
 
   return (
@@ -31,15 +19,7 @@ export const ProjectCell = (cell) => {
         lineHeight: "16px",
       }}
     >
-      {projectList && (
-        projectList
-          .map((projectRef, index) => (
-            <Fragment key={projectRef.project_id}>
-              {index > 0 && ", "}
-              <ProjectLink project={projectRef} />
-            </Fragment>
-          ))
-      )}
+      {projectList && projectList.join(", ")}
     </div>
   );
 };
