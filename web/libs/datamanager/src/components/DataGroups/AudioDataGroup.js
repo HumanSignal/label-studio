c980f5cfef17003bd6fb07c2c962ba15002534da
Yousif Yassi <yousif@heartex.com>
2024-01-30 19:02:35 +0000

    [submodules] Copy src HumanSignal/dm2
    Workflow run: https://github.com/HumanSignal/label-studio/actions/runs/7716119703

--- web/libs/datamanager/src/components/DataGroups/AudioDataGroup.js
+++ web/libs/datamanager/src/components/DataGroups/AudioDataGroup.js
@@ -1,8 +1,14 @@
 import { MediaPlayer } from "../Common/MediaPlayer/MediaPlayer";
 
 export const AudioDataGroup = ({ value }) => {
+  const style = {
+    padding: 10, 
+    height: AudioDataGroup.height, 
+    boxSizing: "content-box",
+  };
+
   return (
-    <div style={{ padding: 10, height: AudioDataGroup.height, boxSizing: "content-box" }}>
+    <div style={style}>
       <MediaPlayer src={value} />
     </div>
   );
