3f0ff7468f25d2a55e46c618294ec228e3ef5a93
hakan458 <hakan@heartex.com>
2024-02-08 15:24:05 -0800

    feat: DIA-851: Add ModelVersion

--- label_studio/ml_models/models.py
+++ label_studio/ml_models/models.py
@@ -4,6 +4,7 @@
 from django.conf import settings
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from ml_model_providers.models import ModelProviderConnection
 
 
 class ModelInterface(models.Model):
@@ -26,3 +27,52 @@ class ModelInterface(models.Model):
 
     def has_permission(self, user):
         return user.active_organization == self.organization
+
+
+class ModelVersion(models.Model):
+    class Meta:
+        abstract = True
+
+    title = models.CharField(_('title'), max_length=500, null=False, blank=False, help_text='Model name')
+
+    parent_model = models.ForeignKey(ModelInterface, related_name='model_versions', on_delete=models.CASCADE)
+
+    # TODO add field containing model run IDs
+
+    prompt = models.TextField(_('prompt'), null=False, blank=False, help_text='Prompt to execute')
+
+
+class ThirdPartyModelVersion(ModelVersion):
+
+    provider = models.CharField(
+        max_length=255,
+        choices=ModelProviderConnection.ModelProviders.choices,
+        default=ModelProviderConnection.ModelProviders.OPENAI,
+        help_text='The model provider to use e.g. OpenAI',
+    )
+
+    provider_model_id = models.CharField(
+        max_length=255,
+        blank=False,
+        null=False,
+        help_text='The model ID to use within the given provider, e.g. gpt-3.5',
+    )
+
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='created_third_party_model_versions',
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+
+    created_at = models.DateTimeField(_('created at'), auto_now_add=True)
+
+    updated_at = models.DateTimeField(_('updated at'), auto_now=True)
+
+    organization = models.ForeignKey(
+        'organizations.Organization', on_delete=models.CASCADE, related_name='third_party_model_versions', null=True
+    )
+
+    def has_permission(self, user):
+        return user.active_organization == self.organization
+
