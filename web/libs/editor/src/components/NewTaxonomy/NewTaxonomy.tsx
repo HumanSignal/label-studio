f902c94fed1821a93d98f0170a2fde5e857a7299
Yousif Yassi <yousif@heartex.com>
2024-02-08 20:13:52 -0500

    changing treeExpandAction back to click

--- web/libs/editor/src/components/NewTaxonomy/NewTaxonomy.tsx
+++ web/libs/editor/src/components/NewTaxonomy/NewTaxonomy.tsx
@@ -191,7 +191,7 @@ const NewTaxonomy = ({
       }}
       treeCheckStrictly
       showCheckedStrategy={TreeSelect.SHOW_ALL}
-      treeExpandAction={false}
+      treeExpandAction={"click"}
       dropdownMatchSelectWidth={dropdownWidth}
       placeholder={options.placeholder || 'Click to add...'}
       style={style}
