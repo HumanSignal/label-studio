99b0d65f5b0802b9a2c614eb926abc6ff74a3bf2
hlomzik <hlomzik@gmail.com>
2024-02-22 00:59:26 +0000

    [submodules] Copy src HumanSignal/label-studio-frontend
    Workflow run: https://github.com/HumanSignal/label-studio/actions/runs/7997800769

--- web/libs/editor/src/tags/control/TextArea/TextArea.js
+++ web/libs/editor/src/tags/control/TextArea/TextArea.js
@@ -239,7 +239,7 @@ const Model = types.model({
       currentArea?.notifyDrawingFinished();
     },
 
-    validateValue(text) {
+    validateText(text) {
       if (isFF(FF_LSDV_4659) && self.skipduplicates && self.hasResult(text)) {
         self.uniqueModal();
         return false;
@@ -248,7 +248,7 @@ const Model = types.model({
     },
 
     addText(text, pid) {
-      if (!self.validateValue(text)) return;
+      if (!self.validateText(text)) return;
 
       self.createRegion(text, pid, self.leadTime);
       // actually creates a new result
@@ -280,7 +280,7 @@ const Model = types.model({
     },
 
     addTextToResult(text, result) {
-      if (!self.validateValue(text)) return;
+      if (!self.validateText(text)) return;
 
       const newValue = result.mainValue.toJSON();
 
