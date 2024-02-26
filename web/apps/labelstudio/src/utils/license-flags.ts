fdca66e6f1b074f8d374f761444e20979d5dd671
Yousif Yassi <yousif@heartex.com>
2024-02-26 13:38:32 -0500

    cleaning up line endings

--- web/apps/labelstudio/src/utils/license-flags.ts
+++ web/apps/labelstudio/src/utils/license-flags.ts
@@ -8,4 +8,3 @@ export const LF_CLOUD_STORAGE_FOR_MANAGERS = "hide_storage_settings_for_manager"
 export function isInLicense(id: string) {
   return isFlagEnabled(id, window.APP_SETTINGS?.flags || {});
 }
-
