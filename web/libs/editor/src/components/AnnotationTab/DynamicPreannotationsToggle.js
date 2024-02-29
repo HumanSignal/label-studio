aabea3aa10d352be72285fd388fd17517f07c0ee
kla101 <kla101@uclive.ac.nz>
2024-03-01 11:12:34 +1300

    test commit

--- web/libs/editor/src/components/AnnotationTab/DynamicPreannotationsToggle.js
+++ web/libs/editor/src/components/AnnotationTab/DynamicPreannotationsToggle.js
@@ -46,7 +46,7 @@ export const DynamicPreannotationsToggle = injector(observer(({
                 ToolsManager.allInstances().forEach(inst => inst.selectDefault());
               }
             }}
-            label="Auto-Annotation"
+            label="Auto-Annotation0"
             style={{ color: '#7F64FF' }}
           />
           {suggestions.size > 0 && (
