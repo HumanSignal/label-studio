9ef194b3f081ec328c726c03c625aef07ab1cc71
Brandon Martel <brandonmartel@gmail.com>
2024-03-11 12:35:41 -0500

    Running a formatter/lint on the code

--- web/apps/labelstudio/src/components/EmptyState/EmptyState.js
+++ web/apps/labelstudio/src/components/EmptyState/EmptyState.js
@@ -1,20 +1,14 @@
-import { FC, ReactElement } from "react";
-import { BemWithSpecifiContext } from '../../utils/bem';
-import "./EmptyState.styl";
+import { Block, Elem } from '../../utils/bem';
+import './EmptyState.styl';
 
-const { Block, Elem } = BemWithSpecifiContext();
-
-
-const EmptyState = ({ icon, title, description, action, footer }) => {
+export const EmptyState = ({ icon, title, description, action, footer }) => {
   return (
-      <Block name="empty-state-default" tag="div">	  
-	  { icon && <Elem name="icon">{ icon }</Elem> }
-	  { title && <Elem name="title">{ title }</Elem> }
-	  { description && <Elem name="description">{ description }</Elem> }
-	  { action && <Elem name="action">{ action }</Elem> }
-	  { footer && <Elem name="footer">{ footer }</Elem> }
+    <Block name="empty-state-default" tag="div">
+      {icon && <Elem name="icon">{icon}</Elem>}
+      {title && <Elem name="title">{title}</Elem>}
+      {description && <Elem name="description">{description}</Elem>}
+      {action && <Elem name="action">{action}</Elem>}
+      {footer && <Elem name="footer">{footer}</Elem>}
     </Block>
   );
 };
-
-export { EmptyState };
