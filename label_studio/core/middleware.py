54214d49b1bbbb3fe06b6eed1d454b7553247900
Jo Booth <jo.m.booth@gmail.com>
2024-03-11 17:40:01 -0400

    remove example pasted from django docs :facepalm:

--- label_studio/core/middleware.py
+++ label_studio/core/middleware.py
@@ -159,23 +159,6 @@ class DatabaseIsLockedRetryMiddleware(CommonMiddleware):
         return response
 
 
-class SimpleMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-        # One-time configuration and initialization.
-
-    def __call__(self, request):
-        # Code to be executed for each request before
-        # the view (and later middleware) are called.
-
-        response = self.get_response(request)
-
-        # Code to be executed for each request/response after
-        # the view is called.
-
-        return response
-
-
 class XApiKeySupportMiddleware:
     """Middleware that adds support for the X-Api-Key header, by having its value supersede
     anything that's set in the Authorization header."""
