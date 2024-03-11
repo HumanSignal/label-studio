a644e14626ce7e38b70a11b4bf1800d3bede7135
Brandon Martel <brandonmartel@gmail.com>
2024-03-11 12:45:22 -0500

    Running a formatter/lint on the code

--- web/apps/labelstudio/src/components/Form/Elements/Select/Select.js
+++ web/apps/labelstudio/src/components/Form/Elements/Select/Select.js
@@ -1,11 +1,16 @@
-import React, {useEffect, useMemo, useState} from 'react';
-import {cn} from '../../../../utils/bem';
-import {FormField} from '../../FormField';
-import {default as Label} from '../Label/Label';
+import React, { useEffect, useMemo, useState } from 'react';
+import { cn } from '../../../../utils/bem';
+import { FormField } from '../../FormField';
+import { default as Label } from '../Label/Label';
 import './Select.styl';
 
-
-const SelectOption = ({value, label, disabled = false, hidden = false, ...props}) => {
+const SelectOption = ({
+  value,
+  label,
+  disabled = false,
+  hidden = false,
+  ...props
+}) => {
   return (
     <option value={value} disabled={disabled} hidden={hidden} {...props}>
       {label ?? value}
@@ -13,23 +18,40 @@ const SelectOption = ({value, label, disabled = false, hidden = false, ...props}
   );
 };
 
-const Select = ({label, className, options, validate, required, skip, labelProps, groupProps, ghost, ...props}) => {
+const Select = ({
+  label,
+  className,
+  options,
+  validate,
+  required,
+  skip,
+  labelProps,
+  groupProps,
+  ghost,
+  ...props
+}) => {
   const rootClass = cn('select');
-  const initialValue = useMemo(() => props.value ?? "", [props.value]);
+  const initialValue = useMemo(() => props.value ?? '', [props.value]);
   const [value, setValue] = useState(initialValue);
 
   let grouped = options.reduce((groupedOptions, option) => {
-
     const key = option.group || 'NoGroup'; // fallback group for items without a group property
+
     (groupedOptions[key] = groupedOptions[key] || []).push(option);
     return groupedOptions;
   }, {});
 
   const renderOptions = (option) => {
-    return <SelectOption {...(option.value ? { ...option, key: option.value } : { value: option, key: option })} />;
+    return (
+      <SelectOption
+        {...(option.value
+          ? { ...option, key: option.value }
+          : { value: option, key: option })}
+      />
+    );
   };
 
-  const classList = rootClass.mod({ghost}).mix(className);
+  const classList = rootClass.mod({ ghost }).mix(className);
 
   useEffect(() => {
     setValue(initialValue);
@@ -42,35 +64,37 @@ const Select = ({label, className, options, validate, required, skip, labelProps
       validate={validate}
       required={required}
       skip={skip}
-      setValue={val => setValue(val)}
+      setValue={(val) => setValue(val)}
       {...props}
     >
-      {ref => {
+      {(ref) => {
         return (
           <div className={classList}>
             <select
               {...props}
               value={value}
               onChange={(e) => {
-                setValue(e.target.value),
-                  props.onChange?.(e);
+                setValue(e.target.value);
+                props.onChange?.(e);
               }}
               ref={ref}
               className={rootClass.elem('list')}
             >
               {props.placeholder && (!props.defaulValue || !props.value) && (
-                <option value="" disabled hidden>{props.placeholder}</option>
+                <option value="" disabled hidden>
+                  {props.placeholder}
+                </option>
               )}
 
-              {
-                Object.keys(grouped).map((group) => {
-                  return (group === 'NoGroup') ?
-                    grouped[group].map(renderOptions) :
-                    <optgroup label={group}>
-                      {grouped[group].map(renderOptions)}
-                    </optgroup>
-                })
-              }
+              {Object.keys(grouped).map((group) => {
+                return group === 'NoGroup' ? (
+                  grouped[group].map(renderOptions)
+                ) : (
+                  <optgroup label={group}>
+                    {grouped[group].map(renderOptions)}
+                  </optgroup>
+                );
+              })}
             </select>
           </div>
         );
@@ -78,8 +102,13 @@ const Select = ({label, className, options, validate, required, skip, labelProps
     </FormField>
   );
 
-  return label ?
-    <Label {...(labelProps ?? {})} text={label} required={required}>{selectWrapper}</Label> : selectWrapper;
+  return label ? (
+    <Label {...(labelProps ?? {})} text={label} required={required}>
+      {selectWrapper}
+    </Label>
+  ) : (
+    selectWrapper
+  );
 };
 
 export default Select;
