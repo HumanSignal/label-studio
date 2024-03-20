51eede32175a4bdf9b720ad5c888de97ff31e364
hlomzik <hlomzik@gmail.com>
2024-03-20 14:10:06 +0000

    [submodules] Copy src HumanSignal/label-studio-frontend
    Workflow run: https://github.com/HumanSignal/label-studio/actions/runs/8360285462

--- web/libs/editor/src/tools/Brush.js
+++ web/libs/editor/src/tools/Brush.js
@@ -53,6 +53,9 @@ const _Tool = types
     group: 'segmentation',
     shortcut: 'B',
     smart: true,
+
+    // Support the existing unselect behavior until the Magic Wand feature flag is on by default.
+    // @todo change to false once the Magic Wand is on by default.
     unselectRegionOnToolChange: isFF(FF_DEV_4081) ? false : true,
   })
   .volatile(() => ({
