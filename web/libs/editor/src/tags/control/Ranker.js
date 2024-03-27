3b401a909c98cc0aa95e89812da379a10e1716be
makseq-ubnt <makseq@gmail.com>
2024-03-27 04:09:33 +0000

    docs: Fix misspells in Ranker and List docs

--- web/libs/editor/src/tags/control/Ranker.js
+++ web/libs/editor/src/tags/control/Ranker.js
@@ -32,14 +32,15 @@ const ORIGINAL_ITEMS_KEY = '_';
  *   <Ranker name="rank" toName="results" />
  * </View>
  * @example
- * <!-- Example data and result for Ranker tag -->
+ * <!-- Example task data for Ranker tag -->
  * {
  *   "items": [
  *     { "id": "blog", "title": "10 tips to write a better function", "body": "There is nothing worse than being left in the lurch when it comes to writing a function!" },
  *     { "id": "mdn", "title": "Arrow function expressions", "body": "An arrow function expression is a compact alternative to a traditional function" },
- *     { "id": "wiki", "title": "Arrow (computer science)", "body": "In computer science, arrows or bolts are a type class..." },
+ *     { "id": "wiki", "title": "Arrow (computer science)", "body": "In computer science, arrows or bolts are a type class..." }
  *   ]
  * }
+ * <!-- Example result for Ranker tag -->
  * {
  *   "from_name": "rank",
  *   "to_name": "results",
