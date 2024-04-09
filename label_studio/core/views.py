51603b1434879c7296f2a377a04a540067f34e46
makseq-ubnt <makseq@gmail.com>
2024-04-09 02:33:31 +0100

    linter

--- label_studio/core/views.py
+++ label_studio/core/views.py
@@ -36,7 +36,6 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 
-
 logger = logging.getLogger(__name__)
 
 
