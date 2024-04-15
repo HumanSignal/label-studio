16b7952dfc234d420f2563d0b2bbac2b60254de4
niklub <nikolai@heartex.com>
2024-04-15 13:11:06 +0100

    Update lsf-sdk.js

--- web/libs/datamanager/src/sdk/lsf-sdk.js
+++ web/libs/datamanager/src/sdk/lsf-sdk.js
@@ -443,7 +443,7 @@ export class LSFWrapper {
     } else {
       if (this.annotations.length === 0 && this.predictions.length > 0 && !this.isInteractivePreannotations) {
         const predictionByModelVersion = this.predictions.find(p => p.createdBy === this.project.model_version);
-        annotation = cs.addAnnotationFromPrediction(predictionByModelVersion);
+        annotation = cs.addAnnotationFromPrediction(predictionByModelVersion ?? this.predictions[0]);
       } else if (this.annotations.length > 0 && id && id !== "auto") {
         annotation = this.annotations.find((c) => c.pk === id || c.id === id);
       } else if (this.annotations.length > 0 && (id === "auto" || hasAutoAnnotations)) {
