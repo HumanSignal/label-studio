2ca366c71616ffe2a58694407f7f04d35578a675
hlomzik <hlomzik@gmail.com>
2024-04-16 01:27:09 +0100

    Fix loading state and conditions
    - don't display Auto-Accept panel for not supported tags
    - display spinner while request is in progress
    - also dim the toggle if it's there, but don't disable it

--- web/libs/editor/src/components/AnnotationTab/AutoAcceptToggle.js
+++ web/libs/editor/src/components/AnnotationTab/AutoAcceptToggle.js
@@ -25,32 +25,40 @@ export const AutoAcceptToggle = injector(observer(({
   annotation,
   suggestions,
 }) => {
-  return store.autoAnnotation && !store.forceAutoAcceptSuggestions ? (
+  if (!store.autoAnnotation) return null;
+
+  const withSuggestions = annotation.hasSuggestionsSupport && !store.forceAutoAcceptSuggestions;
+  const loading = store.awaitingSuggestions;
+
+  return (
     <Block name="auto-accept">
-      <Elem name="wrapper">
-        <Space spread>
-          {suggestions.size > 0 ? (
-            <Space size="small">
-              <Elem name="info">
-                {suggestions.size} suggestion{suggestions.size > 0 && 's'}
-              </Elem>
-              <Elem name="action" tag={Button} mod={{ type: 'reject' }} onClick={() => annotation.rejectAllSuggestions()}>
-                <IconCross/>
-              </Elem>
-              <Elem name="action" tag={Button} mod={{ type: 'accept' }} onClick={() => annotation.acceptAllSuggestions()}>
-                <IconCheck/>
-              </Elem>
-            </Space>
-          ) : (
-            <Toggle
-              checked={store.autoAcceptSuggestions}
-              onChange={(e) => store.setAutoAcceptSuggestions(e.target.checked)}
-              label="Auto-Accept Suggestions"
-              style={{ color: '#7F64FF' }}
-            />
-          )}
-        </Space>
-      </Elem>
+      {withSuggestions && (
+        <Elem name="wrapper" mod={{ loading }}>
+          <Space spread>
+            {suggestions.size > 0 ? (
+              <Space size="small">
+                <Elem name="info">
+                  {suggestions.size} suggestion{suggestions.size > 0 && 's'}
+                </Elem>
+                <Elem name="action" tag={Button} mod={{ type: 'reject' }} onClick={() => annotation.rejectAllSuggestions()}>
+                  <IconCross/>
+                </Elem>
+                <Elem name="action" tag={Button} mod={{ type: 'accept' }} onClick={() => annotation.acceptAllSuggestions()}>
+                  <IconCheck/>
+                </Elem>
+              </Space>
+            ) : (
+              <Toggle
+                checked={store.autoAcceptSuggestions}
+                onChange={(e) => store.setAutoAcceptSuggestions(e.target.checked)}
+                label="Auto-Accept Suggestions"
+                style={{ color: '#7F64FF' }}
+              />
+            )}
+          </Space>
+        </Elem>
+      )}
+      {loading && <Elem name="spinner" />}
     </Block>
-  ) : null;
+  );
 }));
