de5ad0dc2a1b0a4b4c9edcfbe104c164f3e7b0d3
François Ponchon <fponchon@live.com>
2024-04-23 07:44:02 +0000

    Docker Build fix

--- deploy/docker-entrypoint.sh
+++ deploy/docker-entrypoint.sh
@@ -69,7 +69,7 @@ if [ "$1" = "nginx" ]; then
   # in this mode we're running in a separate container
   export APP_HOST=${APP_HOST:=app}
   exec_entrypoint "$ENTRYPOINT_PATH/nginx/"
-  exec nginx -c $OPT_DIR/nginx/nginx.conf -e /dev/stderr
+  exec nginx -c $OPT_DIR/nginx/nginx.conf
 elif [ "$1" = "label-studio-uwsgi" ]; then
   exec_entrypoint "$ENTRYPOINT_PATH/app/"
   exec_or_wrap_n_exec uwsgi --ini /label-studio/deploy/uwsgi.ini
