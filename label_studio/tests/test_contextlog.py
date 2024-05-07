83dc71c6dd2ea341f03a1af284476aa39df75c5a
sajarin <sajarindider@gmail.com>
2024-05-07 15:45:19 -0400

    add back django_override settings

--- label_studio/tests/test_contextlog.py
+++ label_studio/tests/test_contextlog.py
@@ -2,12 +2,13 @@ import json
 
 import pytest
 import responses
+from django.test import override_settings
 
 
 @responses.activate
 @pytest.mark.django_db
-def test_contextlog(business_client, settings):
-    settings.CONTEXTLOG_SYNC = True
+@override_settings(CONTEXTLOG_SYNC=True)
+def test_contextlog(business_client):
     responses.add(
         responses.POST,
         'https://tele.labelstud.io',
