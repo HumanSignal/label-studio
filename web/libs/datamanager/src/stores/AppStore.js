6314e338c016a09875f09b9a2e7ed2e5737f5b35
hlomzik <hlomzik@gmail.com>
2024-05-08 13:46:29 +0100

    fix actual error

--- web/libs/datamanager/src/stores/AppStore.js
+++ web/libs/datamanager/src/stores/AppStore.js
@@ -154,7 +154,7 @@ export const AppStore = types
 
     beforeDestroy() {
       clearTimeout(self._poll);
-      self.viewsStore?.forEach((view) => view.destroy());
+      self.viewsStore?.forEach?.((view) => view.destroy());
       window.removeEventListener("popstate", self.handlePopState);
       networkActivity.destroy();
     },
