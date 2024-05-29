60e82bffeb43204083a2dd30f2b0d5c0bbb8d31c
hlomzik <hlomzik@gmail.com>
2024-05-29 18:56:03 +0100

    Add FF for new Reviewer Flow

--- web/libs/editor/src/components/BottomBar/Controls.jsx
+++ web/libs/editor/src/components/BottomBar/Controls.jsx
@@ -4,7 +4,7 @@ import { Tooltip } from "../../common/Tooltip/Tooltip";
 import { Block, Elem } from "../../utils/bem";
 import { isDefined } from "../../utils/utilities";
 import { IconBan } from "../../assets/icons";
-import { FF_PROD_E_111, isFF } from "../../utils/feature-flags";
+import { FF_PROD_E_111, FF_REVIEWER_FLOW, isFF } from "../../utils/feature-flags";
 import "./Controls.styl";
 import { useCallback, useMemo, useState } from "react";
 import { LsChevron } from "../../assets/icons";
