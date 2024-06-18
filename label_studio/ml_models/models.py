7feb77bc7f5a7f033cdb29ba57602cc5db0c5b89
Brandon Martel <brandonmartel@gmail.com>
2024-06-18 17:44:42 -0500

    feat: OPTIC-791: Update model run stats to be based on total tasks, predictions, and ground truths rather than the current page of tasks

--- label_studio/ml_models/models.py
+++ label_studio/ml_models/models.py
@@ -1,5 +1,4 @@
-"""This file and its contents are licensed under the Apache License 2.0. Please see the included NOTICE for copyright information and LICENSE for a copy of the license.
-"""
+"""This file and its contents are licensed under the Apache License 2.0. Please see the included NOTICE for copyright information and LICENSE for a copy of the license."""
 
 from django.conf import settings
 from django.db import models
@@ -12,36 +11,35 @@ from tasks.models import Annotation, Prediction
 
 def validate_string_list(value):
     if not value:
-        raise ValidationError('list should not be empty')
+        raise ValidationError("list should not be empty")
     if not isinstance(value, list):
-        raise ValidationError('Value must be a list')
+        raise ValidationError("Value must be a list")
     if not all(isinstance(item, str) for item in value):
-        raise ValidationError('All items in the list must be strings')
+        raise ValidationError("All items in the list must be strings")
 
 
 class ModelInterface(models.Model):
+    title = models.CharField(_("title"), max_length=500, null=False, blank=False, help_text="Model name")
 
-    title = models.CharField(_('title'), max_length=500, null=False, blank=False, help_text='Model name')
-
-    description = models.TextField(_('description'), null=True, blank=True, help_text='Model description')
+    description = models.TextField(_("description"), null=True, blank=True, help_text="Model description")
 
     created_by = models.ForeignKey(
-        settings.AUTH_USER_MODEL, related_name='created_models', on_delete=models.SET_NULL, null=True
+        settings.AUTH_USER_MODEL, related_name="created_models", on_delete=models.SET_NULL, null=True
     )
 
-    created_at = models.DateTimeField(_('created at'), auto_now_add=True)
+    created_at = models.DateTimeField(_("created at"), auto_now_add=True)
 
-    updated_at = models.DateTimeField(_('updated at'), auto_now=True)
+    updated_at = models.DateTimeField(_("updated at"), auto_now=True)
 
     organization = models.ForeignKey(
-        'organizations.Organization', on_delete=models.CASCADE, related_name='model_interfaces', null=True
+        "organizations.Organization", on_delete=models.CASCADE, related_name="model_interfaces", null=True
     )
 
     input_fields = models.JSONField(default=list, validators=[validate_string_list])
 
     output_classes = models.JSONField(default=list, validators=[validate_string_list])
 
-    associated_projects = models.ManyToManyField('projects.Project', blank=True)
+    associated_projects = models.ManyToManyField("projects.Project", blank=True)
 
     def has_permission(self, user):
         return user.active_organization == self.organization
@@ -51,15 +49,15 @@ class ModelVersion(models.Model):
     class Meta:
         abstract = True
 
-    title = models.CharField(_('title'), max_length=500, null=False, blank=False, help_text='Model name')
+    title = models.CharField(_("title"), max_length=500, null=False, blank=False, help_text="Model name")
 
-    parent_model = models.ForeignKey(ModelInterface, related_name='model_versions', on_delete=models.CASCADE)
+    parent_model = models.ForeignKey(ModelInterface, related_name="model_versions", on_delete=models.CASCADE)
 
-    prompt = models.TextField(_('prompt'), null=False, blank=False, help_text='Prompt to execute')
+    prompt = models.TextField(_("prompt"), null=False, blank=False, help_text="Prompt to execute")
 
     @property
     def full_title(self):
-        return f'{self.parent_model.title}__{self.title}'
+        return f"{self.parent_model.title}__{self.title}"
 
     def delete(self, *args, **kwargs):
         """
@@ -72,34 +70,33 @@ class ModelVersion(models.Model):
 
 
 class ThirdPartyModelVersion(ModelVersion):
-
     provider = models.CharField(
         max_length=255,
         choices=ModelProviderConnection.ModelProviders.choices,
         default=ModelProviderConnection.ModelProviders.OPENAI,
-        help_text='The model provider to use e.g. OpenAI',
+        help_text="The model provider to use e.g. OpenAI",
     )
 
     provider_model_id = models.CharField(
         max_length=255,
         blank=False,
         null=False,
-        help_text='The model ID to use within the given provider, e.g. gpt-3.5',
+        help_text="The model ID to use within the given provider, e.g. gpt-3.5",
     )
 
     created_by = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        related_name='created_third_party_model_versions',
+        related_name="created_third_party_model_versions",
         on_delete=models.SET_NULL,
         null=True,
     )
 
-    created_at = models.DateTimeField(_('created at'), auto_now_add=True)
+    created_at = models.DateTimeField(_("created at"), auto_now_add=True)
 
-    updated_at = models.DateTimeField(_('updated at'), auto_now=True)
+    updated_at = models.DateTimeField(_("updated at"), auto_now=True)
 
     organization = models.ForeignKey(
-        'organizations.Organization', on_delete=models.CASCADE, related_name='third_party_model_versions', null=True
+        "organizations.Organization", on_delete=models.CASCADE, related_name="third_party_model_versions", null=True
     )
 
     def has_permission(self, user):
@@ -108,32 +105,32 @@ class ThirdPartyModelVersion(ModelVersion):
 
 class ModelRun(models.Model):
     class ProjectSubset(models.TextChoices):
-        ALL = 'All', _('All')
-        HASGT = 'HasGT', _('HasGT')
-        SAMPLE = 'Sample', _('Sample')
+        ALL = "All", _("All")
+        HASGT = "HasGT", _("HasGT")
+        SAMPLE = "Sample", _("Sample")
 
     class FileType(models.TextChoices):
-        INPUT = 'Input', _('Input')
-        OUTPUT = 'Output', _('Output')
+        INPUT = "Input", _("Input")
+        OUTPUT = "Output", _("Output")
 
     class ModelRunStatus(models.TextChoices):
-        PENDING = 'Pending', _('Pending')
-        INPROGRESS = 'InProgress', _('InProgress')
-        COMPLETED = 'Completed', ('Completed')
-        FAILED = 'Failed', ('Failed')
-        CANCELED = 'Canceled', ('Canceled')
+        PENDING = "Pending", _("Pending")
+        INPROGRESS = "InProgress", _("InProgress")
+        COMPLETED = "Completed", ("Completed")
+        FAILED = "Failed", ("Failed")
+        CANCELED = "Canceled", ("Canceled")
 
     organization = models.ForeignKey(
-        'organizations.Organization', on_delete=models.CASCADE, related_name='model_runs', null=True
+        "organizations.Organization", on_delete=models.CASCADE, related_name="model_runs", null=True
     )
 
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='model_runs')
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="model_runs")
 
-    model_version = models.ForeignKey(ThirdPartyModelVersion, on_delete=models.CASCADE, related_name='model_runs')
+    model_version = models.ForeignKey(ThirdPartyModelVersion, on_delete=models.CASCADE, related_name="model_runs")
 
     created_by = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        related_name='model_runs',
+        related_name="model_runs",
         on_delete=models.SET_NULL,
         null=True,
     )
@@ -147,16 +144,22 @@ class ModelRun(models.Model):
         null=True,
         blank=True,
         default=None,
-        help_text='Job ID for inference job for a ModelRun e.g. Adala job ID',
+        help_text="Job ID for inference job for a ModelRun e.g. Adala job ID",
     )
 
-    created_at = models.DateTimeField(_('created at'), auto_now_add=True)
+    total_predictions = models.IntegerField(_("total predictions"), default=0)
 
-    triggered_at = models.DateTimeField(_('triggered at'), null=True, default=None)
+    total_correct_predictions = models.IntegerField(_("total correct predictions"), default=0)
 
-    predictions_updated_at = models.DateTimeField(_('predictions updated at'), null=True, default=None)
+    total_tasks = models.IntegerField(_("total tasks"), default=0)
 
-    completed_at = models.DateTimeField(_('completed at'), null=True, default=None)
+    created_at = models.DateTimeField(_("created at"), auto_now_add=True)
+
+    triggered_at = models.DateTimeField(_("triggered at"), null=True, default=None)
+
+    predictions_updated_at = models.DateTimeField(_("predictions updated at"), null=True, default=None)
+
+    completed_at = models.DateTimeField(_("completed at"), null=True, default=None)
 
     def delete_predictions(self):
         """
