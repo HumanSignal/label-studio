a32cbbc96d26075526de2a1d150309d134322cc1
Gondragos <gondragos@gmail.com>
2024-06-20 16:26:04 +0100

    test: LEAP-1141: Add test for concurrent saving annotation and draft

---
+++ web/libs/editor/tests/integration/e2e/drafts/submit.cy.ts
@@ -0,0 +1,66 @@
+import { Choices, LabelStudio, ToolBar } from "@humansignal/frontend-test/helpers/LSF";
+import { FF_DEV_3873, FF_REVIEWER_FLOW } from "../../../../src/utils/feature-flags";
+
+enum RESPONSE_TYPE {
+  DRAFT = "draft",
+  SUBMIT = "submit",
+}
+const RESPONSE_DELAY = 100;
+const WAITING_FOR_EVENTS_DELAY = 200;
+
+describe("Annotation submitting process", () => {
+  it("should not create a duplicate annotation if the annotation and draft are saved concurrently", () => {
+    LabelStudio.addFeatureFlagsOnPageLoad({
+      [FF_DEV_3873]: true,
+      [FF_REVIEWER_FLOW]: true,
+    });
+    // here will be stored an order of completed saving action
+    // it consists of strings: "draft" and "submit"
+    const callOrder: RESPONSE_TYPE[] = [];
+
+    LabelStudio.params()
+      .data({
+        text: "Some words",
+      })
+      .config(`<View>
+  <Text name="text" value="$text" />
+  <Choices toName="text" name="choice">
+    <Choice value="ClickMe"/>
+  </Choices>
+</View>`)
+      // To have "new" annotation we need to use userGenerate
+      .withAnnotation({ userGenerate: true, result: [] })
+      .withEventListener("submitDraft", () => {
+        return new Promise((resolve) => {
+          // initialize saving annotation exactly when request of saving draft should be sent to the server
+          ToolBar.submitBtn.click();
+
+          // this emulates server response delay
+          setTimeout(() => {
+            callOrder.push(RESPONSE_TYPE.DRAFT);
+            resolve(void 0);
+          }, RESPONSE_DELAY);
+        });
+      })
+      .withEventListener("submitAnnotation", () => {
+        callOrder.push(RESPONSE_TYPE.SUBMIT);
+      })
+      .init();
+
+    // just to have something to save
+    Choices.findChoice("ClickMe").click();
+
+    // Preventing waiting for autosave. It will save time.
+    cy.window().then(async (win) => {
+      win.Htx.annotationStore.selected.saveDraftImmediately();
+    });
+
+    // Wait for all submit events to be invoked, to have `callOrder` ready to check
+    cy.wait(WAITING_FOR_EVENTS_DELAY);
+
+    // Check order
+    cy.window().then(async (win) => {
+      expect(callOrder).to.deep.equal([RESPONSE_TYPE.DRAFT, RESPONSE_TYPE.SUBMIT]);
+    });
+  });
+});
