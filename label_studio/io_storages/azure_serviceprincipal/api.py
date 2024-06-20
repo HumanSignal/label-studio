660ecb453a27acc01710fbdc2fe782c1a3037b46
Manrique.Vargas-Quesada <mv1742@nyu.edu>
2024-06-20 13:46:25 -0400

    add sdk vars after comparing

--- label_studio/io_storages/azure_serviceprincipal/api.py
+++ label_studio/io_storages/azure_serviceprincipal/api.py
@@ -3,6 +3,8 @@
 from django.utils.decorators import method_decorator
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
+from drf_yasg.utils import no_body, swagger_auto_schema
+
 from io_storages.api import (
     ExportStorageDetailAPI,
     ExportStorageFormLayoutAPI,
@@ -24,11 +26,20 @@ from io_storages.azure_serviceprincipal.serializers import (
     AzureServicePrincipalImportStorageSerializer,
 )
 
+from .openapi_schema import (
+    _azure_serviceprincipal_export_storage_schema,
+    _azure_serviceprincipal_export_storage_schema_with_id,
+    _azure_serviceprincipal_import_storage_schema,
+    _azure_serviceprincipal_import_storage_schema_with_id,
+)
 
 @method_decorator(
     name='get',
     decorator=swagger_auto_schema(
         tags=['Storage: Azure SPI'],
+        x_fern_sdk_group_name=['import_storage', 'azure_spi'],
+        x_fern_sdk_method_name='list',
+        x_fern_audiences=['public'],
         operation_summary='Get all import storage',
         operation_description='Get list of all Azure import storage connections.',
         manual_parameters=[
@@ -45,8 +56,12 @@ from io_storages.azure_serviceprincipal.serializers import (
     name='post',
     decorator=swagger_auto_schema(
         tags=['Storage: Azure SPI'],
+        x_fern_sdk_group_name=['import_storage', 'azure_spi'],
+        x_fern_sdk_method_name='create',
+        x_fern_audiences=['public'],
         operation_summary='Create new storage',
         operation_description='Get new Azure import storage',
+        request_body=_azure_serviceprincipal_import_storage_schema,
     ),
 )
 class AzureServicePrincipalImportStorageListAPI(ImportStorageListAPI):
@@ -58,24 +73,36 @@ class AzureServicePrincipalImportStorageListAPI(ImportStorageListAPI):
     name='get',
     decorator=swagger_auto_schema(
         tags=['Storage: Azure SPI'],
+        x_fern_sdk_group_name=['import_storage', 'azure_spi'],
+        x_fern_sdk_method_name='get',
+        x_fern_audiences=['public'],
         operation_summary='Get import storage',
         operation_description='Get a specific Azure import storage connection.',
+        request_body=no_body,
     ),
 )
 @method_decorator(
     name='patch',
     decorator=swagger_auto_schema(
         tags=['Storage: Azure SPI'],
+        x_fern_sdk_group_name=['import_storage', 'azure_spi'],
+        x_fern_sdk_method_name='update',
+        x_fern_audiences=['public'],
         operation_summary='Update import storage',
         operation_description='Update a specific Azure import storage connection.',
+        request_body=_azure_serviceprincipal_import_storage_schema,
     ),
 )
 @method_decorator(
     name='delete',
     decorator=swagger_auto_schema(
         tags=['Storage: Azure SPI'],
+        x_fern_sdk_group_name=['import_storage', 'azure_spi'],
+        x_fern_sdk_method_name='delete',
+        x_fern_audiences=['public'],
         operation_summary='Delete import storage',
         operation_description='Delete a specific Azure import storage connection.',
+        request_body=no_body,
     ),
 )
 class AzureServicePrincipalImportStorageDetailAPI(ImportStorageDetailAPI):
@@ -87,8 +114,20 @@ class AzureServicePrincipalImportStorageDetailAPI(ImportStorageDetailAPI):
     name='post',
     decorator=swagger_auto_schema(
         tags=['Storage: Azure SPI'],
+        x_fern_sdk_group_name=['import_storage', 'azure_spi'],
+        x_fern_sdk_method_name='sync',
+        x_fern_audiences=['public'],
         operation_summary='Sync import storage',
         operation_description='Sync tasks from an Azure import storage connection.',
+        manual_parameters=[
+            openapi.Parameter(
+                name='id',
+                type=openapi.TYPE_INTEGER,
+                in_=openapi.IN_PATH,
+                description='Storage ID',
+            ),
+        ],
+        request_body=no_body,
     ),
 )
 class AzureServicePrincipalImportStorageSyncAPI(ImportStorageSyncAPI):
@@ -99,8 +138,12 @@ class AzureServicePrincipalImportStorageSyncAPI(ImportStorageSyncAPI):
     name='post',
     decorator=swagger_auto_schema(
         tags=['Storage: Azure SPI'],
+        x_fern_sdk_group_name=['export_storage', 'azure_spi'],
+        x_fern_sdk_method_name='sync',
+        x_fern_audiences=['public'],
         operation_summary='Sync export storage',
         operation_description='Sync tasks from an Azure export storage connection.',
+        request_body=no_body,
     ),
 )
 class AzureServicePrincipalExportStorageSyncAPI(ExportStorageSyncAPI):
@@ -111,8 +154,14 @@ class AzureServicePrincipalExportStorageSyncAPI(ExportStorageSyncAPI):
     name='post',
     decorator=swagger_auto_schema(
         tags=['Storage: Azure SPI'],
+        x_fern_sdk_group_name=['import_storage', 'azure_spi'],
+        x_fern_sdk_method_name='validate',
+        x_fern_audiences=['public'],
         operation_summary='Validate import storage',
         operation_description='Validate a specific Azure import storage connection.',
+        request_body=_azure_serviceprincipal_import_storage_schema_with_id,
+        # expecting empty response
+        responses={200: openapi.Response(description='OK')},
     ),
 )
 class AzureServicePrincipalImportStorageValidateAPI(ImportStorageValidateAPI):
@@ -123,8 +172,14 @@ class AzureServicePrincipalImportStorageValidateAPI(ImportStorageValidateAPI):
     name='post',
     decorator=swagger_auto_schema(
         tags=['Storage: Azure SPI'],
+        x_fern_sdk_group_name=['export_storage', 'azure_spi'],
+        x_fern_sdk_method_name='validate',
+        x_fern_audiences=['public'],
         operation_summary='Validate export storage',
         operation_description='Validate a specific Azure export storage connection.',
+        request_body=_azure_serviceprincipal_export_storage_schema_with_id,
+        # expecting empty response
+        responses={200: openapi.Response(description='OK')},
     ),
 )
 class AzureServicePrincipalExportStorageValidateAPI(ExportStorageValidateAPI):
@@ -135,6 +190,9 @@ class AzureServicePrincipalExportStorageValidateAPI(ExportStorageValidateAPI):
     name='get',
     decorator=swagger_auto_schema(
         tags=['Storage: Azure SPI'],
+        x_fern_sdk_group_name=['export_storage', 'azure_spi'],
+        x_fern_sdk_method_name='list',
+        x_fern_audiences=['public'],
         operation_summary='Get all export storage',
         operation_description='Get a list of all Azure export storage connections.',
         manual_parameters=[
@@ -145,14 +203,20 @@ class AzureServicePrincipalExportStorageValidateAPI(ExportStorageValidateAPI):
                 description='Project ID',
             ),
         ],
+        request_body=no_body,
+
     ),
 )
 @method_decorator(
     name='post',
     decorator=swagger_auto_schema(
         tags=['Storage: Azure SPI'],
+        x_fern_sdk_group_name=['export_storage', 'azure_spi'],
+        x_fern_sdk_method_name='create',
+        x_fern_audiences=['public'],
         operation_summary='Create export storage',
         operation_description='Create a new Azure export storage connection to store annotations.',
+        request_body=_azure_serviceprincipal_export_storage_schema,
     ),
 )
 class AzureServicePrincipalExportStorageListAPI(ExportStorageListAPI):
@@ -164,24 +228,37 @@ class AzureServicePrincipalExportStorageListAPI(ExportStorageListAPI):
     name='get',
     decorator=swagger_auto_schema(
         tags=['Storage: Azure SPI'],
+        x_fern_sdk_group_name=['export_storage', 'azure_spi'],
+        x_fern_sdk_method_name='get',
+        x_fern_audiences=['public'],
         operation_summary='Get export storage',
         operation_description='Get a specific Azure export storage connection.',
+        request_body=no_body,
     ),
 )
 @method_decorator(
     name='patch',
     decorator=swagger_auto_schema(
         tags=['Storage: Azure SPI'],
+        x_fern_sdk_group_name=['export_storage', 'azure_spi'],
+        x_fern_sdk_method_name='update',
+        x_fern_audiences=['public'],
         operation_summary='Update export storage',
         operation_description='Update a specific Azure export storage connection.',
+        request_body=_azure_serviceprincipal_export_storage_schema,
+
     ),
 )
 @method_decorator(
     name='delete',
     decorator=swagger_auto_schema(
         tags=['Storage: Azure SPI'],
+        x_fern_sdk_group_name=['export_storage', 'azure'],
+        x_fern_sdk_method_name='delete',
+        x_fern_audiences=['public'],
         operation_summary='Delete export storage',
         operation_description='Delete a specific Azure export storage connection.',
+        request_body=no_body,
     ),
 )
 class AzureServicePrincipalExportStorageDetailAPI(ExportStorageDetailAPI):
