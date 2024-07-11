a6ea82d1c2630456b4f4491a92d171d86147e13b
nik <nik@heartex.net>
2024-07-11 16:12:00 +0100

    Fix some tests

--- label_studio/tests/sdk/test_annotations.py
+++ label_studio/tests/sdk/test_annotations.py
@@ -7,38 +7,46 @@ from label_studio.tests.sdk.common import LABEL_CONFIG_AND_TASKS
 pytestmark = pytest.mark.django_db
 from label_studio_sdk.client import LabelStudio
 from label_studio_sdk.data_manager import Column, Filters, Operator, Type
+from label_studio_sdk.label_interface import LabelInterface
+from label_studio_sdk.label_interface.create import choices
+from label_studio_sdk.label_interface.objects import AnnotationValue, PredictionValue, TaskValue
+from label_studio_sdk.label_interface.object_tags import TextTag
+from label_studio_sdk.label_interface.control_tags import ChoicesTag, ChoicesValue
 
 
 def test_annotations_CRUD(django_live_url, business_client):
     ls = LabelStudio(base_url=django_live_url, api_key=business_client.api_key)
+    li = LabelInterface(LABEL_CONFIG_AND_TASKS['label_config'])
     p = ls.projects.create(title='New Project', label_config=LABEL_CONFIG_AND_TASKS['label_config'])
 
-    task_data = [{'data': {'my_text': 'Test task'}}]
-    ls.projects.import_tasks(id=p.id, request=task_data)
+    task_data = TaskValue(data={'my_text': 'Test task'})
+    ls.projects.import_tasks(id=p.id, request=[task_data.model_dump()])
 
     for task in ls.tasks.list(project=p.id):
-        assert task.data == task_data[0]['data']
+        assert task.data == task_data.data
 
     task_id = task.id
-    annotation_data = {
-        'result': [{'from_name': 'label', 'to_name': 'my_text', 'type': 'choices', 'value': {'choices': ['Positive']}}]
-    }
-    new_annotation = ls.annotations.create(task_id, **annotation_data)
+    tag_name = 'sentiment_class'
+    annotation_data = AnnotationValue(
+        result=[li.get_control(tag_name).label(['Positive'])],
+        completed_by=business_client.user.id
+    ).model_dump()
+    new_annotation = ls.annotations.create(task_id, result=annotation_data['result'])
     assert (annotation_id := new_annotation.id)
     assert new_annotation.result == annotation_data['result']
 
     ls.annotations.update(
         id=annotation_id,
-        result=[{'from_name': 'label', 'to_name': 'my_text', 'type': 'choices', 'value': {'choices': ['Negative']}}],
+        result=[li.get_control(tag_name).label(['Negative'])],
     )
-    for task_with_annotation in ls.tasks.list(project=p.id, fields='all'):
+    for task_with_annotation in ls.tasks.list(project=p.id):
         updated_annotation = task_with_annotation.annotations[0]
     assert updated_annotation['result'][0]['value'] == {'choices': ['Negative']}
 
     # create another annotation
     another_annotation = ls.annotations.create(
         id=task_id,
-        result=[{'from_name': 'label', 'to_name': 'my_text', 'type': 'choices', 'value': {'choices': ['Neutral']}}],
+        result=[li.get_control(tag_name).label(['Neutral'])],
     )
 
     # check that there are two annotations
@@ -55,13 +63,28 @@ def test_annotations_CRUD(django_live_url, business_client):
 
 def test_annotation_marks_task_as_labeled(django_live_url, business_client):
     ls = LabelStudio(base_url=django_live_url, api_key=business_client.api_key)
+
+    label_config = LabelInterface.create({
+        'choices': ChoicesTag(
+            name='sentiment_class',
+            to_name=['message'],
+            choices=ChoicesValue(choices=['Positive', 'Negative']),
+            attr={},
+            tag="Control"
+        ),
+        'message': TextTag(name='message', value='my_text', tag='Text'),
+    })
+
     p = ls.projects.create(
         title='New Project',
-        label_config=LABEL_CONFIG_AND_TASKS['label_config'],
+        label_config=label_config,
     )
 
-    task_data = [{'data': {'my_text': 'Test task'}}, {'data': {'my_text': 'Test task 2'}}]
-    ls.projects.import_tasks(id=p.id, request=task_data)
+    task_data = [
+        TaskValue(data={'my_text': 'Test task'}),
+        TaskValue(data={'my_text': 'Test task 2'}),
+    ]
+    ls.projects.import_tasks(id=p.id, request=[task.model_dump() for task in task_data])
 
     filters = Filters.create(
         Filters.OR,
@@ -83,13 +106,15 @@ def test_annotation_marks_task_as_labeled(django_live_url, business_client):
     assert len(tasks) == 2
 
     task_id = tasks[0].id
-    annotation_data = {
-        'result': [{'from_name': 'label', 'to_name': 'my_text', 'type': 'choices', 'value': {'choices': ['Positive']}}]
-    }
-    ls.annotations.create(id=task_id, **annotation_data)
+    annotation_data = AnnotationValue(
+        result=[label_config.get_control('sentiment_class').label(['Positive'])],
+        completed_by=business_client.user.id
+    ).model_dump()
+
+    ls.annotations.create(id=task_id, result=annotation_data['result'])
 
     labeled_tasks = []
-    for task in ls.tasks.list(project=p.id, query=query, fields='all'):
+    for task in ls.tasks.list(project=p.id, query=query):
         labeled_tasks.append(task)
 
     assert len(labeled_tasks) == 1
