8c2af4106afccad2b15d880f941b115fbfc0dfe3
hlomzik <hlomzik@gmail.com>
2024-07-26 18:40:13 +0100

    feat: LEAP-1266: Video frame classification
    Add new type of regions, Video Classification, to store both `sequence` and classification.
    Only Choices are supported for now. Every Choice will have it's own region if used.

---
+++ web/libs/editor/src/regions/VideoClassification.js
@@ -0,0 +1,60 @@
+import { types } from "mobx-state-tree";
+
+import Registry from "../core/Registry";
+import { VideoRegion } from "./VideoRegion";
+
+const Model = types
+  .model("VideoClassificationModel", {
+    type: "videoclassification",
+    choices: types.array(types.string),
+  })
+  .views((self) => ({
+    get label() {
+      return self.choices.join(", ");
+    },
+
+    getShape() {
+      return null;
+    },
+
+    getVisibility() {
+      return true;
+    },
+  }))
+  .actions((self) => {
+    const Super = {
+      toggleLifespan: self.toggleLifespan,
+      addKeypoint: self.addKeypoint,
+      removeKeypoint: self.removeKeypoint,
+    };
+
+    return {
+      updateShape() {
+        return;
+      },
+
+      toggleLifespan(frame) {
+        Super.toggleLifespan(frame);
+
+        self.object.updatePerFrameViews();
+      },
+
+      addKeypoint(frame) {
+        Super.addKeypoint(frame);
+
+        self.object.updatePerFrameViews();
+      },
+
+      removeKeypoint(frame) {
+        Super.removeKeypoint(frame);
+
+        self.object.updatePerFrameViews();
+      },
+    };
+  });
+
+const VideoClassificationModel = types.compose("VideoClassificationModel", VideoRegion, Model);
+
+Registry.addRegionType(VideoClassificationModel, "video");
+
+export { VideoClassificationModel };
