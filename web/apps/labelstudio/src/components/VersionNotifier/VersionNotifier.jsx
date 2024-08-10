89d980b0bec8a718d89a3485aba20f67b1f46bb0
David Hayes <dave@euri.ca>
2024-08-09 20:01:46 -0700

    Add redirect for update link
    Today labelstud.io/redirect/update redirects to pypi.org/project/label-studio/#history so this doesn't change behavior, but once we create an upgrade guide, we can point to that
    
    https://github.com/HumanSignal/label-studio-website/pull/57

--- web/apps/labelstudio/src/components/VersionNotifier/VersionNotifier.jsx
+++ web/apps/labelstudio/src/components/VersionNotifier/VersionNotifier.jsx
@@ -43,7 +43,7 @@ export const VersionProvider = ({ children }) => {
 };
 
 export const VersionNotifier = ({ showNewVersion, showCurrentVersion }) => {
-  const url = "https://pypi.org/project/label-studio/#history";
+  const url = "https://labelstud.io/redirect/update";
   const { newVersion, updateTime, latestVersion, version } = useContext(VersionContext) ?? {};
 
   return newVersion && showNewVersion ? (
