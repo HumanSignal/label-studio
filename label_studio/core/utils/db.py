569c997605bba00f1da88cca35cdc5cc50953533
Wesley Lima <wesley@heartex.com>
2024-08-14 16:04:23 -0400

    fix: OPTIC-1013: Adding fast_first_or_create util function

--- label_studio/core/utils/db.py
+++ label_studio/core/utils/db.py
@@ -23,3 +23,10 @@ def fast_first(queryset: QuerySet[ModelType]) -> Optional[ModelType]:
     if result := queryset[:1]:
         return result[0]
     return None
+
+
+def fast_first_or_create(model, **model_params) -> Optional[ModelType]:
+    """Like get_or_create, but using fast_first instead of first()"""
+    if instance := fast_first(model.objects.filter(**model_params)):
+        return instance
+    return model.objects.create(**model_params)
