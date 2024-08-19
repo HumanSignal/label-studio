1d97fa0eab1608147a6c7c219553a23db6f88313
bmartel <brandonmartel@gmail.com>
2024-08-19 13:56:04 -0500

    Apply suggestions from code review

--- web/libs/editor/src/tags/object/Paragraphs/HtxParagraphs.jsx
+++ web/libs/editor/src/tags/object/Paragraphs/HtxParagraphs.jsx
@@ -487,10 +487,10 @@ class HtxParagraphsView extends Component {
   _handleScrollContainerHeight = () => {
     requestAnimationFrame(() => {
       const container = this.myRef.current;
-      const mainContentView = document.querySelector(this.mainContent);
+      const mainContentView = document.querySelector(this.mainContentSelector);
       const mainRect = mainContentView.getBoundingClientRect();
       const visibleHeight = document.documentElement.clientHeight - mainRect.top;
-      const annotationView = document.querySelector(this.mainViewAnnotation);
+      const annotationView = document.querySelector(this.mainViewAnnotationSelector);
       const totalVisibleSpace = Math.floor(
         visibleHeight < mainRect.height ? visibleHeight : mainContentView?.offsetHeight || 0,
       );
@@ -513,12 +513,12 @@ class HtxParagraphsView extends Component {
 
   componentDidMount() {
     if (isFF(FF_LSDV_E_278) && this.props.item.contextscroll)
-      this._resizeObserver.observe(document.querySelector(this.mainContent));
+      this._resizeObserver.observe(document.querySelector(this.mainContentSelector));
     this._handleUpdate();
   }
 
   componentWillUnmount() {
-    const target = document.querySelector(this.mainContent);
+    const target = document.querySelector(this.mainContentSelector);
 
     if (target) this._resizeObserver?.unobserve(target);
     this._resizeObserver?.disconnect();
