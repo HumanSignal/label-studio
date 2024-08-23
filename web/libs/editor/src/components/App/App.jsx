e5faaf640c04c160d974aa19cdafa28cb6ae0603
Raul Martin <raul@humansignal.com>
2024-08-23 15:46:10 -0700

    recheck app.jsx
    correction

--- web/libs/editor/src/components/App/App.jsx
+++ web/libs/editor/src/components/App/App.jsx
@@ -25,7 +25,7 @@ import "../../tags/visual";
 import { Space } from "../../common/Space/Space";
 import { Button } from "../../common/Button/Button";
 import { Block, Elem } from "../../utils/bem";
-import { FF_DEV_1170, FF_DEV_3873, FF_LSDV_4620_3_ML, FF_SIMPLE_INIT, isFF } from "../../utils/feature-flags";
+import { FF_DEV_3873, FF_LSDV_4620_3_ML, FF_SIMPLE_INIT, isFF } from "../../utils/feature-flags";
 import { sanitizeHtml } from "../../utils/html";
 import { reactCleaner } from "../../utils/reactCleaner";
 import { guidGenerator } from "../../utils/unique";
@@ -219,13 +219,12 @@ class App extends Component {
       </Block>
     );
 
-    const outlinerEnabled = isFF(FF_DEV_1170);
     const newUIEnabled = isFF(FF_DEV_3873);
 
     return (
       <Block
         name="editor"
-        mod={{ fullscreen: settings.fullscreen, _auto_height: !outlinerEnabled }}
+        mod={{ fullscreen: settings.fullscreen }}
         ref={isFF(FF_LSDV_4620_3_ML) ? reactCleaner(this) : null}
       >
         <Settings store={store} />
@@ -254,47 +253,29 @@ class App extends Component {
             mod={{
               viewAll: viewingAll,
               bsp: settings.bottomSidePanel,
-              outliner: outlinerEnabled,
+              outliner: true,
               showingBottomBar: newUIEnabled,
             }}
           >
-            {outlinerEnabled ? (
-              newUIEnabled ? (
-                <SideTabsPanels
-                  panelsHidden={viewingAll}
-                  currentEntity={as.selectedHistory ?? as.selected}
-                  regions={as.selected.regionStore}
-                  showComments={store.hasInterface("annotations:comments")}
-                  focusTab={store.commentStore.tooltipMessage ? "comments" : null}
-                >
-                  {mainContent}
-                  {store.hasInterface("topbar") && <BottomBar store={store} />}
-                </SideTabsPanels>
-              ) : (
-                <SidePanels
-                  panelsHidden={viewingAll}
-                  currentEntity={as.selectedHistory ?? as.selected}
-                  regions={as.selected.regionStore}
-                >
-                  {mainContent}
-                </SidePanels>
-              )
-            ) : (
-              <>
+            {newUIEnabled ? (
+              <SideTabsPanels
+                panelsHidden={viewingAll}
+                currentEntity={as.selectedHistory ?? as.selected}
+                regions={as.selected.regionStore}
+                showComments={store.hasInterface("annotations:comments")}
+                focusTab={store.commentStore.tooltipMessage ? "comments" : null}
+              >
                 {mainContent}
-
-                {viewingAll === false && (
-                  <Block name="menu" mod={{ bsp: settings.bottomSidePanel }}>
-                    {store.hasInterface("side-column") && (
-                      <SidebarTabs>
-                        <AnnotationTab store={store} />
-                      </SidebarTabs>
-                    )}
-                  </Block>
-                )}
-
-                {newUIEnabled && store.hasInterface("topbar") && <BottomBar store={store} />}
-              </>
+                {store.hasInterface("topbar") && <BottomBar store={store} />}
+              </SideTabsPanels>
+            ) : (
+              <SidePanels
+                panelsHidden={viewingAll}
+                currentEntity={as.selectedHistory ?? as.selected}
+                regions={as.selected.regionStore}
+              >
+                {mainContent}
+              </SidePanels>
             )}
           </Block>
         </Provider>
