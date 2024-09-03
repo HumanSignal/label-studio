0e09a0c93237917ead5bb9ca8d2694f4648b0044
Raul Martin <raul@humansignal.com>
2024-09-03 16:37:59 -0700

    refactor: OPTIC-1070: restore stale ff 'ff_front_dev_2432_auto_save_polygon_draft_210622_short': True,

--- label_studio/core/feature_flags/stale_feature_flags.py
+++ label_studio/core/feature_flags/stale_feature_flags.py
@@ -44,6 +44,7 @@ STALE_FEATURE_FLAGS = {
     'fflag_feat_front_dev_2984_dm_draggable_columns_short': True,
     'fflag_feat_front_dev_2461_audio_paragraphs_seek_chunk_position_short': True,
     'fflag-feat-front-dev-2982-label-weights-settings': True,
+    'ff_front_dev_2432_auto_save_polygon_draft_210622_short': True,
     'ff_front_dev_2575_projects_list_performance_280622_short': True,
     'ff_front_dev_2431_delete_polygon_points_080622_short': True,
     'ff_front_dev_2290_draft_in_annotation_history_short': True,
