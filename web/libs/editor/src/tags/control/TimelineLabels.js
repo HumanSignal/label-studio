8e41f49b23f7493aab8be6e2f395e692a556f54d
hlomzik <hlomzik@gmail.com>
2024-09-09 23:48:21 +0100

    Remove outdated tip about interactions with timeline

--- web/libs/editor/src/tags/control/TimelineLabels.js
+++ web/libs/editor/src/tags/control/TimelineLabels.js
@@ -12,8 +12,6 @@ import { HtxLabels, LabelsModel } from "./Labels/Labels";
  *
  * First, select a label and then click once to annotate a single frame. Click and drag to annotate multiple frames.
  *
- * To move forward and backward in the timeline without labeling, ensure that no labels are selected before you click.
- *
  * ![Screenshot of video with frame classification](../images/timelinelabels.png)
  *
  * Use with the `<Video>` control tag.
