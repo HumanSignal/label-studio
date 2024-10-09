44c1ce41a6898189e19518dbba40f20d15fc0a7f
hlomzik <hlomzik@gmail.com>
2024-10-09 15:37:15 +0100

    Fix linting

--- web/libs/editor/src/stores/types.d.ts
+++ web/libs/editor/src/stores/types.d.ts
@@ -12,7 +12,7 @@ type MSTResult = {
   mainValue: any;
   // @todo tag
   from_name: any;
-}
+};
 
 type MSTagProps = {
   isReady?: boolean;
@@ -27,13 +27,7 @@ type MSTTagImage = {
   canvasSize?: { width: number; height: number };
 } & MSTagProps;
 
-type MSTTag = (
-  | MSTTagImage
-  | {
-      type: string;
-    }
-  )
-  & MSTagProps;
+type MSTTag = MSTTagImage | (MSTagProps & { type: string });
 
 type MixinMSTArea = {
   id: string;
@@ -43,6 +37,7 @@ type MixinMSTArea = {
   control: object;
   object: object;
   classification?: boolean;
+  selected: boolean;
 };
 
 type MixinMSTRegion = {
@@ -83,6 +78,7 @@ type MSTAnnotation = {
   results: MSTResult[];
   names: Map<string, MSTTag>;
   isLinkingMode: boolean;
+  linkingMode: "create_relation" | "link_to_comment";
 
   submissionInProgress: () => void;
 };
