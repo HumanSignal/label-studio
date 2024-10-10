8265f89c8334387ec3978c5183523165deb93fdf
louis.julien <43572828+LouisJULIEN@users.noreply.github.com>
2024-10-10 16:02:42 +0200

    feat: add region:visibility-all shortcut (ctrl+h) (#6212)
    Co-authored-by: Brandon Martel <brandonmartel@gmail.com>

--- web/libs/editor/src/stores/AppStore.js
+++ web/libs/editor/src/stores/AppStore.js
@@ -433,6 +433,11 @@ export default types
         }
       });
 
+      hotkeys.addNamed("region:visibility-all", () => {
+        const { selected } = self.annotationStore;
+        selected.regionStore.toggleVisibility();
+      });
+
       hotkeys.addNamed("annotation:undo", () => {
         const annotation = self.annotationStore.selected;
 
