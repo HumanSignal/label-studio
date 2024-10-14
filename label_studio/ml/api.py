77a2be925f797930388cff7f4b31c33619b586e4
makseq-ubnt <makseq@gmail.com>
2024-10-14 21:13:24 +0100

    feat: RND-124: Make clear error descriptions in Sent Test Request for ML backends

--- label_studio/ml/api.py
+++ label_studio/ml/api.py
@@ -291,9 +291,17 @@ class MLBackendPredictTestAPI(APIView):
         if random:
             task = Task.get_random(project=ml_backend.project)
             if not task:
-                raise Http404
+                return Response(
+                    status=status.HTTP_500_INTERNAL_SERVER_ERROR, 
+                    data={'error': 'Project has no tasks to run prediction on, import at least 1 task to run prediction'}
+                )
 
             kwargs = ml_backend._predict(task)
+            if not kwargs:
+                return Response(
+                    status=status.HTTP_500_INTERNAL_SERVER_ERROR, 
+                    data={'error': 'ML backend did not return any predictions, check ML backend logs for more details'}
+                )
             return Response(**kwargs)
 
         else:
