ca44d44502045c2e23e24e4b95128e9330d352bd
bmartel <brandonmartel@gmail.com>
2025-10-16 10:25:44 -0500

    Apply suggestion from @bmartel

--- web/libs/app-common/src/pages/AccountSettings/sections/PersonalJWTToken.tsx
+++ web/libs/app-common/src/pages/AccountSettings/sections/PersonalJWTToken.tsx
@@ -14,7 +14,7 @@ import { Button } from "@humansignal/ui";
  * each one of these eventually has to be migrated to core/ui
  */
 import { getApiInstance } from "@humansignal/core";
-import { modal, confirm } from "@humansignal/ui";
+import { modal, confirm } from "@humansignal/ui/lib/modal";
 import { Input, Label } from "apps/labelstudio/src/components/Form/Elements";
 import { Tooltip } from "@humansignal/ui";
 
