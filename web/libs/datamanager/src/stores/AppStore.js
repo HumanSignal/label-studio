d0a1b1cecf554c7177b2540e32696416a152aea0
Yousif Yassi <yousif@heartex.com>
2025-10-24 15:53:19 -0400

    removed memoize and unneded call to actions

--- web/libs/datamanager/src/stores/AppStore.js
+++ web/libs/datamanager/src/stores/AppStore.js
@@ -583,11 +583,6 @@ export const AppStore = types
       }
 
       if (!isLabelStream || (self.project?.show_annotation_history && task)) {
-        if (self.SDK.type === "dm") {
-          // Fetch actions in background to avoid blocking the main thread
-          setTimeout(() => self.fetchActions(), 0);
-        }
-
         if (self.SDK.settings?.onlyVirtualTabs && self.project?.show_annotation_history && !task) {
           requests.push(
             self.viewsStore.addView(
