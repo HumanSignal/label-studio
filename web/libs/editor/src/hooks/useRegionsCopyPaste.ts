188a2aea795130615eaba79bb2c6cc86887121f1
Jouke van der Maas <jouke.vandermaas@orbitaleye.nl>
2025-11-21 16:03:16 +0100

    Fix pasting within images when using valueList
    Previously, copying annotations from one image and pasting on another
    did not work: the annotation was always pasted on the first image.
    
    Now it pastes on the currently selected image.
    
    Co-authored-by: alessandro-oe <alessandro-oe@users.noreply.github.com>

--- web/libs/editor/src/hooks/useRegionsCopyPaste.ts
+++ web/libs/editor/src/hooks/useRegionsCopyPaste.ts
@@ -40,6 +40,17 @@ export const useRegionsCopyPaste = (entity: any) => {
           return { ...res, readonly: false };
         });
 
+        if (isFF(FF_LSDV_4583)) {
+          // if user selected another image, we should paste in the current
+          // image, not the one they copied from
+          for (const result of results) {
+            const target = entity.names.get(result.to_name);
+            if (target.isMultiItem) {
+              result.item_index = target.currentItemIndex;
+            }
+          }
+        }
+
         entity.appendResults(results);
         ev.preventDefault();
       } catch (e) {
