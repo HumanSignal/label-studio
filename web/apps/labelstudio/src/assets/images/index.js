2a852a9c403722a63f9b9e60bd5631b6b171253e
gavin <gavinlouuu@gmail.com>
2025-11-28 23:30:09 +0800

    opt: clean up tips

--- web/apps/labelstudio/src/assets/images/index.js
+++ web/apps/labelstudio/src/assets/images/index.js
@@ -1,3 +1,4 @@
 export { ReactComponent as HeidiSpeaking } from "./heidi-speaking.svg";
 export { ReactComponent as HeidiAi } from "./heidi-ai.svg";
 export { ReactComponent as LSLogo } from "./logo.svg";
+export { ReactComponent as HSLogo } from "./hs-logo.svg";
