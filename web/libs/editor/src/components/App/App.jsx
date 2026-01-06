87713047310d1bc0aacb9b11020b100a39a31a7a
Yousif Yassi <yousif@heartex.com>
2026-01-06 13:18:54 -0500

    undoing unnecessary change

--- web/libs/editor/src/components/App/App.jsx
+++ web/libs/editor/src/components/App/App.jsx
@@ -7,7 +7,6 @@ import { getEnv, getRoot } from "mobx-state-tree";
 import { observer, Provider } from "mobx-react";
 
 /**
- * adding something to force dependency update
  * Core
  */
 import { CommentsOverlay } from "../InteractiveOverlays/CommentsOverlay";
