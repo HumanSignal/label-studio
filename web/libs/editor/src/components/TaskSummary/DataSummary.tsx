4f6ccff369536851b8ba78297fc089ae21d1a754
nik <nik@heartex.net>
2026-01-13 20:53:59 +0300

    fix: BROS-701: Postfix

--- web/libs/editor/src/components/TaskSummary/DataSummary.tsx
+++ web/libs/editor/src/components/TaskSummary/DataSummary.tsx
@@ -60,6 +60,7 @@ export const DataSummary = ({ data_types }: { data_types: ObjectTypes }) => {
               </div>
             );
           }
+          // TODO: PDF URLs
 
           return value;
         },
