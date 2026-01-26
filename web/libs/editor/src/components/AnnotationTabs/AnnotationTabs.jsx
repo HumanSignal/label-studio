09ffc9e7c05f532d62f20319e0271c423e3b78c6
Ricardo Cabral <ricardo.cabralm@gmail.com>
2026-01-26 18:24:44 -0500

    Removes unnecessary handling.

--- web/libs/editor/src/components/AnnotationTabs/AnnotationTabs.jsx
+++ web/libs/editor/src/components/AnnotationTabs/AnnotationTabs.jsx
@@ -11,23 +11,18 @@ export const EntityTab = observer(
     ({ entity, selected, style, onClick, bordered = true, prediction = false, displayGroundTruth = false }, ref) => {
       const isUnsaved = (entity.userGenerate && !entity.sentUserGenerate) || entity.draftSelected;
       const infoIsHidden = entity.store.hasInterface("annotations:hide-info");
-      const isInteractive = !!onClick;
 
       return (
         <div
           ref={ref}
           data-annotation-id={entity.pk ?? entity.id}
-          className={cn("entity-tab").mod({ selected, bordered, interactive: isInteractive }).toClassName()}
+          className={cn("entity-tab").mod({ selected, bordered }).toClassName()}
           style={style}
-          onClick={
-            isInteractive
-              ? (e) => {
-                  e.preventDefault();
-                  e.stopPropagation();
-                  onClick(entity, prediction);
-                }
-              : undefined
-          }
+          onClick={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            if (onClick) onClick(entity, prediction);
+          }}
         >
           <Space size="small">
             <Userpic
