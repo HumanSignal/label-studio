87e47975666a68bee1570423335339a445522408
Yousif Yassi <yousif@heartex.com>
2026-02-13 14:28:13 -0500

    switching to pascalCase

--- web/libs/core/src/lib/utils/string.ts
+++ web/libs/core/src/lib/utils/string.ts
@@ -4,14 +4,4 @@
  */
 
 export { camelCase, capitalize, kebabCase, snakeCase, startCase } from "es-toolkit/compat";
-
-import { camelCase } from "es-toolkit/compat";
-
-/**
- * Converts `string` to PascalCase (StudlyCase).
- * This is camelCase with the first letter capitalized.
- */
-export const toStudlyCaps = (str: string): string => {
-  const cc = camelCase(str);
-  return cc.charAt(0).toUpperCase() + cc.slice(1);
-};
+export { pascalCase } from "es-toolkit/string";
