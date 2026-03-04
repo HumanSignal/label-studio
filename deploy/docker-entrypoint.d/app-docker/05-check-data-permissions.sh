38194cb610527622e425effc7eddcaf8a899f8d1
Geddez <geddyville@gmail.com>
2026-03-04 15:24:21 -0500

    Updated dev docker

--- deploy/docker-entrypoint.d/app-docker/05-check-data-permissions.sh
+++ deploy/docker-entrypoint.d/app-docker/05-check-data-permissions.sh
@@ -1,1 +1,5 @@
-../common/05-check-data-permissions.sh
+#!/bin/bash
+
+# Delegate to shared data-permissions check script
+SCRIPT_DIR=$(cd -- "$(dirname -- "$0")" && pwd)
+exec "$SCRIPT_DIR/../common/05-check-data-permissions.sh"
