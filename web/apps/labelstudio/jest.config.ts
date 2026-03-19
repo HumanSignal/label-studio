52e4ed8fc06a1319a8c0b993a0bff984bc88e74b
Brandon Martel <brandonmartel@gmail.com>
2026-03-19 16:01:16 -0500

    Revert "upping the jest limits to try and tune this to the base runner resources"
    This reverts commit e05d8182debdbe5eeba74a54163ab1c969c5e144.
    
    GitOrigin-RevId: c0619ab196da1c111a12acb7b188424bb6818362

--- web/apps/labelstudio/jest.config.ts
+++ web/apps/labelstudio/jest.config.ts
@@ -15,5 +15,5 @@ export default {
   coverageDirectory: "../../coverage/apps/labelstudio",
   // In CI, fewer workers reduces peak memory while coverage is collected and merged (parent process
   // still holds the combined result). Locally we keep Jest's default for speed.
-  ...(isCi ? { maxWorkers: 4, workerIdleMemoryLimit: "1024MB" } : {}),
+  ...(isCi ? { maxWorkers: 2, workerIdleMemoryLimit: "512MB" } : {}),
 };
