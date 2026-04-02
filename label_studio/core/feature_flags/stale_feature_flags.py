b3a14b9a808c7a11d22b5721a65e3a75bb00d780
Raul Martin <luarmr@gmail.com>
2026-04-02 08:48:25 -0700

    chore: FIT-1563: Projects list still toggles page size and selector via a stale optic feature flag
    GitOrigin-RevId: 63a49890b21a498e8c23f0656980845ffb308804

--- label_studio/core/feature_flags/stale_feature_flags.py
+++ label_studio/core/feature_flags/stale_feature_flags.py
@@ -9,7 +9,6 @@ STALE_FEATURE_FLAGS = {
     # Feb 5
     'fflag_feature_all_optic_1421_cold_start_v2': False,
     'fflag_feat_optic_1025_zendesk_widget_integration': False,
-    'fflag_feat_optic_378_limit_projects_per_page_to_ten_short': True,
     # Feb 6
     'fflag_feat_all_dia_1576_prompts_easy_breezy_onboarding_short_async_presets_ks': False,
     'fflag_front_dia_1150_ddisco_sneak_preview': False,
