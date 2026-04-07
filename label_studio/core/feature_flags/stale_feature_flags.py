b5afa5f35480f84b3431035edd2925a8ca35d519
Raul Martin <luarmr@gmail.com>
2026-04-07 12:51:22 -0700

    chore: FIT-1578: Feature flag removal: fflag_root_43_prediction_results_filter
    GitOrigin-RevId: fbcdac547f962c7838ef12f6ab55243d859573e3

--- label_studio/core/feature_flags/stale_feature_flags.py
+++ label_studio/core/feature_flags/stale_feature_flags.py
@@ -14,7 +14,6 @@ STALE_FEATURE_FLAGS = {
     # Dec 29
     'fflag_feat_all_optic_1811_automax_project_setup_streaming': True,
     # Dec 31
-    'fflag_root_43_prediction_results_filter': True,
     'fflag_root_45_better_user_filter': True,
     'fflag_root_13_annotation_results_filtering': True,
     'fflag_feat_root_16_support_parquet_cloud_storage': True,
