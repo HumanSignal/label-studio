f4194bae8867f4e51c04696982a8d37c2267e6d4
Raul Martin <luarmr@gmail.com>
2026-04-09 08:57:06 -0700

    chore: FIT-1590: Feature flag removal: fflag_feat_all_optic_1811_automax_project_setup_streaming
    GitOrigin-RevId: 56e2f9b0860ca7961f28cce89624297063ea9a7d

--- label_studio/core/feature_flags/stale_feature_flags.py
+++ label_studio/core/feature_flags/stale_feature_flags.py
@@ -10,8 +10,6 @@ STALE_FEATURE_FLAGS = {
     # Feb 6
     'fflag_feat_all_dia_1576_prompts_easy_breezy_onboarding_short_async_presets_ks': False,
     'fflag_front_dia_1150_ddisco_sneak_preview': False,
-    # Dec 29
-    'fflag_feat_all_optic_1811_automax_project_setup_streaming': True,
     # Dec 31
     'fflag_root_45_better_user_filter': True,
     'fflag_feat_root_16_support_parquet_cloud_storage': True,
