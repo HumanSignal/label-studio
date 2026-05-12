ded3258eacb86cfa09f756e43847aa7aed577fdf
Yousif Yassi <104568407+yyassi-heartex@users.noreply.github.com>
2026-05-12 15:25:27 -0400

    fix: BROS-1187: Interfaces: Updating workspace for Project created from interface results in error 400 in console
    GitOrigin-RevId: 01a74ed8f4c96f42eb27568e2bb6811437b6f70a

--- web/libs/core/src/index.ts
+++ web/libs/core/src/index.ts
@@ -5,6 +5,7 @@ export * from "./lib/preview";
 export * from "./lib/utils/analytics";
 export * from "./lib/utils/urlJSON";
 export * from "./lib/utils/helpers";
+export * from "./lib/utils/validation-errors";
 export * from "./lib/utils/string";
 export * from "./lib/utils/debounce";
 export * from "./lib/utils/lodash-replacements";
