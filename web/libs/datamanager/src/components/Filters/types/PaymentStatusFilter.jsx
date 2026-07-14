1e82caa138336fcb6600d7d6d4a75950ce7b16e7
Hakan Erol <hakan@humansignal.com>
2026-07-13 19:40:55 -0700

    feat: UTC-1025: Make payment status filtering a dropdown
    GitOrigin-RevId: c4332290966615a22d49b91aae15bd7e339f6619

---
+++ web/libs/datamanager/src/components/Filters/types/PaymentStatusFilter.jsx
@@ -0,0 +1,37 @@
+import { Select } from "@humansignal/ui";
+
+const BaseInput = ({ schema, value, onChange, placeholder }) => {
+  const options = (schema?.items ?? []).map((item) => ({
+    value: item.value,
+    label: item.title,
+    textLabel: item.title,
+  }));
+
+  return (
+    <Select
+      options={options}
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
+      searchable={true}
+      onSearch={(query) => options.filter((option) => option.textLabel.toLowerCase().includes(query.toLowerCase()))}
+      size="small"
+      triggerClassName="min-w-[100px]"
+    />
+  );
+};
+
+export const PaymentStatusFilter = [
+  {
+    key: "contains",
+    label: "contains",
+    valueType: "single",
+    input: (props) => <BaseInput {...props} />,
+  },
+  {
+    key: "not_contains",
+    label: "not contains",
+    valueType: "single",
+    input: (props) => <BaseInput {...props} />,
+  },
+];
