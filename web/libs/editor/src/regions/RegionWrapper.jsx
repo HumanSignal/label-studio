7d0fc122b5877e44df6851dfca2243380ca8755e
Dandelion <3301085697@qq.com>
2026-08-05 09:55:03 +0800

    fix:fix the problem that frontend can not corectly render after ml-backend return rles

--- web/libs/editor/src/regions/RegionWrapper.jsx
+++ web/libs/editor/src/regions/RegionWrapper.jsx
@@ -1,15 +1,10 @@
 import { observer } from "mobx-react";
-import { Fragment, useContext } from "react";
-import { ImageViewContext } from "../components/ImageView/ImageViewContext";
-import { SuggestionControls } from "../components/ImageView/SuggestionControls";
+import { Fragment } from "react";
 
-export const RegionWrapper = observer(({ item, children }) => {
-  const { suggestion } = useContext(ImageViewContext) ?? {};
-
+export const RegionWrapper = observer(({ children }) => {
   return (
     <Fragment>
       {children}
-      {suggestion && <SuggestionControls item={item} useLayer={item.type === "brushregion"} />}
     </Fragment>
   );
 });
