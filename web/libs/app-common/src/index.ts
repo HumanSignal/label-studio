a83c11f0a3507d0885fccc3e8739fd3f025f8517
JackPan <826647196@qq.com>
2026-08-12 18:13:52 +0800

    feat(app-common): export i18n public API

--- web/libs/app-common/src/index.ts
+++ web/libs/app-common/src/index.ts
@@ -7,3 +7,16 @@ export { useStateHistory, type StateHistoryItem, type StateHistoryResponse } fro
 
 // Components
 export * from "./components/state-chips";
+
+// i18n
+export { I18nProvider, type I18nProviderProps } from "./i18n/I18nProvider";
+export { useLanguage, type UseLanguageResult } from "./i18n/useLanguage";
+export {
+  APP_LOCALES,
+  DEFAULT_LOCALE,
+  FALLBACK_LOCALE,
+  isSupportedLocale,
+  type SupportedLocale,
+} from "./i18n/types";
+export { resolveBrowserLocale } from "./i18n/detection";
+export { resolveInitialLanguage } from "./i18n/config";
